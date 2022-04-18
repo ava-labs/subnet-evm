e38c48ac3412f8d6c0d4a41c37d7f6e2ecc1cdfc
Connor Daly <connor.daly@avalabs.org>
2022-04-18 13:57:18 -0400

    Framework is working

--- go.mod
+++ go.mod
@@ -1,14 +1,16 @@
 module github.com/ava-labs/subnet-evm
 
-go 1.17
+go 1.18
 
 require (
 	github.com/VictoriaMetrics/fastcache v1.9.0
-	github.com/ava-labs/avalanchego v1.7.10-rc.0
+	github.com/ava-labs/avalanche-network-runner v1.0.6
+	github.com/ava-labs/avalanchego v1.7.10
 	github.com/cespare/cp v0.1.0
 	github.com/davecgh/go-spew v1.1.1
 	github.com/deckarep/golang-set v1.8.0
 	github.com/ethereum/go-ethereum v1.10.16
+	github.com/fatih/color v1.13.0
 	github.com/fjl/memsize v0.0.0-20190710130421-bcb5799ab5e5
 	github.com/gballet/go-libpcsclite v0.0.0-20191108122812-4678299bea08
 	github.com/google/uuid v1.1.5
@@ -22,6 +24,8 @@ require (
 	github.com/mattn/go-colorable v0.1.12
 	github.com/mattn/go-isatty v0.0.14
 	github.com/olekukonko/tablewriter v0.0.5
+	github.com/onsi/ginkgo/v2 v2.1.0
+	github.com/onsi/gomega v1.17.0
 	github.com/prometheus/client_golang v1.11.0
 	github.com/prometheus/client_model v0.2.0
 	github.com/rjeczalik/notify v0.9.2
@@ -38,18 +42,20 @@ require (
 	golang.org/x/time v0.0.0-20210723032227-1f47c861a9ac
 	gopkg.in/olebedev/go-duktape.v3 v3.0.0-20200619000410-60c24ae608a6
 	gopkg.in/urfave/cli.v1 v1.20.0
+	gopkg.in/yaml.v2 v2.4.0
 	gotest.tools v2.2.0+incompatible
 )
 
 require (
+	github.com/NYTimes/gziphandler v1.1.1 // indirect
 	github.com/StackExchange/wmi v0.0.0-20180116203802-5d049714c4a6 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/btcsuite/btcd v0.21.0-beta // indirect
 	github.com/btcsuite/btcutil v1.0.2 // indirect
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/decred/dcrd/dcrec/secp256k1/v3 v3.0.0-20200627015759-01fd2de07837 // indirect
 	github.com/deepmap/oapi-codegen v1.8.2 // indirect
 	github.com/edsrzf/mmap-go v1.0.0 // indirect
-	github.com/fatih/color v1.13.0 // indirect
 	github.com/fsnotify/fsnotify v1.5.1 // indirect
 	github.com/go-ole/go-ole v1.2.1 // indirect
 	github.com/go-stack/stack v1.8.0 // indirect
@@ -57,7 +63,10 @@ require (
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
 	github.com/google/go-cmp v0.5.6 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/graph-gophers/graphql-go v1.3.0 // indirect
+	github.com/grpc-ecosystem/go-grpc-prometheus v1.2.0 // indirect
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.7.2 // indirect
 	github.com/hashicorp/go-hclog v1.0.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/hashicorp/yamux v0.0.0-20200609203250-aecfd211c9ce // indirect
@@ -86,11 +95,16 @@ require (
 	github.com/prometheus/tsdb v0.10.0 // indirect
 	github.com/rs/cors v1.7.0 // indirect
 	github.com/shirou/gopsutil v3.21.4-0.20210419000835-c7a38de76ee5+incompatible // indirect
+	github.com/spaolacci/murmur3 v1.1.0 // indirect
 	github.com/spf13/afero v1.6.0 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
+	github.com/stretchr/objx v0.2.0 // indirect
 	github.com/subosito/gotenv v1.2.0 // indirect
 	github.com/tklauser/go-sysconf v0.3.5 // indirect
 	github.com/tklauser/numcpus v0.2.2 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/multierr v1.6.0 // indirect
+	go.uber.org/zap v1.19.0 // indirect
 	golang.org/x/net v0.0.0-20210813160813-60bc85c4be6d // indirect
 	golang.org/x/sys v0.0.0-20220204135822-1c1b9b1eba6a // indirect
 	golang.org/x/term v0.0.0-20210220032956-6a3ed077a48d // indirect
@@ -100,6 +114,5 @@ require (
 	google.golang.org/protobuf v1.27.1 // indirect
 	gopkg.in/ini.v1 v1.66.2 // indirect
 	gopkg.in/natefinch/npipe.v2 v2.0.0-20160621034901-c1b8fa8bdcce // indirect
-	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
---
+++ scripts/tests.e2e.sh
@@ -0,0 +1,149 @@
+#!/usr/bin/env bash
+set -e
+
+# e.g.,
+# ./scripts/build.sh
+# ./scripts/tests.e2e.sh ./build/avalanchego
+# ENABLE_WHITELIST_VTX_TESTS=true ./scripts/tests.e2e.sh ./build/avalanchego
+if ! [[ "$0" =~ scripts/tests.e2e.sh ]]; then
+  echo "must be run from repository root"
+  exit 255
+fi
+
+SUBNET_EVM_PATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )"; cd .. && pwd )
+
+# Load the versions
+source "$SUBNET_EVM_PATH"/scripts/versions.sh
+
+# Load the constants
+source "$SUBNET_EVM_PATH"/scripts/constants.sh
+
+VERSION=$1
+if [[ -z "${VERSION}" ]]; then
+  echo "Missing version argument!"
+  echo "Usage: ${0} [VERSION] [GENESIS_ADDRESS]" >> /dev/stderr
+  exit 255
+fi
+
+# AVALANCHEGO_PATH=$1
+# if [[ -z "${AVALANCHEGO_PATH}" ]]; then
+#   echo "Missing AVALANCHEGO_PATH argument!"
+#   echo "Usage: ${0} [AVALANCHEGO_PATH]" >> /dev/stderr
+#   exit 255
+# fi
+
+#################################
+# download avalanchego
+# https://github.com/ava-labs/avalanchego/releases
+GOARCH=$(go env GOARCH)
+GOOS=$(go env GOOS)
+DOWNLOAD_URL=https://github.com/ava-labs/avalanchego/releases/download/v${VERSION}/avalanchego-linux-${GOARCH}-v${VERSION}.tar.gz
+DOWNLOAD_PATH=/tmp/avalanchego.tar.gz
+if [[ ${GOOS} == "darwin" ]]; then
+  DOWNLOAD_URL=https://github.com/ava-labs/avalanchego/releases/download/v${VERSION}/avalanchego-macos-v${VERSION}.zip
+  DOWNLOAD_PATH=/tmp/avalanchego.zip
+fi
+
+rm -rf /tmp/avalanchego-v${VERSION}
+rm -rf /tmp/avalanchego-build
+rm -f ${DOWNLOAD_PATH}
+
+echo "downloading avalanchego ${VERSION} at ${DOWNLOAD_URL}"
+curl -L ${DOWNLOAD_URL} -o ${DOWNLOAD_PATH}
+
+echo "extracting downloaded avalanchego"
+if [[ ${GOOS} == "linux" ]]; then
+  tar xzvf ${DOWNLOAD_PATH} -C /tmp
+elif [[ ${GOOS} == "darwin" ]]; then
+  unzip ${DOWNLOAD_PATH} -d /tmp/avalanchego-build
+  mv /tmp/avalanchego-build/build /tmp/avalanchego-v${VERSION}
+fi
+find /tmp/avalanchego-v${VERSION}
+
+# Check if SUBNET_EVM_COMMIT is set, if not retrieve the last commit from the repo.
+# This is used in the Dockerfile to allow a commit hash to be passed in without
+# including the .git/ directory within the Docker image.
+subnet_evm_commit=${SUBNET_EVM_COMMIT:-$( git rev-list -1 HEAD )}
+
+# Build Subnet EVM, which is run as a subprocess
+echo "Building Subnet EVM Version: $subnet_evm_version; GitCommit: $subnet_evm_commit"
+go build \
+-ldflags "-X github.com/ava-labs/subnet_evm/plugin/evm.GitCommit=$subnet_evm_commit -X github.com/ava-labs/subnet_evm/plugin/evm.Version=$subnet_evm_version" \
+-o /tmp/avalanchego-v${VERSION}/plugins/srEXiWaHuhNyGwPUi444Tu47ZEDwxTWrbQiuD7FmgSAQ6X7Dy \
+"plugin/"*.go
+find /tmp/avalanchego-v${VERSION}
+
+ENABLE_WHITELIST_VTX_TESTS=${ENABLE_WHITELIST_VTX_TESTS:-false}
+
+#################################
+# download avalanche-network-runner
+# https://github.com/ava-labs/avalanche-network-runner
+# TODO: migrate to upstream avalanche-network-runner
+NETWORK_RUNNER_VERSION=1.0.6
+DOWNLOAD_PATH=/tmp/avalanche-network-runner.tar.gz
+DOWNLOAD_URL=https://github.com/ava-labs/avalanche-network-runner/releases/download/v${NETWORK_RUNNER_VERSION}/avalanche-network-runner_${NETWORK_RUNNER_VERSION}_linux_amd64.tar.gz
+if [[ ${GOOS} == "darwin" ]]; then
+  DOWNLOAD_URL=https://github.com/ava-labs/avalanche-network-runner/releases/download/v${NETWORK_RUNNER_VERSION}/avalanche-network-runner_${NETWORK_RUNNER_VERSION}_darwin_amd64.tar.gz
+fi
+
+rm -f ${DOWNLOAD_PATH}
+rm -f /tmp/avalanche-network-runner
+
+echo "downloading avalanche-network-runner ${NETWORK_RUNNER_VERSION} at ${DOWNLOAD_URL}"
+curl -L ${DOWNLOAD_URL} -o ${DOWNLOAD_PATH}
+
+echo "extracting downloaded avalanche-network-runner"
+tar xzvf ${DOWNLOAD_PATH} -C /tmp
+/tmp/avalanche-network-runner -h
+
+#################################
+echo "building e2e.test"
+# to install the ginkgo binary (required for test build and run)
+go install -v github.com/onsi/ginkgo/v2/ginkgo@v2.0.0
+ACK_GINKGO_RC=true ginkgo build ./e2e
+./e2e/e2e.test --help
+
+#################################
+# run "avalanche-network-runner" server
+echo "launch avalanche-network-runner in the background"
+/tmp/avalanche-network-runner \
+server \
+--log-level debug \
+--port=":12342" \
+--grpc-gateway-port=":12343" 2> /dev/null &
+PID=${!}
+
+#################################
+# By default, it runs all e2e test cases!
+# Use "--ginkgo.skip" to skip tests.
+# Use "--ginkgo.focus" to select tests.
+#
+# to run only ping tests:
+# --ginkgo.focus "\[Local\] \[Ping\]"
+#
+# to run only X-Chain whitelist vtx tests:
+# --ginkgo.focus "\[X-Chain\] \[WhitelistVtx\]"
+#
+# to skip all "Local" tests
+# --ginkgo.skip "\[Local\]"
+#
+# set "--enable-whitelist-vtx-tests" to explicitly enable/disable whitelist vtx tests
+echo "running e2e tests against the local cluster with /tmp/avalanchego-v${VERSION}/avalanchego"
+./e2e/e2e.test \
+--ginkgo.v \
+--log-level debug \
+--ginkgo.focus "\[Local\]" \
+--network-runner-grpc-endpoint="0.0.0.0:12342" \
+--avalanchego-log-level=INFO \
+--avalanchego-path=/tmp/avalanchego-v${VERSION}/avalanchego || EXIT_CODE=$?
+# --enable-whitelist-vtx-tests=${ENABLE_WHITELIST_VTX_TESTS}
+# --enable-whitelist-vtx-tests=false || EXIT_CODE=$?
+
+kill ${PID}
+
+if [[ ${EXIT_CODE} -gt 0 ]]; then
+  echo "FAILURE with exit code ${EXIT_CODE}"
+  exit ${EXIT_CODE}
+else
+  echo "ALL SUCCESS!"
+fi
