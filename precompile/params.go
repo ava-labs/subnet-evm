8bc7ae2703023ecb2d98beb6dd8f63fcb367444e
Aaron Buchwald <aaron.buchwald56@gmail.com>
2022-07-30 14:32:44 -0400

    Add comment documenting the reason for reserving address prefix 0x01 for coreth precompiles

--- precompile/params.go
+++ precompile/params.go
@@ -27,8 +27,11 @@ const (
 // Designated addresses of stateful precompiles
 // Note: it is important that none of these addresses conflict with each other or any other precompiles
 // in core/vm/contracts.go.
-// We start at 0x0200000000000000000000000000000000000000 and will increment by 1 from here to reduce
-// the risk of conflicts.
+// The first stateful precompiles were added in coreth to support nativeAssetCall and nativeAssetBalance. New stateful precompiles
+// originating in coreth will continue at this prefix, so we reserve this range in subnet-evm so that they can be migrated into
+// subnet-evm without issue. These start at the address: 0x0100000000000000000000000000000000000000 and will increment by 1.
+// Optional precompiles implemented in subnet-evm start at 0x0200000000000000000000000000000000000000 and will increment by 1
+// from here to reduce the risk of conflicts.
 // For forks of subnet-evm, users should start at 0x0300000000000000000000000000000000000000 to ensure
 // that their own modifications do not conflict with stateful precompiles that may be added to subnet-evm
 // in the future.
