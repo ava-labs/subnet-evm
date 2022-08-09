df130e076e7d8ead78379bea2ef46c5b1e65cb66
Ceyhun Onur <ceyhun.onur@avalabs.org>
2022-08-09 17:13:05 +0300

    rename contract to avoid collision

--- contract-examples/contracts/IFeeManagerPC.sol
+++ contract-examples/contracts/IFeeManagerPC.sol
@@ -2,42 +2,36 @@
 pragma solidity ^0.8.0;
 import "./IAllowList.sol";
 
-interface IFeeManager is IAllowList {
-    // Set fee config fields to contract storage
-    function setFeeConfigPC(
-        uint256 gasLimit,
-        uint256 targetBlockRate,
-        uint256 minBaseFee,
-        uint256 targetGas,
-        uint256 baseFeeChangeDenominator,
-        uint256 minBlockGasCost,
-        uint256 maxBlockGasCost,
-        uint256 blockGasCostStep
-    ) external;
+interface IFeeManagerPC is IAllowList {
+  // Set fee config fields to contract storage
+  function setFeeConfigPC(
+    uint256 gasLimit,
+    uint256 targetBlockRate,
+    uint256 minBaseFee,
+    uint256 targetGas,
+    uint256 baseFeeChangeDenominator,
+    uint256 minBlockGasCost,
+    uint256 maxBlockGasCost,
+    uint256 blockGasCostStep
+  ) external;
 
-    // Get fee config from the contract storage
-    function getFeeConfigPC()
-        external
-        view
-        returns (
-            uint256 gasLimit,
-            uint256 targetBlockRate,
-            uint256 minBaseFee,
-            uint256 targetGas,
-            uint256 baseFeeChangeDenominator,
-            uint256 minBlockGasCost,
-            uint256 maxBlockGasCost,
-            uint256 blockGasCostStep
-        );
+  // Get fee config from the contract storage
+  function getFeeConfigPC()
+    external
+    view
+    returns (
+      uint256 gasLimit,
+      uint256 targetBlockRate,
+      uint256 minBaseFee,
+      uint256 targetGas,
+      uint256 baseFeeChangeDenominator,
+      uint256 minBlockGasCost,
+      uint256 maxBlockGasCost,
+      uint256 blockGasCostStep
+    );
 
-    // Get the last block number changed the fee config from the contract storage
-    function getFeeConfigPCLastChangedAt()
-        external
-        view
-        returns (uint256 blockNumber);
+  // Get the last block number changed the fee config from the contract storage
+  function getFeeConfigPCLastChangedAt() external view returns (uint256 blockNumber);
 
-    function getTestPC(uint256 testt)
-        external
-        view
-        returns (uint256 blockNumber);
+  function getTestPC(uint256 testt) external view returns (uint256 blockNumber);
 }
