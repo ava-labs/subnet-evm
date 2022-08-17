e662db52572ae3dde73085181ce2a368585a84f3
Gyuho Lee <gyuho.lee@avalabs.org>
2022-08-17 12:02:01 -0700

    *: use avalanchego 1.7.17 for tests
    Signed-off-by: Gyuho Lee <gyuho.lee@avalabs.org>

--- scripts/versions.sh
+++ scripts/versions.sh
@@ -3,7 +3,7 @@
 # Set up the versions to be used
 subnet_evm_version=${SUBNET_EVM_VERSION:-'v0.2.9'}
 # Don't export them as they're used in the context of other calls
-avalanche_version=${AVALANCHE_VERSION:-'v1.7.16'}
+avalanche_version=${AVALANCHE_VERSION:-'v1.7.17'}
 network_runner_version=${NETWORK_RUNNER_VERSION:-'v1.2.0'}
 ginkgo_version=${GINKGO_VERSION:-'v2.1.4'}
 
