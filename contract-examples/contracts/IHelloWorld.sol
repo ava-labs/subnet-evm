4794ac68f49d08af1749158aee86f242b9342744
Anusha Chillara <anushachillara@gmail.com>
2022-08-24 15:13:16 -0700

    interface and abi

---
+++ contract-examples/contracts/IHelloWorld.sol
@@ -0,0 +1,13 @@
+// (c) 2022-2023, Ava Labs, Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.8.0;
+
+interface IHelloWorld {
+  function sayHello() external returns (string calldata);
+
+  // SetGreeting
+  function setGreeting(string calldata recipient) external;
+}
