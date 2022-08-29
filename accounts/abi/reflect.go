5f504cb69d7a85e87073d12af13d1a555bdd428e
Anusha Chillara <anushachillara@gmail.com>
2022-08-29 14:28:44 -0700

    fix reflect

--- accounts/abi/reflect.go
+++ accounts/abi/reflect.go
@@ -37,8 +37,8 @@ import (
 // ConvertType converts an interface of a runtime type into a interface of the
 // given type
 // e.g. turn
-// var fields []reflect.StructField
 //
+//	var fields []reflect.StructField
 //	fields = append(fields, reflect.StructField{
 //			Name: "X",
 //			Type: reflect.TypeOf(new(big.Int)),
@@ -183,13 +183,10 @@ func setStruct(dst, src reflect.Value) error {
 
 // mapArgNamesToStructFields maps a slice of argument names to struct fields.
 // first round: for each Exportable field that contains a `abi:""` tag
-//
-//	and this field name exists in the given argument name list, pair them together.
-//
+// and this field name exists in the given argument name list, pair them together.
 // second round: for each argument name that has not been already linked,
-//
-//	find what variable is expected to be mapped into, if it exists and has not been
-//	used, pair them.
+// find what variable is expected to be mapped into, if it exists and has not been
+// used, pair them.
 //
 // Note this function assumes the given value is a struct value.
 func mapArgNamesToStructFields(argNames []string, value reflect.Value) (map[string]string, error) {
