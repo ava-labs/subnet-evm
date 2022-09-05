5ad0659c80345a096fdb24570d934c5b7143eee0
Sam Batschelet <sam.batschelet@avalabs.org>
2022-09-05 01:24:04 -0400

    *: react to bump
    Signed-off-by: Sam Batschelet <sam.batschelet@avalabs.org>

--- scripts/versions.sh
+++ scripts/versions.sh
@@ -3,7 +3,7 @@
 # Set up the versions to be used
 subnet_evm_version=${SUBNET_EVM_VERSION:-'v0.2.9'}
 # Don't export them as they're used in the context of other calls
-avalanche_version=${AVALANCHE_VERSION:-'v1.7.18'}
-network_runner_version=${NETWORK_RUNNER_VERSION:-'v1.2.0'}
+avalanche_version=${AVALANCHE_VERSION:-'v1.8.0'}
+network_runner_version=${NETWORK_RUNNER_VERSION:-'v1.2.1'}
 ginkgo_version=${GINKGO_VERSION:-'v2.1.4'}
 
