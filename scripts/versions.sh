2e29e23ea8fefdf3a9504528cea155fc9b5f34fc
Aaron Buchwald <aaron.buchwald56@gmail.com>
2022-09-05 02:42:47 -0400

    Update subnet evm version to v0.3.0

--- scripts/versions.sh
+++ scripts/versions.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Set up the versions to be used
-subnet_evm_version=${SUBNET_EVM_VERSION:-'v0.2.9'}
+subnet_evm_version=${SUBNET_EVM_VERSION:-'v0.3.0'}
 # Don't export them as they're used in the context of other calls
 avalanche_version=${AVALANCHE_VERSION:-'v1.8.0'}
 network_runner_version=${NETWORK_RUNNER_VERSION:-'v1.2.1'}
