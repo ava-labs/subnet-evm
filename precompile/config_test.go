3964fe59695f4329ff5741c32956bc596e389e1a
Ceyhun Onur <ceyhun.onur@avalabs.org>
2022-09-16 14:09:16 +0300

    add equal tesets for precompile configs

--- precompile/config_test.go
+++ precompile/config_test.go
@@ -13,6 +13,19 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+var validFeeConfig = commontype.FeeConfig{
+	GasLimit:        big.NewInt(8_000_000),
+	TargetBlockRate: 2, // in seconds
+
+	MinBaseFee:               big.NewInt(25_000_000_000),
+	TargetGas:                big.NewInt(15_000_000),
+	BaseFeeChangeDenominator: big.NewInt(36),
+
+	MinBlockGasCost:  big.NewInt(0),
+	MaxBlockGasCost:  big.NewInt(1_000_000),
+	BlockGasCostStep: big.NewInt(200_000),
+}
+
 func TestVerifyPrecompileUpgrades(t *testing.T) {
 	admins := []common.Address{{1}}
 	enableds := []common.Address{{2}}
@@ -93,6 +106,262 @@ func TestVerifyPrecompileUpgrades(t *testing.T) {
 			} else {
 				require.ErrorContains(err, tt.expectedError)
 			}
+		})
+	}
+}
+
+func TestEqualTxAllowListConfig(t *testing.T) {
+	admins := []common.Address{{1}}
+	enableds := []common.Address{{2}}
+	tests := []struct {
+		name     string
+		config   StatefulPrecompileConfig
+		other    StatefulPrecompileConfig
+		expected bool
+	}{
+		{
+			name:     "non-nil config and nil other",
+			config:   NewTxAllowListConfig(big.NewInt(3), admins, enableds),
+			other:    nil,
+			expected: false,
+		},
+		{
+			name:     "different type",
+			config:   NewTxAllowListConfig(big.NewInt(3), admins, enableds),
+			other:    NewContractDeployerAllowListConfig(big.NewInt(3), admins, enableds),
+			expected: false,
+		},
+		{
+			name:     "different admin",
+			config:   NewTxAllowListConfig(big.NewInt(3), admins, enableds),
+			other:    NewTxAllowListConfig(big.NewInt(3), []common.Address{{3}}, enableds),
+			expected: false,
+		},
+		{
+			name:     "different enabled",
+			config:   NewTxAllowListConfig(big.NewInt(3), admins, enableds),
+			other:    NewTxAllowListConfig(big.NewInt(3), admins, []common.Address{{3}}),
+			expected: false,
+		},
+		{
+			name:     "different version",
+			config:   NewTxAllowListConfig(big.NewInt(3), admins, enableds),
+			other:    NewTxAllowListConfig(big.NewInt(4), admins, enableds),
+			expected: false,
+		},
+		{
+			name:     "same config",
+			config:   NewTxAllowListConfig(big.NewInt(3), admins, enableds),
+			other:    NewTxAllowListConfig(big.NewInt(3), admins, enableds),
+			expected: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			require := require.New(t)
+
+			require.Equal(tt.expected, tt.config.Equal(tt.other))
+		})
+	}
+}
+
+func TestEqualContractDeployerAllowListConfig(t *testing.T) {
+	admins := []common.Address{{1}}
+	enableds := []common.Address{{2}}
+	tests := []struct {
+		name     string
+		config   StatefulPrecompileConfig
+		other    StatefulPrecompileConfig
+		expected bool
+	}{
+		{
+			name:     "non-nil config and nil other",
+			config:   NewContractDeployerAllowListConfig(big.NewInt(3), admins, enableds),
+			other:    nil,
+			expected: false,
+		},
+		{
+			name:     "different type",
+			config:   NewContractDeployerAllowListConfig(big.NewInt(3), admins, enableds),
+			other:    NewTxAllowListConfig(big.NewInt(3), admins, enableds),
+			expected: false,
+		},
+		{
+			name:     "different admin",
+			config:   NewContractDeployerAllowListConfig(big.NewInt(3), admins, enableds),
+			other:    NewContractDeployerAllowListConfig(big.NewInt(3), []common.Address{{3}}, enableds),
+			expected: false,
+		},
+		{
+			name:     "different enabled",
+			config:   NewContractDeployerAllowListConfig(big.NewInt(3), admins, enableds),
+			other:    NewContractDeployerAllowListConfig(big.NewInt(3), admins, []common.Address{{3}}),
+			expected: false,
+		},
+		{
+			name:     "different version",
+			config:   NewContractDeployerAllowListConfig(big.NewInt(3), admins, enableds),
+			other:    NewContractDeployerAllowListConfig(big.NewInt(4), admins, enableds),
+			expected: false,
+		},
+		{
+			name:     "same config",
+			config:   NewContractDeployerAllowListConfig(big.NewInt(3), admins, enableds),
+			other:    NewContractDeployerAllowListConfig(big.NewInt(3), admins, enableds),
+			expected: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			require := require.New(t)
+
+			require.Equal(tt.expected, tt.config.Equal(tt.other))
+		})
+	}
+}
+
+func TestEqualContractNativeMinterConfig(t *testing.T) {
+	admins := []common.Address{{1}}
+	enableds := []common.Address{{2}}
+	tests := []struct {
+		name     string
+		config   StatefulPrecompileConfig
+		other    StatefulPrecompileConfig
+		expected bool
+	}{
+		{
+			name:     "non-nil config and nil other",
+			config:   NewContractNativeMinterConfig(big.NewInt(3), admins, enableds, nil),
+			other:    nil,
+			expected: false,
+		},
+		{
+			name:     "different type",
+			config:   NewContractNativeMinterConfig(big.NewInt(3), admins, enableds, nil),
+			other:    NewTxAllowListConfig(big.NewInt(3), []common.Address{{1}}, []common.Address{{2}}),
+			expected: false,
+		},
+		{
+			name:     "different version",
+			config:   NewContractNativeMinterConfig(big.NewInt(3), admins, nil, nil),
+			other:    NewContractNativeMinterConfig(big.NewInt(4), admins, nil, nil),
+			expected: false,
+		},
+		{
+			name:     "different enabled",
+			config:   NewContractNativeMinterConfig(big.NewInt(3), admins, nil, nil),
+			other:    NewContractNativeMinterConfig(big.NewInt(3), admins, enableds, nil),
+			expected: false,
+		},
+		{
+			name: "different initial mint amounts",
+			config: NewContractNativeMinterConfig(big.NewInt(3), admins, nil,
+				map[common.Address]*math.HexOrDecimal256{
+					common.HexToAddress("0x01"): math.NewHexOrDecimal256(1),
+				}),
+			other: NewContractNativeMinterConfig(big.NewInt(3), admins, nil,
+				map[common.Address]*math.HexOrDecimal256{
+					common.HexToAddress("0x01"): math.NewHexOrDecimal256(2),
+				}),
+			expected: false,
+		},
+		{
+			name: "different initial mint addresses",
+			config: NewContractNativeMinterConfig(big.NewInt(3), admins, nil,
+				map[common.Address]*math.HexOrDecimal256{
+					common.HexToAddress("0x01"): math.NewHexOrDecimal256(1),
+				}),
+			other: NewContractNativeMinterConfig(big.NewInt(3), admins, nil,
+				map[common.Address]*math.HexOrDecimal256{
+					common.HexToAddress("0x02"): math.NewHexOrDecimal256(1),
+				}),
+			expected: false,
+		},
+
+		{
+			name: "same config",
+			config: NewContractNativeMinterConfig(big.NewInt(3), admins, nil,
+				map[common.Address]*math.HexOrDecimal256{
+					common.HexToAddress("0x01"): math.NewHexOrDecimal256(1),
+				}),
+			other: NewContractNativeMinterConfig(big.NewInt(3), admins, nil,
+				map[common.Address]*math.HexOrDecimal256{
+					common.HexToAddress("0x01"): math.NewHexOrDecimal256(1),
+				}),
+			expected: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			require := require.New(t)
+
+			require.Equal(tt.expected, tt.config.Equal(tt.other))
+		})
+	}
+}
+
+func TestEqualFeeConfigManagerConfig(t *testing.T) {
+	admins := []common.Address{{1}}
+	enableds := []common.Address{{2}}
+	tests := []struct {
+		name     string
+		config   StatefulPrecompileConfig
+		other    StatefulPrecompileConfig
+		expected bool
+	}{
+		{
+			name:     "non-nil config and nil other",
+			config:   NewFeeManagerConfig(big.NewInt(3), admins, enableds, nil),
+			other:    nil,
+			expected: false,
+		},
+		{
+			name:     "different type",
+			config:   NewFeeManagerConfig(big.NewInt(3), admins, enableds, nil),
+			other:    NewTxAllowListConfig(big.NewInt(3), []common.Address{{1}}, []common.Address{{2}}),
+			expected: false,
+		},
+		{
+			name:     "different version",
+			config:   NewFeeManagerConfig(big.NewInt(3), admins, nil, nil),
+			other:    NewFeeManagerConfig(big.NewInt(4), admins, nil, nil),
+			expected: false,
+		},
+		{
+			name:     "different enabled",
+			config:   NewFeeManagerConfig(big.NewInt(3), admins, nil, nil),
+			other:    NewFeeManagerConfig(big.NewInt(3), admins, enableds, nil),
+			expected: false,
+		},
+		{
+			name:     "non-nil initial config and nil initial config",
+			config:   NewFeeManagerConfig(big.NewInt(3), admins, nil, &validFeeConfig),
+			other:    NewFeeManagerConfig(big.NewInt(3), admins, nil, nil),
+			expected: false,
+		},
+		{
+			name:   "different initial config",
+			config: NewFeeManagerConfig(big.NewInt(3), admins, nil, &validFeeConfig),
+			other: NewFeeManagerConfig(big.NewInt(3), admins, nil,
+				func() *commontype.FeeConfig {
+					c := validFeeConfig
+					c.GasLimit = big.NewInt(123)
+					return &c
+				}()),
+			expected: false,
+		},
+		{
+			name:     "same config",
+			config:   NewFeeManagerConfig(big.NewInt(3), admins, nil, &validFeeConfig),
+			other:    NewFeeManagerConfig(big.NewInt(3), admins, nil, &validFeeConfig),
+			expected: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			require := require.New(t)
+
+			require.Equal(tt.expected, tt.config.Equal(tt.other))
 		})
 	}
 }
