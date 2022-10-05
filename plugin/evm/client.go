4701eb45d930dfbdd2bff895adf5d381edbffe02
Anusha Chillara <anushachillara@gmail.com>
2022-10-05 09:50:27 -0700

    changes to requester

--- plugin/evm/client.go
+++ plugin/evm/client.go
@@ -34,8 +34,8 @@ type client struct {
 // NewClient returns a Client for interacting with EVM [chain]
 func NewClient(uri, chain string) Client {
 	return &client{
-		requester:      rpc.NewEndpointRequester(fmt.Sprintf("%s/ext/bc/%s/avax", uri, chain), "avax"),
-		adminRequester: rpc.NewEndpointRequester(fmt.Sprintf("%s/ext/bc/%s/admin", uri, chain), "admin"),
+		requester:      rpc.NewEndpointRequester(fmt.Sprintf("%s/ext/bc/%s/avax", uri, chain)),
+		adminRequester: rpc.NewEndpointRequester(fmt.Sprintf("%s/ext/bc/%s/admin", uri, chain)),
 	}
 }
 
@@ -46,24 +46,24 @@ func NewCChainClient(uri string) Client {
 }
 
 func (c *client) StartCPUProfiler(ctx context.Context) error {
-	return c.adminRequester.SendRequest(ctx, "startCPUProfiler", struct{}{}, &api.EmptyReply{})
+	return c.adminRequester.SendRequest(ctx, "admin.startCPUProfiler", struct{}{}, &api.EmptyReply{})
 }
 
 func (c *client) StopCPUProfiler(ctx context.Context) error {
-	return c.adminRequester.SendRequest(ctx, "stopCPUProfiler", struct{}{}, &api.EmptyReply{})
+	return c.adminRequester.SendRequest(ctx, "admin.stopCPUProfiler", struct{}{}, &api.EmptyReply{})
 }
 
 func (c *client) MemoryProfile(ctx context.Context) error {
-	return c.adminRequester.SendRequest(ctx, "memoryProfile", struct{}{}, &api.EmptyReply{})
+	return c.adminRequester.SendRequest(ctx, "admin.memoryProfile", struct{}{}, &api.EmptyReply{})
 }
 
 func (c *client) LockProfile(ctx context.Context) error {
-	return c.adminRequester.SendRequest(ctx, "lockProfile", struct{}{}, &api.EmptyReply{})
+	return c.adminRequester.SendRequest(ctx, "admin.lockProfile", struct{}{}, &api.EmptyReply{})
 }
 
 // SetLogLevel dynamically sets the log level for the C Chain
 func (c *client) SetLogLevel(ctx context.Context, level log.Lvl) error {
-	return c.adminRequester.SendRequest(ctx, "setLogLevel", &SetLogLevelArgs{
+	return c.adminRequester.SendRequest(ctx, "admin.setLogLevel", &SetLogLevelArgs{
 		Level: level.String(),
 	}, &api.EmptyReply{})
 }
@@ -71,6 +71,6 @@ func (c *client) SetLogLevel(ctx context.Context, level log.Lvl) error {
 // GetVMConfig returns the current config of the VM
 func (c *client) GetVMConfig(ctx context.Context) (*Config, error) {
 	res := &ConfigReply{}
-	err := c.adminRequester.SendRequest(ctx, "getVMConfig", struct{}{}, res)
+	err := c.adminRequester.SendRequest(ctx, "admin.getVMConfig", struct{}{}, res)
 	return res.Config, err
 }
