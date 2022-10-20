baa27478a74857441af6ab818cfbd56329a093d6
Aaron Buchwald <aaron.buchwald56@gmail.com>
2022-10-20 15:05:11 -0700

    Add trie clean journal params to config

--- eth/ethconfig/config.go
+++ eth/ethconfig/config.go
@@ -113,6 +113,8 @@ type Config struct {
 	// DatabaseFreezer    string
 
 	TrieCleanCache        int
+	TrieCleanJournal      string
+	TrieCleanRejournal    time.Duration
 	TrieDirtyCache        int
 	TrieDirtyCommitTarget int
 	SnapshotCache         int
