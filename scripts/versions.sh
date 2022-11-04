297d140af3dd32121adc8befc1c893320e821e55
Aaron Buchwald <aaron.buchwald56@gmail.com>
2022-11-04 18:35:22 -0400

    Bump avalanchego to v1.9.2

--- scripts/versions.sh
+++ scripts/versions.sh
@@ -3,7 +3,7 @@
 # Set up the versions to be used
 subnet_evm_version=${SUBNET_EVM_VERSION:-'v0.4.3'}
 # Don't export them as they're used in the context of other calls
-avalanche_version=${AVALANCHE_VERSION:-'v1.9.2-rc.7'}
+avalanche_version=${AVALANCHE_VERSION:-'v1.9.2'}
 network_runner_version=${NETWORK_RUNNER_VERSION:-'bb4b661ac88ebe50ab719424eecc1a55e01e7019'}
 ginkgo_version=${GINKGO_VERSION:-'v2.2.0'}
 
