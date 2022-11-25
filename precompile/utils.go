53696da98e3c1497aae6f79a8c6f2becc9a30327
Aaron Buchwald <aaron.buchwald56@gmail.com>
2022-11-25 14:13:04 -0500

    Add CalculateANTAssetID helper

--- precompile/utils.go
+++ precompile/utils.go
@@ -67,3 +67,10 @@ func returnPackedHash(packed []byte, index int) []byte {
 	end := start + common.HashLength
 	return packed[start:end]
 }
+
+// CalculateANTAssetID calculates the hash of the caller address concatenated with the blockchainID, which
+// serves as the assetID of the token that can be minted on this blockchain by the specified caller.
+func CalculateANTAssetID(blockchainID common.Hash, caller common.Address) common.Hash {
+	assetID := crypto.Keccak256Hash(blockchainID[:], caller[:])
+	return assetID
+}
