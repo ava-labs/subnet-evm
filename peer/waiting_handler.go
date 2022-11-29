950675c44ade81d160cd642bd401e1750604991e
Anusha <anushachillara@gmail.com>
2022-11-28 17:12:28 -0800

    Add CrossChainRequest to Subnet-EVM

--- peer/waiting_handler.go
+++ peer/waiting_handler.go
@@ -9,6 +9,7 @@ import (
 )
 
 var _ message.ResponseHandler = &waitingResponseHandler{}
+var _ message.CrossChainResponseHandler = &waitingCrossChainResponseHandler{}
 
 // waitingResponseHandler implements the ResponseHandler interface
 // Internally used to wait for response after making a request synchronously
@@ -36,4 +37,32 @@ func (w *waitingResponseHandler) OnFailure(ids.NodeID, uint32) error {
 // newWaitingResponseHandler returns new instance of the waitingResponseHandler
 func newWaitingResponseHandler() *waitingResponseHandler {
 	return &waitingResponseHandler{responseChan: make(chan []byte)}
+}
+
+// waitingCrossChainResponseHandler implements the CrossChainResponseHandler interface.
+// This is used internally to wait for a response after making a synchronous CrossChainRequest.
+// responseChan may contain response bytes if the original request has not failed.
+// responseChan is closed in either fail or success scenario.
+type waitingCrossChainResponseHandler struct {
+	responseChan chan []byte // blocking channel with response bytes
+	failed       bool        // whether the original request is failed
+}
+
+// OnResponse passes the response bytes to the responseChan and closes the channel
+func (w *waitingCrossChainResponseHandler) OnResponse(_ ids.ID, _ uint32, response []byte) error {
+	w.responseChan <- response
+	close(w.responseChan)
+	return nil
+}
+
+// OnFailure sets the failed flag to true and closes the channel
+func (w *waitingCrossChainResponseHandler) OnFailure(ids.ID, uint32) error {
+	w.failed = true
+	close(w.responseChan)
+	return nil
+}
+
+// newWaitingCrossChainResponseHandler returns new instance of the waitingCrossChainResponseHandler
+func newWaitingCrossChainResponseHandler() *waitingCrossChainResponseHandler {
+	return &waitingCrossChainResponseHandler{responseChan: make(chan []byte)}
 }
