094e3cae98f5140348e1591d7f0992f04a85ec0f
Anusha <anushachillara@gmail.com>
2022-11-30 15:52:33 -0800

    pack into abi

--- plugin/evm/message/test_cross_chain_request.go
+++ plugin/evm/message/test_cross_chain_request.go
@@ -2,29 +2,53 @@ package message
 
 import (
 	"encoding/json"
+	"os"
+	"strings"
 
+	"github.com/ava-labs/avalanchego/ids"
+	"github.com/ava-labs/subnet-evm/accounts/abi"
 	"github.com/ava-labs/subnet-evm/internal/ethapi"
+	"github.com/ava-labs/subnet-evm/peer"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/hexutil"
 )
 
-
-func SendEthCallCrossChainRequest(networkClient peer.NetworkClient, chainID id.ID) error {
-	// TO BE FILLED
-	ethCallArgs := &ethapi.TransactionArgs{
-		To: 
-		Data: 
-	} 
-
-	ethCallBytes, err := json.Marshal(ethCallArgs)
-	if err != nil {
-		return err 
-	}
-
-	ethCallRequest := CrossChainCodec.Marshal(Version, EthCallRequest{RequestArgs: ethCallBytes})
-
-	response, err := networkClient.CrossChainRequest(chainID, ethCethCallRequest)
+func SendEthCallCrossChainRequest(networkClient peer.NetworkClient, chainID ids.ID) error {
+	abiJSON, err := os.ReadFile("./abi/ERC20NativeMinterABI.json")
 	if err != nil {
 		return err
 	}
 
-	return nil 
+	abi, err := abi.JSON(strings.NewReader(string(abiJSON)))
+	if err != nil {
+		return err
+	}
+
+	dataBytes, err := abi.Pack("decimals", "")
+	data := hexutil.Bytes(dataBytes)
+
+	address := common.HexToAddress("0XFAKE")
+
+	//TO BE FILLED
+	ethCallArgs := &ethapi.TransactionArgs{
+		To:   &address,
+		Data: &data,
+	}
+
+	ethCallBytes, err := json.Marshal(ethCallArgs)
+	if err != nil {
+		return err
+	}
+
+	ethCallRequest, err := CrossChainCodec.Marshal(Version, EthCallRequest{RequestArgs: ethCallBytes})
+	if err != nil {
+		return err
+	}
+
+	response, err := networkClient.CrossChainRequest(chainID, ethCallRequest)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
