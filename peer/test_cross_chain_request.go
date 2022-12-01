07ac05a9a431ed66c68a8002238176d68c18139b
Anusha <anushachillara@gmail.com>
2022-11-30 16:02:38 -0800

    correct tempalte

--- peer/test_cross_chain_request.go
+++ peer/test_cross_chain_request.go
@@ -1,4 +1,4 @@
-package message
+package peer
 
 import (
 	"encoding/json"
@@ -8,19 +8,23 @@ import (
 	"github.com/ava-labs/avalanchego/ids"
 	"github.com/ava-labs/subnet-evm/accounts/abi"
 	"github.com/ava-labs/subnet-evm/internal/ethapi"
-	"github.com/ava-labs/subnet-evm/peer"
+	"github.com/ava-labs/subnet-evm/plugin/evm/message"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/ethereum/go-ethereum/log"
 )
 
-func SendEthCallCrossChainRequest(networkClient peer.NetworkClient, chainID ids.ID) error {
+func SendEthCallCrossChainRequest(networkClient NetworkClient, chainID ids.ID) error {
 	abiJSON, err := os.ReadFile("./abi/ERC20NativeMinterABI.json")
 	if err != nil {
+		log.Error("failed to read file", "error", err)
 		return err
 	}
 
 	abi, err := abi.JSON(strings.NewReader(string(abiJSON)))
 	if err != nil {
+		log.Error("failed to parse ABI interface ", "error", err)
+
 		return err
 	}
 
@@ -37,18 +41,23 @@ func SendEthCallCrossChainRequest(networkClient peer.NetworkClient, chainID ids.
 
 	ethCallBytes, err := json.Marshal(ethCallArgs)
 	if err != nil {
+		log.Error("failed to marshal into JSON encoding ", "error", err)
+
 		return err
 	}
 
-	ethCallRequest, err := CrossChainCodec.Marshal(Version, EthCallRequest{RequestArgs: ethCallBytes})
+	ethCallRequest, err := message.CrossChainCodec.Marshal(message.Version, message.EthCallRequest{RequestArgs: ethCallBytes})
 	if err != nil {
+		log.Error("failed to marshal into codec encoding", "error", err)
 		return err
 	}
 
 	response, err := networkClient.CrossChainRequest(chainID, ethCallRequest)
 	if err != nil {
+		log.Error("failed to send CCR", "error", err)
 		return err
 	}
 
+	log.Info("Success ! Response from CCR", "response", response)
 	return nil
 }
