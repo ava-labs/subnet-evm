b7afb02c150a94ee3e0d8342a7406740b9aa7696
Anusha <anushachillara@gmail.com>
2022-12-20 15:08:34 -0800

    remove err

--- plugin/evm/vm.go
+++ plugin/evm/vm.go
@@ -386,7 +386,7 @@ func (vm *VM) Initialize(
 	vm.genesisHash = genesisBlock.Hash()
 
 	if !vm.chainConfig.IsSubnetEVM(genesisBlock.Number()) {
-		return fmt.Errorf("SubnetEVM upgrade is not enabled: %w", err)
+		return fmt.Errorf("SubnetEVM upgrade is not enabled")
 	}
 
 	lastAcceptedHash, lastAcceptedHeight, err := vm.readLastAccepted()
