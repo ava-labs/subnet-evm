5457c13dc705d81c8890188a0881734c1b8fc808
Darioush Jalali <darioush.jalali@avalabs.org>
2023-01-04 09:25:36 -0800

    Light client mode to unlock dynamic state sync

--- scripts/versions.sh
+++ scripts/versions.sh
@@ -3,7 +3,7 @@
 # Set up the versions to be used
 subnet_evm_version=${SUBNET_EVM_VERSION:-'v0.4.7'}
 # Don't export them as they're used in the context of other calls
-avalanche_version=${AVALANCHE_VERSION:-'v1.9.5'}
+avalanche_version=${AVALANCHE_VERSION:-'v1.9.6-rc.0'}
 network_runner_version=${NETWORK_RUNNER_VERSION:-'35be10cd3867a94fbe960a1c14a455f179de60d9'}
 ginkgo_version=${GINKGO_VERSION:-'v2.2.0'}
 
