f4c399e9d3f272183fc54b43c722662671d1e1c5
Ceyhun Onur <ceyhun.onur@avalabs.org>
2023-01-04 10:45:33 +0300

    Update precompile/nativeminter/contract_native_minter.go
    Co-authored-by: Darioush Jalali <darioush.jalali@avalabs.org>

--- precompile/nativeminter/contract_native_minter.go
+++ precompile/nativeminter/contract_native_minter.go
@@ -83,7 +83,7 @@ func mintNativeCoin(accessibleState precompile.PrecompileAccessibleState, caller
 	}
 
 	stateDB := accessibleState.GetStateDB()
-	// Verify that the caller is in the allow list and therefore has the right to modify it
+	// Verify that the caller is in the allow list
 	callerStatus := allowlist.GetAllowListStatus(stateDB, precompile.ContractNativeMinterAddress, caller)
 	if !callerStatus.IsEnabled() {
 		return nil, remainingGas, fmt.Errorf("%w: %s", ErrCannotMint, caller)
