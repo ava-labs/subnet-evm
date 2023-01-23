b0f2ff3c4cf3970bf0de5ee2dfb9346eeb80c3df
Matthew Lam <matthew.lam@avalabs.org>
2023-01-23 13:35:00 -0800

    fix pr nits

--- plugin/evm/warp/backend.go
+++ plugin/evm/warp/backend.go
@@ -1,7 +1,7 @@
 // (c) 2019-2020, Ava Labs, Inc. All rights reserved.
 // See the file LICENSE for licensing terms.
 
-package evm
+package warp
 
 import (
 	"context"
@@ -9,19 +9,13 @@ import (
 
 	"github.com/ava-labs/avalanchego/cache"
 	"github.com/ava-labs/avalanchego/database"
-	"github.com/ava-labs/avalanchego/database/prefixdb"
-	"github.com/ava-labs/avalanchego/database/versiondb"
 	"github.com/ava-labs/avalanchego/ids"
 	"github.com/ava-labs/avalanchego/snow"
 	"github.com/ava-labs/avalanchego/utils/hashing"
 	"github.com/ava-labs/avalanchego/vms/platformvm/teleporter"
 )
 
-var (
-	_ WarpBackend = &warpBackend{}
-
-	dbPrefix = []byte("warp")
-)
+var _ WarpBackend = &warpBackend{}
 
 // WarpBackend tracks signature eligible warp messages and provides an interface to fetch them.
 // The backend is also used to query for warp message signatures by the signature request handler.
@@ -35,48 +29,48 @@ type WarpBackend interface {
 
 // warpBackend implements WarpBackend, keeps track of warp messages, and generates message signatures.
 type warpBackend struct {
-	database.Database
+	db             database.Database
 	snowCtx        *snow.Context
 	signatureCache *cache.LRU
 }
 
-// NewWarpBackend creates a new warpBackend, and initializes the signature cache and message tracking database.
-func NewWarpBackend(snowCtx *snow.Context, vmDB *versiondb.Database, signatureCacheSize int) WarpBackend {
+// NewWarpBackend creates a new WarpBackend, and initializes the signature cache and message tracking database.
+func NewWarpBackend(snowCtx *snow.Context, db database.Database, signatureCacheSize int) WarpBackend {
 	return &warpBackend{
-		Database:       prefixdb.New(dbPrefix, vmDB),
+		db:             db,
 		snowCtx:        snowCtx,
 		signatureCache: &cache.LRU{Size: signatureCacheSize},
 	}
 }
 
 func (w *warpBackend) AddMessage(ctx context.Context, unsignedMessage *teleporter.UnsignedMessage) error {
-	messageHashBytes := hashing.ComputeHash256(unsignedMessage.Bytes())
-	messageHash, err := ids.ToID(messageHashBytes)
-	if err != nil {
-		return fmt.Errorf("failed to generate message hash for warp message db: %w", err)
-	}
+	messageID := hashing.ComputeHash256Array(unsignedMessage.Bytes())
 
-	// We generate the signature here and only save the signature in the db.
+	// We generate the signature here and only save the signature in the db and cache.
 	// It is left to smart contracts built on top of Warp to save messages if required.
 	signature, err := w.snowCtx.TeleporterSigner.Sign(unsignedMessage)
 	if err != nil {
-		return fmt.Errorf("failed to sign warp message %s: %w", messageHash.String(), err)
+		return fmt.Errorf("failed to sign warp message: %w", err)
 	}
 
-	return w.Put(messageHash[:], signature)
+	if err := w.db.Put(messageID[:], signature); err != nil {
+		return fmt.Errorf("failed to put warp signature in db: %w", err)
+	}
+
+	w.signatureCache.Put(messageID[:], signature)
+	return nil
 }
 
-func (w *warpBackend) GetSignature(ctx context.Context, messageHash ids.ID) ([]byte, error) {
-	// Attempt to get the signature from cache before calling the db.
-	if sig, ok := w.signatureCache.Get(messageHash[:]); ok {
+func (w *warpBackend) GetSignature(ctx context.Context, messageID ids.ID) ([]byte, error) {
+	if sig, ok := w.signatureCache.Get(messageID[:]); ok {
 		return sig.([]byte), nil
 	}
 
-	signature, err := w.Get(messageHash[:])
+	signature, err := w.db.Get(messageID[:])
 	if err != nil {
-		return nil, fmt.Errorf("failed to get warp signature for message %s from db: %w", messageHash.String(), err)
+		return nil, fmt.Errorf("failed to get warp signature for message %s from db: %w", messageID.String(), err)
 	}
 
-	w.signatureCache.Put(messageHash[:], signature)
+	w.signatureCache.Put(messageID[:], signature)
 	return signature, nil
 }
