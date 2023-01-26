934a64ed141e1bb62cb0ee904560e6e64dfceba0
Aaron Buchwald <aaron.buchwald56@gmail.com>
2023-01-26 17:53:03 -0500

    Step 0: Generating the precompile

---
+++ contract-examples/contracts/IHelloWorld.sol
@@ -0,0 +1,14 @@
+// (c) 2022-2023, Ava Labs, Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.8.0;
+
+interface IHelloWorld {
+  // sayHello returns the stored greeting string
+  function sayHello() external view returns (string calldata result);
+
+  // setGreeting  stores the greeting string
+  function setGreeting(string calldata response) external;
+}
