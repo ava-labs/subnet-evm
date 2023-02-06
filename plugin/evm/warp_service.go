fc668f9cc8c585671b168b7eb30369512097822d
cam-schultz <camschultz32@gmail.com>
2023-02-06 18:17:54 +0000

    build fix

--- plugin/evm/warp_service.go
+++ plugin/evm/warp_service.go
@@ -6,7 +6,6 @@ package evm
 import (
 	"context"
 
-	"github.com/ava-labs/avalanchego/utils/crypto/bls"
 	"github.com/ava-labs/subnet-evm/plugin/evm/message"
 	"github.com/ethereum/go-ethereum/log"
 )
@@ -15,13 +14,12 @@ import (
 type WarpAPI struct{ vm *VM }
 
 func (api *WarpAPI) GetSignature(ctx context.Context, signatureRequest *message.SignatureRequest) (*message.SignatureResponse, error) {
-	sig, err := api.vm.backend.GetSignature(ctx, signatureRequest.MessageID)
+	signature, err := api.vm.backend.GetSignature(ctx, signatureRequest.MessageID)
 	if err != nil {
 		log.Debug("Unknown warp signature requested", "messageID", signatureRequest.MessageID)
 		return nil, nil
 	}
-	var signature [bls.SignatureLen]byte
-	copy(signature[:], sig)
+
 	response := message.SignatureResponse{
 		Signature: signature,
 	}
