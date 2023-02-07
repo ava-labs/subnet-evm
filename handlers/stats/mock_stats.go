9c084578d21db2b0ab70c5be74205d1f94d84fe2
Aaron Buchwald <aaron.buchwald56@gmail.com>
2023-02-06 16:19:30 -0800

    Remove unnecessary lock

--- handlers/stats/mock_stats.go
+++ handlers/stats/mock_stats.go
@@ -4,8 +4,6 @@
 package stats
 
 import (
-	"sync"
-
 	warpStats "github.com/ava-labs/subnet-evm/handlers/warp/stats"
 	syncStats "github.com/ava-labs/subnet-evm/sync/handlers/stats"
 )
@@ -14,16 +12,11 @@ var _ HandlerStats = &MockHandlerStats{}
 
 // MockHandlerStats is mock for capturing and asserting on handler metrics in test
 type MockHandlerStats struct {
-	lock sync.Mutex
-
 	syncStats.MockHandlerStats
 	warpStats.MockSignatureRequestHandlerStats
 }
 
 func (m *MockHandlerStats) Reset() {
-	m.lock.Lock()
-	defer m.lock.Unlock()
-
 	m.MockHandlerStats.Reset()
 	m.MockSignatureRequestHandlerStats.Reset()
 }
