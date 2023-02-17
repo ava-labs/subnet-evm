f176ae6d7c38a8b279671ac81f8dd15cfc3770f2
Shubham Goyal <sgshubham@gmail.com>
2023-02-17 17:37:10 +0530

    Add GetOpenOrders API

--- plugin/evm/limitorders/service.go
+++ plugin/evm/limitorders/service.go
@@ -5,7 +5,10 @@ package limitorders
 
 import (
 	"context"
+	"fmt"
 	"math/big"
+	"strconv"
+	"strings"
 )
 
 type OrderBookAPI struct {
@@ -32,9 +35,24 @@ func (api *OrderBookAPI) GetDetailedOrderBookData(ctx context.Context) InMemoryD
 	return api.db.GetOrderBookData()
 }
 
-func (api *OrderBookAPI) GetOrderBook(ctx context.Context) OrderBookResponse {
+func (api *OrderBookAPI) GetOrderBook(ctx context.Context, marketStr string) (*OrderBookResponse, error) {
+	// market is a string cuz it's an optional param
 	allOrders := api.db.GetAllOrders()
 	orders := []OrderMin{}
+
+	if len(marketStr) > 0 {
+		market, err := strconv.Atoi(marketStr)
+		if err != nil {
+			return nil, fmt.Errorf("invalid market")
+		}
+		marketOrders := []LimitOrder{}
+		for _, order := range allOrders {
+			if order.Market == Market(market) {
+				marketOrders = append(marketOrders, order)
+			}
+		}
+		allOrders = marketOrders
+	}
 
 	for _, order := range allOrders {
 		orders = append(orders, OrderMin{
@@ -44,5 +62,21 @@ func (api *OrderBookAPI) GetOrderBook(ctx context.Context) OrderBookResponse {
 		})
 	}
 
-	return OrderBookResponse{Orders: orders}
+	return &OrderBookResponse{Orders: orders}, nil
+}
+
+func (api *OrderBookAPI) GetOpenOrders(ctx context.Context, trader string) OrderBookResponse {
+	traderOrders := []OrderMin{}
+	orderMap := api.db.GetOrderBookData().OrderMap
+	for _, order := range orderMap {
+		if strings.ToLower(order.UserAddress) == strings.ToLower(trader) {
+			traderOrders = append(traderOrders, OrderMin{
+				Market: order.Market,
+				Price: order.Price,
+				Size: order.GetUnFilledBaseAssetQuantity(),
+			})
+		}
+	}
+
+	return OrderBookResponse{Orders: traderOrders}
 }
