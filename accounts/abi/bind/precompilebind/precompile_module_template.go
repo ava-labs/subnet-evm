9a63a87806847eb8c3018eb97eb3056d5a5df2fe
Ceyhun Onur <ceyhun.onur@avalabs.org>
2023-02-17 12:25:09 +0300

    fix comments

--- accounts/abi/bind/precompilebind/precompile_module_template.go
+++ accounts/abi/bind/precompilebind/precompile_module_template.go
@@ -11,23 +11,24 @@ const tmplSourcePrecompileModuleGo = `
 // There are some must-be-done changes waiting in the file. Each area requiring you to add your code is marked with CUSTOM CODE to make them easy to find and modify.
 // Additionally there are other files you need to edit to activate your precompile.
 // These areas are highlighted with comments "ADD YOUR PRECOMPILE HERE".
-// For testing take a look at other precompile tests in core/stateful_precompile_test.go and config_test.go in other precompile folders.
+// For testing take a look at other precompile tests in contract_test.go and config_test.go in other precompile folders.
+// See the tutorial in https://docs.avax.network/subnets/hello-world-precompile-tutorial for more information about precompile development.
 
 /* General guidelines for precompile development:
-1- Read the comment and set a suitable contract address in generated contract.go. E.g:
+1- Read the comment and set a suitable contract address in generated module.go. E.g:
 	ContractAddress = common.HexToAddress("ASUITABLEHEXADDRESS")
-2- It is recommended to only modify code in the highlighted areas marked with "CUSTOM CODE STARTS HERE". Modifying code outside of these areas should be done with caution and with a deep understanding of how these changes may impact the EVM.
-Typically, custom codes are required in only those areas.
-3- Set gas costs in generated contract.go
-4- Register your precompile module in params/precompile_modules.go
+2- Set a suitable config key in generated module.go. E.g: "yourPrecompileConfig"
+3- It is recommended to only modify code in the highlighted areas marked with "CUSTOM CODE STARTS HERE". Typically, custom codes are required in only those areas.
+Modifying code outside of these areas should be done with caution and with a deep understanding of how these changes may impact the EVM.
+4- Set gas costs in generated contract.go
 5- Add your config unit tests under generated package config_test.go
-6- Add your contract unit tests under tests/statefulprecompiles/{precompilename}_test.go
-7- Add your solidity interface and test contract to contract-examples/contracts
-8- Write solidity tests for your precompile in contract-examples/test
-9- Create your genesis with your precompile enabled in tests/e2e/genesis/
-10- Create e2e test for your solidity test in tests/e2e/solidity/suites.go
-11- Run your e2e precompile Solidity tests with 'E2E=true ./scripts/run.sh'
-
+6- Add your contract unit tests undertgenerated package contract_test.go
+7- Additionally you can add a full-fledged VM test for your precompile under plugin/vm/vm_test.go. See existing precompile tests for examples.
+8- Add your solidity interface and test contract to contract-examples/contracts
+9- Write solidity tests for your precompile in contract-examples/test
+10- Create your genesis with your precompile enabled in tests/e2e/genesis/
+11- Create e2e test for your solidity test in tests/e2e/solidity/suites.go
+12- Run your e2e precompile Solidity tests with 'E2E=true ./scripts/run.sh'
 */
 
 package {{.Package}}
