7bba43ad03fa7bcc9b0eac57db13f7895050e512
Ceyhun Onur <ceyhun.onur@avalabs.org>
2023-02-21 20:40:36 +0300

    nit improvements

--- accounts/abi/bind/precompilebind/precompile_module_template.go
+++ accounts/abi/bind/precompilebind/precompile_module_template.go
@@ -15,20 +15,21 @@ const tmplSourcePrecompileModuleGo = `
 // See the tutorial in https://docs.avax.network/subnets/hello-world-precompile-tutorial for more information about precompile development.
 
 /* General guidelines for precompile development:
-1- Read the comment and set a suitable contract address in generated module.go. E.g:
+1- Set a suitable config key in generated module.go. E.g: "yourPrecompileConfig"
+2- Read the comment and set a suitable contract address in generated module.go. E.g:
 	ContractAddress = common.HexToAddress("ASUITABLEHEXADDRESS")
-2- Set a suitable config key in generated module.go. E.g: "yourPrecompileConfig"
 3- It is recommended to only modify code in the highlighted areas marked with "CUSTOM CODE STARTS HERE". Typically, custom codes are required in only those areas.
 Modifying code outside of these areas should be done with caution and with a deep understanding of how these changes may impact the EVM.
 4- Set gas costs in generated contract.go
-5- Add your config unit tests under generated package config_test.go
-6- Add your contract unit tests undertgenerated package contract_test.go
-7- Additionally you can add a full-fledged VM test for your precompile under plugin/vm/vm_test.go. See existing precompile tests for examples.
-8- Add your solidity interface and test contract to contract-examples/contracts
-9- Write solidity tests for your precompile in contract-examples/test
-10- Create your genesis with your precompile enabled in tests/e2e/genesis/
-11- Create e2e test for your solidity test in tests/e2e/solidity/suites.go
-12- Run your e2e precompile Solidity tests with 'E2E=true ./scripts/run.sh'
+5- Force import your precompile package in precompile/registry/registry.go
+6- Add your config unit tests under generated package config_test.go
+7- Add your contract unit tests undertgenerated package contract_test.go
+8- Additionally you can add a full-fledged VM test for your precompile under plugin/vm/vm_test.go. See existing precompile tests for examples.
+9- Add your solidity interface and test contract to contract-examples/contracts
+10- Write solidity tests for your precompile in contract-examples/test
+11- Create your genesis with your precompile enabled in tests/e2e/genesis/
+12- Create e2e test for your solidity test in tests/e2e/solidity/suites.go
+13- Run your e2e precompile Solidity tests with 'E2E=true ./scripts/run.sh
 */
 
 package {{.Package}}
@@ -36,7 +37,7 @@ package {{.Package}}
 import (
 	"fmt"
 
-	"github.com/ava-labs/subnet-evm/precompile/config"
+	"github.com/ava-labs/subnet-evm/precompile/precompileconfig"
 	"github.com/ava-labs/subnet-evm/precompile/contract"
 	"github.com/ava-labs/subnet-evm/precompile/modules"
 
@@ -45,7 +46,7 @@ import (
 
 var _ contract.Configurator = &configurator{}
 
-// ConfigKey is the key used in json config files to specify this precompile config.
+// ConfigKey is the key used in json config files to specify this precompile precompileconfig.
 // must be unique across all precompiles.
 const ConfigKey = "{{decapitalise .Contract.Type}}Config"
 
@@ -72,17 +73,17 @@ func init() {
 	}
 }
 
-// NewConfig returns a new precompile config.
-// This is required for Marshal/Unmarshal the precompile config.
-func (*configurator) NewConfig() config.Config {
+// NewConfig returns a new precompile precompileconfig.
+// This is required for Marshal/Unmarshal the precompile precompileconfig.
+func (*configurator) NewConfig() precompileconfig.Config {
 	return &Config{}
 }
 
-// Configure configures [state] with the given [cfg] config.
+// Configure configures [state] with the given [cfg] precompileconfig.
 // This function is called by the EVM once per precompile contract activation.
 // You can use this function to set up your precompile contract's initial state,
 // by using the [cfg] config and [state] stateDB.
-func (*configurator) Configure(chainConfig contract.ChainConfig, cfg config.Config, state contract.StateDB, _ contract.BlockContext) error {
+func (*configurator) Configure(chainConfig contract.ChainConfig, cfg precompileconfig.Config, state contract.StateDB, _ contract.BlockContext) error {
 	config, ok := cfg.(*Config)
 	if !ok {
 		return fmt.Errorf("incorrect config %T: %v", config, config)
@@ -90,7 +91,7 @@ func (*configurator) Configure(chainConfig contract.ChainConfig, cfg config.Conf
 	// CUSTOM CODE STARTS HERE
 	{{if .Contract.AllowList}}
 	// AllowList is activated for this precompile. Configuring allowlist addresses here.
-	return config.Configure(state, ContractAddress)
+	return config.AllowListConfig.Configure(state, ContractAddress)
 	{{else}}
 	return nil
 	{{end}}
