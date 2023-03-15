485ed590651aff19e15e3df1e457f0408ad0c498
Aaron Buchwald <aaron.buchwald56@gmail.com>
2023-03-15 15:09:37 -0400

    Fix typo

--- plugin/evm/vm_upgrade_bytes_test.go
+++ plugin/evm/vm_upgrade_bytes_test.go
@@ -94,7 +94,7 @@ func TestVMUpgradeBytesPrecompile(t *testing.T) {
 		metrics.Enabled = true
 	}()
 	if err := vm.Initialize(
-		context.Background(), vm.ctx, dbManager, []byte(genesisJSONSubnetEVM), upgradeBytesJSON, []byte{}, issuer, []*common.Fx{}, appSender,
+		context.Background(), vm.ctx, dbManager, []byte(genesisJSONSubnetEVM), upgradeBytesJSON, []byte{}, issuer, []*commonEng.Fx{}, appSender,
 	); err != nil {
 		t.Fatal(err)
 	}
