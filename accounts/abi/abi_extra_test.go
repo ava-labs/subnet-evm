dd14a6bd34bfea4c88fce36ee87af348719ee2d3
Darioush Jalali <darioush.jalali@avalabs.org>
2023-03-17 15:24:43 -0700

    use struct type

--- accounts/abi/abi_extra_test.go
+++ accounts/abi/abi_extra_test.go
@@ -5,29 +5,44 @@ package abi
 
 import (
 	"bytes"
+	"math/big"
 	"strings"
 	"testing"
 
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/stretchr/testify/require"
 )
 
 // Note: This file contains tests in addition to those found in go-ethereum.
 
-const TEST_ABI = `[ { "type" : "function", "name" : "receive", "inputs" : [ { "name" : "memo", "type" : "bytes" }], "outputs":[{"internalType":"bool","name":"isAllowed","type":"bool"}] } ]`
+const TEST_ABI = `[{"type":"function","name":"receive","inputs":[{"name":"sender","type":"address"},{"name":"amount","type":"uint256"},{"name":"memo","type":"bytes"}],"outputs":[{"internalType":"bool","name":"isAllowed","type":"bool"}]}]`
 
 func TestUnpackInputIntoInterface(t *testing.T) {
 	abi, err := JSON(strings.NewReader(TEST_ABI))
 	require.NoError(t, err)
 
-	input := []byte("hello")
-	data, err := abi.Pack("receive", input)
+	type inputType struct {
+		Sender common.Address
+		Amount *big.Int
+		Memo   []byte
+	}
+	input := inputType{
+		Sender: common.HexToAddress("0x02"),
+		Amount: big.NewInt(100),
+		Memo:   []byte("hello"),
+	}
+	data, err := abi.Pack("receive", input.Sender, input.Amount, input.Memo)
 	require.NoError(t, err)
 
-	var v []byte
+	// Unpack into interface
+	var v inputType
 	err = abi.UnpackInputIntoInterface(&v, "receive", data[4:]) // skips 4 byte selector
 	require.NoError(t, err)
 
-	require.True(t, bytes.Equal(v, input))
+	// Verify unpacked values match input
+	require.Equal(t, v.Amount, input.Amount)
+	require.EqualValues(t, v.Amount, input.Amount)
+	require.True(t, bytes.Equal(v.Memo, input.Memo))
 }
 
 func TestPackOutput(t *testing.T) {
