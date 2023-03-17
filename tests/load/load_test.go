51f58c0202eecd76c871109ceff5c5e8e3185bbd
Aaron Buchwald <aaron.buchwald56@gmail.com>
2023-03-17 10:47:21 -0400

    Update load test to use ENV var from CI if populated

--- tests/load/load_test.go
+++ tests/load/load_test.go
@@ -21,11 +21,20 @@ import (
 )
 
 var (
-	config   = runner.NewDefaultANRConfig()
-	manager  = runner.NewNetworkManager(config)
+	config   runner.ANRConfig
+	manager  runner.NetworkManager
 	done     <-chan struct{}
 	numNodes = 5
 )
+
+func init() {
+	config := runner.NewDefaultANRConfig()
+	if envBuildPath, exists := os.LookupEnv("AVALANCHEGO_BUILD_PATH"); exists {
+		config.AvalancheGoExecPath = fmt.Sprintf("%s/avalanchego", envBuildPath)
+		config.PluginDir = fmt.Sprintf("%s/plugins", envBuildPath)
+	}
+	manager = *runner.NewNetworkManager(config)
+}
 
 func TestE2E(t *testing.T) {
 	gomega.RegisterFailHandler(ginkgo.Fail)
