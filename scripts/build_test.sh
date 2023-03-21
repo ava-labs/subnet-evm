8e697f816bf7de0815756d7b09d6799cd7538213
Aaron Buchwald <aaron.buchwald56@gmail.com>
2023-03-21 13:15:20 -0400

    Add warp e2e test to exclusion from build_test.sh

--- scripts/build_test.sh
+++ scripts/build_test.sh
@@ -21,4 +21,4 @@ source "$SUBNET_EVM_PATH"/scripts/constants.sh
 # We pass in the arguments to this script directly to enable easily passing parameters such as enabling race detection,
 # parallelism, and test coverage.
 # DO NOT RUN "tests/precompile" or "tests/load" since it's run by ginkgo
-go test -coverprofile=coverage.out -covermode=atomic -timeout="30m" $@ $(go list ./... | grep -v tests/precompile | grep -v tests/load)
+go test -coverprofile=coverage.out -covermode=atomic -timeout="30m" $@ $(go list ./... | grep -v tests/precompile | grep -v tests/load | grep -v tests/warp)
