4dce9032f9d0582e9f2f223f463c3d8d30cb6441
Richard Pringle <richard.pringle@avalabs.org>
2023-04-12 18:19:44 -0400

    WIP-2

--- contract-examples/contracts/ExampleFeeManager.sol
+++ contract-examples/contracts/ExampleFeeManager.sol
@@ -9,48 +9,66 @@ import "./IFeeManager.sol";
 
 address constant FEE_MANAGER_ADDRESS = 0x0200000000000000000000000000000000000003;
 
+uint constant WAGMI_GAS_LIMIT = 20_000_000;
+uint constant WAGMI_TARGET_BLOCK_RATE = 2;
+uint constant WAGMI_MIN_BASE_FEE = 1_000_000_000;
+uint constant WAGMI_TARGET_GAS = 100_000_000;
+uint constant WAGMI_BASE_FEE_CHANGE_DENOMINATOR = 48;
+uint constant WAGMI_MIN_BLOCK_GAS_COST = 0;
+uint constant WAGMI_MAX_BLOCK_GAS_COST = 10_000_000;
+uint constant WAGMI_BLOCK_GAS_COST_STEP = 500_000;
+
+uint constant CCHAIN_GAS_LIMIT = 8_000_000;
+uint constant CCHAIN_TARGET_BLOCK_RATE = 2;
+uint constant CCHAIN_MIN_BASE_FEE = 25_000_000_000;
+uint constant CCHAIN_TARGET_GAS = 15_000_000;
+uint constant CCHAIN_BASE_FEE_CHANGE_DENOMINATOR = 36;
+uint constant CCHAIN_MIN_BLOCK_GAS_COST = 0;
+uint constant CCHAIN_MAX_BLOCK_GAS_COST = 1_000_000;
+uint constant CCHAIN_BLOCK_GAS_COST_STEP = 100_000;
+
+struct FeeConfig {
+  uint256 gasLimit;
+  uint256 targetBlockRate;
+  uint256 minBaseFee;
+  uint256 targetGas;
+  uint256 baseFeeChangeDenominator;
+  uint256 minBlockGasCost;
+  uint256 maxBlockGasCost;
+  uint256 blockGasCostStep;
+}
+
 // ExampleFeeManager shows how FeeManager precompile can be used in a smart contract
 // All methods of [allowList] can be directly called. There are example calls as tasks in hardhat.config.ts file.
 contract ExampleFeeManager is AllowList {
   // Precompiled Fee Manager Contract Address
   IFeeManager feeManager = IFeeManager(FEE_MANAGER_ADDRESS);
 
-  struct FeeConfig {
-    uint256 gasLimit;
-    uint256 targetBlockRate;
-    uint256 minBaseFee;
-    uint256 targetGas;
-    uint256 baseFeeChangeDenominator;
-    uint256 minBlockGasCost;
-    uint256 maxBlockGasCost;
-    uint256 blockGasCostStep;
-  }
-
   constructor() AllowList(FEE_MANAGER_ADDRESS) {}
 
   function enableWAGMIFees() public onlyEnabled {
     feeManager.setFeeConfig(
-      20_000_000, // gasLimit
-      2, // targetBlockRate
-      1_000_000_000, // minBaseFee
-      100_000_000, // targetGas
-      48, // baseFeeChangeDenominator
-      0, // minBlockGasCost
-      10_000_000, // maxBlockGasCost
-      500_000 // blockGasCostStep
+      WAGMI_GAS_LIMIT,
+      WAGMI_TARGET_BLOCK_RATE,
+      WAGMI_MIN_BASE_FEE,
+      WAGMI_TARGET_GAS,
+      WAGMI_BASE_FEE_CHANGE_DENOMINATOR,
+      WAGMI_MIN_BLOCK_GAS_COST,
+      WAGMI_MAX_BLOCK_GAS_COST,
+      WAGMI_BLOCK_GAS_COST_STEP
     );
   }
 
   function enableCChainFees() public onlyEnabled {
     feeManager.setFeeConfig(
-      8_000_000, // gasLimit
-      2, // targetBlockRate
-      25_000_000_000, // minBaseFee
-      15_000_000, // targetGas
-      36, // baseFeeChangeDenominator
-      0, // minBlockGasCost
-      1_000_000, // maxBlockGasCost
-      200_000 // blockGasCostStep
+      CCHAIN_GAS_LIMIT,
+      CCHAIN_TARGET_BLOCK_RATE,
+      CCHAIN_MIN_BASE_FEE,
+      CCHAIN_TARGET_GAS,
+      CCHAIN_BASE_FEE_CHANGE_DENOMINATOR,
+      CCHAIN_MIN_BLOCK_GAS_COST,
+      CCHAIN_MAX_BLOCK_GAS_COST,
+      CCHAIN_BLOCK_GAS_COST_STEP
     );
   }
 
@@ -130,7 +148,7 @@ contract ExampleFeeManagerTest is DSTest {
     assertEq(manager.readAllowList(exampleAddress), 1);
   }
 
-  function test_enableCustomFees() public {
+  function test_changeFees() public {
     ExampleFeeManager example = new ExampleFeeManager();
     address exampleAddress = address(example);
 
@@ -138,21 +156,79 @@ contract ExampleFeeManagerTest is DSTest {
 
     manager.setEnabled(exampleAddress);
 
-    ExampleFeeManager.FeeConfig memory config = example.getCurrentFeeConfig();
+    FeeConfig memory config = example.getCurrentFeeConfig();
 
-    uint256 newGasLimit = config.gasLimit + 10;
-    uint256 newMinBaseFee = config.minBaseFee + 10;
+    FeeConfig memory newFeeConfig = FeeConfig({
+      gasLimit: CCHAIN_GAS_LIMIT,
+      targetBlockRate: CCHAIN_TARGET_BLOCK_RATE,
+      minBaseFee: CCHAIN_MIN_BASE_FEE,
+      targetGas: CCHAIN_TARGET_GAS,
+      baseFeeChangeDenominator: CCHAIN_BASE_FEE_CHANGE_DENOMINATOR,
+      minBlockGasCost: CCHAIN_MIN_BLOCK_GAS_COST,
+      maxBlockGasCost: CCHAIN_MAX_BLOCK_GAS_COST,
+      blockGasCostStep: CCHAIN_BLOCK_GAS_COST_STEP
+    });
 
-    config.gasLimit = newGasLimit;
-    config.minBaseFee = newMinBaseFee;
+    assertNotEq(config.gasLimit, newFeeConfig.gasLimit);
+    // target block rate is the same for wagmi and cchain
+    // assertNotEq(config.targetBlockRate, newFeeConfig.targetBlockRate);
+    assertNotEq(config.minBaseFee, newFeeConfig.minBaseFee);
+    assertNotEq(config.targetGas, newFeeConfig.targetGas);
+    assertNotEq(config.baseFeeChangeDenominator, newFeeConfig.baseFeeChangeDenominator);
+    // min block gas cost is the same for wagmi and cchain
+    // assertNotEq(config.minBlockGasCost, newFeeConfig.minBlockGasCost);
+    assertNotEq(config.maxBlockGasCost, newFeeConfig.maxBlockGasCost);
+    assertNotEq(config.blockGasCostStep, newFeeConfig.blockGasCostStep);
 
-    example.enableCustomFees(config);
+    example.enableCChainFees();
 
-    ExampleFeeManager.FeeConfig memory newConfig = example.getCurrentFeeConfig();
+    FeeConfig memory changedFeeConfig = example.getCurrentFeeConfig();
 
-    assertEq(newConfig.gasLimit, newGasLimit);
-    assertEq(newConfig.minBaseFee, newMinBaseFee);
+    assertEq(changedFeeConfig.gasLimit, newFeeConfig.gasLimit);
+    // target block rate is the same for wagmi and cchain
+    // assertEq(changedFeeConfig.targetBlockRate, newFeeConfig.targetBlockRate);
+    assertEq(changedFeeConfig.minBaseFee, newFeeConfig.minBaseFee);
+    assertEq(changedFeeConfig.targetGas, newFeeConfig.targetGas);
+    assertEq(changedFeeConfig.baseFeeChangeDenominator, newFeeConfig.baseFeeChangeDenominator);
+    // min block gas cost is the same for wagmi and cchain
+    // assertEq(changedFeeConfig.minBlockGasCost, newFeeConfig.minBlockGasCost);
+    assertEq(changedFeeConfig.maxBlockGasCost, newFeeConfig.maxBlockGasCost);
+    assertEq(changedFeeConfig.blockGasCostStep, newFeeConfig.blockGasCostStep);
 
     assertEq(example.getFeeConfigLastChangedAt(), block.number);
+
+    example.enableCustomFees(config);
+  }
+
+  function test_minFeeTransaction() public {
+    // noop
+  }
+
+  function raiseMinFeeByOne() public {
+    ExampleFeeManager example = new ExampleFeeManager();
+    address exampleAddress = address(example);
+
+    IFeeManager manager = IFeeManager(FEE_MANAGER_ADDRESS);
+
+    manager.setEnabled(exampleAddress);
+
+    FeeConfig memory config = example.getCurrentFeeConfig();
+    config.minBaseFee = config.minBaseFee + 1;
+
+    example.enableCustomFees(config);
+  }
+
+  function lowerMinFeeByOne() public {
+    ExampleFeeManager example = new ExampleFeeManager();
+    address exampleAddress = address(example);
+
+    IFeeManager manager = IFeeManager(FEE_MANAGER_ADDRESS);
+
+    manager.setEnabled(exampleAddress);
+
+    FeeConfig memory config = example.getCurrentFeeConfig();
+    config.minBaseFee = config.minBaseFee - 1;
+
+    example.enableCustomFees(config);
   }
 }
--- contract-examples/test/fee_manager.ts
+++ contract-examples/test/fee_manager.ts
@@ -15,41 +15,10 @@ const assert = require("assert");
 const ADMIN_ADDRESS: string = "0x8db97C7cEcE249c2b98bDC0226Cc4C2A57BF52FC"
 const FEE_MANAGER = "0x0200000000000000000000000000000000000003";
 
-const ROLES = {
-  NONE: 0,
-  ENABLED: 1,
-  ADMIN: 2
-};
+const GENESIS_CONFIG = require('../../tests/precompile/genesis/fee_manager.json');
 
-const C_FEES = {
-  gasLimit: 8_000_000, // gasLimit
-  targetBlockRate: 2, // targetBlockRate
-  minBaseFee: 25_000_000_000, // minBaseFee
-  targetGas: 15_000_000, // targetGas
-  baseFeeChangeDenominator: 36, // baseFeeChangeDenominator
-  minBlockGasCost: 0, // minBlockGasCost
-  maxBlockGasCost: 1_000_000, // maxBlockGasCost
-  blockGasCostStep: 100_000 // blockGasCostStep
-}
-
-const WAGMI_FEES = {
-  gasLimit: 20_000_000, // gasLimit
-  targetBlockRate: 2, // targetBlockRate
-  minBaseFee: 1_000_000_000, // minBaseFee
-  targetGas: 100_000_000, // targetGas
-  baseFeeChangeDenominator: 48, // baseFeeChangeDenominator
-  minBlockGasCost: 0, // minBlockGasCost
-  maxBlockGasCost: 10_000_000, // maxBlockGasCost
-  blockGasCostStep: 100_000 // blockGasCostStep
-}
-
-const dbg = val => {
-  console.log(val)
-  return val
-}
-
-const testFn = fnName => async function () {
-  const tx = await this.testContract["test_" + fnName]()
+const testFn = (fnName, overrides = {}) => async function () {
+  const tx = await this.testContract["test_" + fnName](overrides)
   const txReceipt = await tx.wait()
   const failed = await this.testContract.callStatic.failed()
 
@@ -69,251 +38,60 @@ const testFn = fnName => async function () {
   assert(!failed, `${fnName} failed`)
 }
 
-const test = (name, fnName) => it(name, testFn(fnName)); 
-test.only = (name, fnName) => it.only(name, testFn(fnName));
+const test = (name, fnName, overrides = {}) => it(name, testFn(fnName, overrides)); 
+test.only = (name, fnName, overrides = {}) => it.only(name, testFn(fnName, overrides));
 
 // TODO: These tests keep state to the next state. It means that some tests cases assumes some preconditions
 // set by previous test cases. We should make these tests stateless.
 describe("ExampleFeeManager", function () {
   this.timeout("30s")
 
-  let owner: SignerWithAddress
-  let contract: Contract
-  let manager: SignerWithAddress
-  let nonEnabled: SignerWithAddress
-  let managerPrecompile: Contract
-  let ownerPrecompile: Contract
+  beforeEach("setup DS-Test contract", async function () {
+    const signer = await ethers.getSigner(ADMIN_ADDRESS);
+    const feeManagerPromise = ethers.getContractAt("IFeeManager", FEE_MANAGER, signer)
 
-  before(async function () {
-    owner = await ethers.getSigner(ADMIN_ADDRESS);
-    const contractF: ContractFactory = await ethers.getContractFactory("ExampleFeeManager", { signer: owner })
-    contract = await contractF.deploy()
-    await contract.deployed()
-    const contractAddress: string = contract.address
-    console.log(`Contract deployed to: ${contractAddress}`)
-
-    managerPrecompile = await ethers.getContractAt("IFeeManager", FEE_MANAGER, manager);
-    ownerPrecompile = await ethers.getContractAt("IFeeManager", FEE_MANAGER, owner);
-
-    const signers: SignerWithAddress[] = await ethers.getSigners()
-    manager = signers.slice(-1)[0]
-    nonEnabled = signers.slice(-2)[0]
-
-    let tx = await ownerPrecompile.setEnabled(manager.address);
-    await tx.wait()
-
-    tx = await owner.sendTransaction({
-      to: manager.address,
-      value: ethers.utils.parseEther("1")
-    })
-    await tx.wait()
-
-    tx = await owner.sendTransaction({
-      to: nonEnabled.address,
-      value: ethers.utils.parseEther("1")
-    })
-    await tx.wait()
-  });
-
-  beforeEach("setup DS-Test contract", function () {
-    return ethers.getContractFactory("ExampleFeeManagerTest", { signer: owner })
+    return ethers.getContractFactory("ExampleFeeManagerTest", { signer })
       .then(factory => factory.deploy())
       .then(contract => {
         this.testContract = contract
         return contract.deployed().then(() => contract)
       })
       .then(contract => contract.setUp())
-      .then(tx => tx.wait())
-      .then(() => ethers.getSigner(ADMIN_ADDRESS))
-      .then(admin => ethers.getContractAt("IFeeManager", FEE_MANAGER, admin))
-      .then(feeManager => feeManager.setAdmin(this.testContract.address))
+      .then(tx => Promise.all([feeManagerPromise, tx.wait()]))
+      .then(([feeManager]) => feeManager.setAdmin(this.testContract.address))
       .then(tx => tx.wait())
   })
 
-  it("should add contract deployer as owner", async function () {
-    const contractOwnerAddr: string = await contract.owner()
-    expect(owner.address).to.be.equal(contractOwnerAddr)
-  });
+  test("should add contract deployer as owner", "addContractDeployerAsOwner");
 
-  test("* should add contract deployer as owner", "addContractDeployerAsOwner");
+  test("contract should not be able to change fee without enabled", "enableWAGMIFeesFailure"); 
 
-  it("contract should not be able to change fee without enabled", async function () {
-    let contractRole = await managerPrecompile.readAllowList(contract.address);
-    expect(contractRole).to.be.equal(ROLES.NONE)
+  test("contract should be added to manager list", "addContractToManagerList");
+
+  test("admin should be able to enable change fees", "changeFees");
+
+  test("should confirm min-fee transaction", "minFeeTransaction", {
+    maxFeePerGas: GENESIS_CONFIG.config.feeConfig.minBaseFee,
+    maxPriorityFeePerGas: 0,
+  })
+
+  it("should reject a transaction below the minimum", async function() {
+    const maxFeePerGas = GENESIS_CONFIG.config.feeConfig.minBaseFee;
+    const maxPriorityFeePerGas = 0;
+    const gasLimit = GENESIS_CONFIG.config.feeConfig.gasLimit;
+
+    await this.testContract.raiseMinFeeByOne({ maxFeePerGas }).then(tx => tx.wait());
+
     try {
-      let tx = await contract.enableWAGMIFees()
-      await tx.wait()
+      await this.testContract.lowerMinFeeByOne({ maxFeePerGas }).then(tx => tx.wait());
+    } catch (err) {
+      expect(err.toString()).to.include("max fee per gas less than block base fee");
+
+      await this.testContract.lowerMinFeeByOne({ maxFeePerGas: maxFeePerGas + 1 }).then(tx => tx.wait());
+
+      return;
     }
-    catch (err) {
-      return
-    }
-    expect.fail("should have errored")
-  })
 
-  test("* contract should not be able to change fee without enabled", "enableWAGMIFeesFailure"); 
-
-  it("contract should be added to manager list", async function () {
-    let adminRole = await ownerPrecompile.readAllowList(ADMIN_ADDRESS);
-    expect(adminRole).to.be.equal(ROLES.ADMIN)
-    let contractRole = await ownerPrecompile.readAllowList(contract.address);
-    expect(contractRole).to.be.equal(ROLES.NONE)
-
-    let enableTx = await ownerPrecompile.setEnabled(contract.address);
-    await enableTx.wait()
-    contractRole = await ownerPrecompile.readAllowList(contract.address);
-    expect(contractRole).to.be.equal(ROLES.ENABLED)
-  });
-
-  test("* contract should be added to manager list", "addContractToManagerList");
-
-  it("admin should be able to change fees through contract", async function () {
-    const testFees = {
-      gasLimit: 12_345_678, // gasLimit
-      targetBlockRate: 2, // targetBlockRate
-      minBaseFee: 1_234_567, // minBaseFee
-      targetGas: 100_000_000, // targetGas
-      baseFeeChangeDenominator: 48, // baseFeeChangeDenominator
-      minBlockGasCost: 0, // minBlockGasCost
-      maxBlockGasCost: 10_000_000, // maxBlockGasCost
-      blockGasCostStep: 100_000 // blockGasCostStep
-    }
-    var res = await contract.getCurrentFeeConfig()
-    expect(res.gasLimit).to.be.not.equal(testFees.gasLimit)
-    expect(res.minBaseFee).to.be.not.equal(testFees.minBaseFee)
-
-    let enableTx = await contract.enableCustomFees(testFees)
-    let txRes = await enableTx.wait()
-
-    var res = await contract.getCurrentFeeConfig()
-    expect(res.gasLimit).to.be.equal(testFees.gasLimit)
-    expect(res.minBaseFee).to.be.equal(testFees.minBaseFee)
-
-    var res = await contract.getFeeConfigLastChangedAt()
-    expect(res).to.be.equal(txRes.blockNumber)
-  })
-
-  test.only("admin should be able to change fees through contract", "enableCustomFees");
-
-  it("admin should be able to enable wagmi fees through contract", async function () {
-    var res = await contract.getCurrentFeeConfig()
-    expect(res.gasLimit).to.be.not.equal(WAGMI_FEES.gasLimit)
-    expect(res.minBaseFee).to.be.not.equal(WAGMI_FEES.minBaseFee)
-    // set wagmi fees now
-    let enableTx = await contract.enableWAGMIFees({
-      maxFeePerGas: WAGMI_FEES.minBaseFee * 2,
-      maxPriorityFeePerGas: WAGMI_FEES.minBaseFee
-    })
-    let txRes = await enableTx.wait()
-
-    res = await contract.getCurrentFeeConfig()
-    expect(res.gasLimit).to.be.equal(WAGMI_FEES.gasLimit)
-    expect(res.minBaseFee).to.be.equal(WAGMI_FEES.minBaseFee)
-
-    var res = await contract.getFeeConfigLastChangedAt()
-    expect(res).to.be.equal(txRes.blockNumber)
-  })
-
-  it("should let low fee tx to be in mempool", async function () {
-    var res = await contract.getCurrentFeeConfig()
-    expect(res.minBaseFee).to.be.equal(WAGMI_FEES.minBaseFee)
-
-    var testMaxFeePerGas = WAGMI_FEES.minBaseFee + 10000
-
-    let tx = await owner.sendTransaction({
-      to: manager.address,
-      value: ethers.utils.parseEther("0.1"),
-      maxFeePerGas: testMaxFeePerGas,
-      maxPriorityFeePerGas: 0
-    });
-    let confirmedTx = await tx.wait()
-    expect(confirmedTx.confirmations).to.be.greaterThanOrEqual(1)
-  })
-
-  it("should not let low fee tx to be in mempool", async function () {
-
-    let enableTx = await contract.enableCustomFees(C_FEES)
-    await enableTx.wait()
-    let getRes = await contract.getCurrentFeeConfig()
-    expect(getRes.minBaseFee).to.be.equal(C_FEES.minBaseFee)
-
-    var testMaxFeePerGas = C_FEES.minBaseFee - 100000
-
-    // send tx with lower han C_FEES minBaseFee
-    try {
-      let tx = await owner.sendTransaction({
-        to: manager.address,
-        value: ethers.utils.parseEther("0.1"),
-        maxFeePerGas: testMaxFeePerGas,
-        maxPriorityFeePerGas: 10000
-      });
-      await tx.wait()
-    }
-    catch (err) {
-      expect(err.toString()).to.include("max fee per gas less than block base fee")
-      return
-    }
-    expect.fail("should have errored")
-  })
-
-  it("should be able to get current fee config", async function () {
-    let enableTx = await contract.enableCustomFees(C_FEES,
-      {
-        maxFeePerGas: C_FEES.minBaseFee * 2,
-        maxPriorityFeePerGas: 0
-      })
-    await enableTx.wait()
-
-    var res = await contract.getCurrentFeeConfig()
-    expect(res.gasLimit).to.be.equal(C_FEES.gasLimit)
-
-    var res = await contract.connect(manager).getCurrentFeeConfig()
-    expect(res.gasLimit).to.be.equal(C_FEES.gasLimit)
-
-    var res = await contract.connect(nonEnabled).getCurrentFeeConfig()
-    expect(res.gasLimit).to.be.equal(C_FEES.gasLimit)
-  });
-
-  it("nonEnabled should not be able to set fee config", async function () {
-    let nonEnabledRole = await ownerPrecompile.readAllowList(nonEnabled.address);
-
-    expect(nonEnabledRole).to.be.equal(ROLES.NONE)
-    try {
-      await contract.connect(nonEnabled).enableWAGMIFees({
-        maxFeePerGas: C_FEES.minBaseFee * 2,
-        maxPriorityFeePerGas: 0
-      })
-    }
-    catch (err) {
-      return
-    }
-    expect.fail("should have errored")
-  })
-
-  it("manager should be able to change fees through contract", async function () {
-    let enableTx = await contract.connect(manager).enableCustomFees(WAGMI_FEES,
-      {
-        maxFeePerGas: C_FEES.minBaseFee * 2,
-        maxPriorityFeePerGas: 0
-      })
-    await enableTx.wait()
-
-    var res = await contract.connect(manager).getCurrentFeeConfig()
-    expect(res.minBaseFee).to.be.equal(WAGMI_FEES.minBaseFee)
-  })
-
-
-  it("non-enabled should not be able to change fees through contract", async function () {
-    try {
-      let enableTx = await contract.connect(nonEnabled).enableCustomFees(WAGMI_FEES,
-        {
-          maxFeePerGas: WAGMI_FEES.minBaseFee * 2,
-          maxPriorityFeePerGas: 0
-        })
-      await enableTx.wait()
-    }
-    catch (err) {
-      return
-    }
     expect.fail("should have errored")
   })
 })
