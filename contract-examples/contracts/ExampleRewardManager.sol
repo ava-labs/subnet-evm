68141f7781306ba820e0ca22756a378464f649bd
Richard Pringle <richard.pringle@avalabs.org>
2023-04-17 16:12:12 -0400

    WIP-5 RewardManager

--- contract-examples/contracts/ExampleRewardManager.sol
+++ contract-examples/contracts/ExampleRewardManager.sol
@@ -3,10 +3,13 @@ pragma solidity ^0.8.0;
 
 import "./IRewardManager.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "ds-test/src/test.sol";
+
+address constant REWARD_MANAGER_ADDRESS = 0x0200000000000000000000000000000000000004;
+address constant BLACKHOLE_ADDRESS = 0x0100000000000000000000000000000000000000;
 
 // ExampleRewardManager is a sample wrapper contract for RewardManager precompile.
 contract ExampleRewardManager is Ownable {
-  address constant REWARD_MANAGER_ADDRESS = 0x0200000000000000000000000000000000000004;
   IRewardManager rewardManager = IRewardManager(REWARD_MANAGER_ADDRESS);
 
   constructor() Ownable() {}
@@ -29,5 +32,109 @@ contract ExampleRewardManager is Ownable {
 
   function areFeeRecipientsAllowed() public view returns (bool) {
     return rewardManager.areFeeRecipientsAllowed();
+  }
+}
+
+contract ExampleRewardManagerTest is DSTest {
+  uint blackholeBalance;
+  ExampleRewardManager exampleReceiveFees;
+  uint exampleBalance;
+
+  function setUp() public {
+    blackholeBalance = BLACKHOLE_ADDRESS.balance;
+  }
+
+  function test_sendFeesToBlackhole() public {
+    blackholeBalance = BLACKHOLE_ADDRESS.balance;
+  }
+
+  function test_checkSendFeesToBlackhole() public {
+    assertGt(BLACKHOLE_ADDRESS.balance, blackholeBalance);
+  }
+
+  function test_doesNotSetRewardAddressBeforeEnabled() public {
+    ExampleRewardManager example = new ExampleRewardManager();
+    address exampleAddress = address(example);
+    IRewardManager rewardManager = IRewardManager(REWARD_MANAGER_ADDRESS);
+
+    assertEq(rewardManager.readAllowList(exampleAddress), 0);
+
+    try example.setRewardAddress(exampleAddress) {
+      assertTrue(false, "setRewardAddress should fail");
+    } catch {} // TODO should match on an error to make sure that this is failing in the way that's expected
+  }
+
+  function test_setEnabled() public {
+    ExampleRewardManager example = new ExampleRewardManager();
+    address exampleAddress = address(example);
+    IRewardManager rewardManager = IRewardManager(REWARD_MANAGER_ADDRESS);
+
+    assertEq(rewardManager.readAllowList(exampleAddress), 0);
+    rewardManager.setEnabled(exampleAddress);
+    assertEq(rewardManager.readAllowList(exampleAddress), 1);
+  }
+
+  function test_setRewardAddress() public {
+    ExampleRewardManager example = new ExampleRewardManager();
+    address exampleAddress = address(example);
+    IRewardManager rewardManager = IRewardManager(REWARD_MANAGER_ADDRESS);
+
+    rewardManager.setEnabled(exampleAddress);
+    example.setRewardAddress(exampleAddress);
+
+    assertEq(example.currentRewardAddress(), exampleAddress);
+  }
+
+  function test_setupReceiveFees() public {
+    ExampleRewardManager example = new ExampleRewardManager();
+    address exampleAddress = address(example);
+
+    IRewardManager rewardManager = IRewardManager(REWARD_MANAGER_ADDRESS);
+
+    rewardManager.setEnabled(exampleAddress);
+    example.setRewardAddress(exampleAddress);
+
+    exampleReceiveFees = example;
+    exampleBalance = exampleAddress.balance;
+  }
+
+  function test_receiveFees() public {
+    // noop
+  }
+
+  function test_checkReceiveFees() public {
+    assertGt(address(exampleReceiveFees).balance, exampleBalance);
+  }
+
+  function test_areFeeRecipientsAllowed() public {
+    ExampleRewardManager example = new ExampleRewardManager();
+    assertTrue(!example.areFeeRecipientsAllowed());
+  }
+
+  function test_allowFeeRecipients() public {
+    ExampleRewardManager example = new ExampleRewardManager();
+    address exampleAddress = address(example);
+
+    IRewardManager rewardManager = IRewardManager(REWARD_MANAGER_ADDRESS);
+    rewardManager.setEnabled(exampleAddress);
+
+    example.allowFeeRecipients();
+    assertTrue(example.areFeeRecipientsAllowed());
+  }
+
+  function test_disableRewardAddress() public {
+    ExampleRewardManager example = new ExampleRewardManager();
+    address exampleAddress = address(example);
+
+    IRewardManager rewardManager = IRewardManager(REWARD_MANAGER_ADDRESS);
+    rewardManager.setEnabled(exampleAddress);
+
+    example.setRewardAddress(exampleAddress);
+
+    assertEq(example.currentRewardAddress(), exampleAddress);
+
+    example.disableRewards();
+
+    assertEq(example.currentRewardAddress(), BLACKHOLE_ADDRESS);
   }
 }
--- contract-examples/test/reward_manager.ts
+++ contract-examples/test/reward_manager.ts
@@ -10,9 +10,10 @@ import {
 } from "ethers"
 import { ethers } from "hardhat"
 import ts = require("typescript");
+const assert = require("assert");
 
 // make sure this is always an admin for reward manager precompile
-const adminAddress: string = "0x8db97C7cEcE249c2b98bDC0226Cc4C2A57BF52FC"
+const ADMIN_ADDRESS = "0x8db97C7cEcE249c2b98bDC0226Cc4C2A57BF52FC"
 const REWARD_MANAGER_ADDRESS = "0x0200000000000000000000000000000000000004";
 const BLACKHOLE_ADDRESS = "0x0100000000000000000000000000000000000000";
 
@@ -22,6 +23,36 @@ const ROLES = {
   ADMIN: 2
 };
 
+const testFn = (fnNameOrNames: string | string[], overrides = {}, debug = false) => async function () {
+  const fnNames: string[] = Array.isArray(fnNameOrNames) ? fnNameOrNames : [fnNameOrNames];
+
+  return fnNames.reduce((p: Promise<undefined>, fnName) => p.then(async () => {
+    const tx = await this.testContract['test_' + fnName](overrides)
+    const txReceipt = await tx.wait().catch(err => err.receipt)
+
+    const failed = txReceipt.status !== 0 ? await this.testContract.callStatic.failed() : true
+    
+    if (debug || failed) {
+      console.log('')
+
+      if (!txReceipt.events) console.warn(txReceipt);
+
+      txReceipt
+        .events
+        ?.filter(event => debug || event.event?.startsWith('log'))
+        .map(event => event.args?.forEach(arg => console.log(arg)))
+
+      console.log('')
+    }
+
+    assert(!failed, `${fnName} failed`)
+  }), Promise.resolve());
+}
+
+const test = (name, fnName, overrides = {}) => it(name, testFn(fnName, overrides));
+test.only = (name, fnName, overrides = {}) => it.only(name, testFn(fnName, overrides));
+test.debug = (name, fnName, overrides = {}) => it.only(name, testFn(fnName, overrides, true));
+
 describe("ExampleRewardManager", function () {
   this.timeout("30s")
   let owner: SignerWithAddress
@@ -30,137 +61,63 @@ describe("ExampleRewardManager", function () {
   let signer2: SignerWithAddress
   let precompile: Contract
 
-  before(async function () {
-    owner = await ethers.getSigner(adminAddress);
-    signer1 = (await ethers.getSigners())[1]
-    signer2 = (await ethers.getSigners())[2]
-    const Contract: ContractFactory = await ethers.getContractFactory("ExampleRewardManager", { signer: owner })
-    contract = await Contract.deploy()
-    await contract.deployed()
-    const contractAddress: string = contract.address
-    console.log(`Contract deployed to: ${contractAddress}`)
+  beforeEach('Setup DS-Test contract', async function () {
+    const signer = await ethers.getSigner(ADMIN_ADDRESS)
+    const rewardManagerPromise = ethers.getContractAt("IRewardManager", REWARD_MANAGER_ADDRESS, signer);
 
-    precompile = await ethers.getContractAt("IRewardManager", REWARD_MANAGER_ADDRESS, owner);
+    return ethers.getContractFactory("ExampleRewardManagerTest", { signer })
+      .then(factory => factory.deploy())
+      .then(contract => {
+        this.testContract = contract
+        return contract.deployed().then(() => contract)
+      })
+      .then(contract => contract.setUp())
+      .then(tx => Promise.all([rewardManagerPromise, tx.wait()]))
+      .then(([rewardManager]) => rewardManager.setAdmin(this.testContract.address))
+      .then(tx => tx.wait())
+  })
 
-    // Send a transaction to mine a new block
-    const tx = await owner.sendTransaction({
-      to: signer1.address,
-      value: ethers.utils.parseEther("10")
-    })
-    await tx.wait()
-  });
+  it.skip("should add contract deployer as owner", async function () {});
 
-  it("should add contract deployer as owner", async function () {
-    const contractOwnerAddr: string = await contract.owner()
-    expect(owner.address).to.equal(contractOwnerAddr)
-  });
+  test("should send fees to blackhole", ["sendFeesToBlackhole", "checkSendFeesToBlackhole"])
 
   // this contract is not selected as the reward address yet, so should not be able to receive fees
-  it("should send fees to blackhole", async function () {
-    let rewardAddress = await contract.currentRewardAddress();
-    expect(rewardAddress).to.be.equal(BLACKHOLE_ADDRESS)
+  it.skip("should send fees to blackhole", async function () {})
 
-    let firstBHBalance = await ethers.provider.getBalance(BLACKHOLE_ADDRESS)
+  test("should not appoint reward address before enabled", "doesNotSetRewardAddressBeforeEnabled")
 
-    // Send a transaction to mine a new block
-    const tx = await owner.sendTransaction({
-      to: signer1.address,
-      value: ethers.utils.parseEther("0.0001")
-    })
-    await tx.wait()
+  it.skip("should not appoint reward address before enabled", async function () {});
 
-    let secondBHBalance = await ethers.provider.getBalance(BLACKHOLE_ADDRESS)
-    expect(secondBHBalance.gt(firstBHBalance)).to.be.true
-  })
+  test("contract should be added to enabled list", "setEnabled")
 
-  it("should not appoint reward address before enabled", async function () {
-    let contractRole = await precompile.readAllowList(contract.address);
-    expect(contractRole).to.be.equal(ROLES.NONE)
-    try {
-      let tx = await contract.setRewardAddress(signer1.address);
-      await tx.wait()
-    }
-    catch (err) {
-      return
-    }
-    expect.fail("should have errored")
-  });
+  it.skip("contract should be added to enabled list", async function () {});
 
+  test("should be appointed as reward address", "setRewardAddress")
 
-  it("contract should be added to enabled list", async function () {
-    let contractRole = await precompile.readAllowList(contract.address);
-    expect(contractRole).to.be.equal(ROLES.NONE)
+  it.skip("should be appointed as reward address", async function () {});
 
-    let enableTx = await precompile.setEnabled(contract.address);
-    await enableTx.wait()
-    contractRole = await precompile.readAllowList(contract.address);
-    expect(contractRole).to.be.equal(ROLES.ENABLED)
-  });
+  // we need to change the fee receiver, send a transaction for the new receiver to receive fees, then check the balance change. 
+  // the new fee receiver won't receive fees in the same block where it was set.
+  test("should be able to receive fees", ["setupReceiveFees", "receiveFees", "checkReceiveFees"])
 
+  // I don't think it's necessary to test with an EOA since the logic is the same
+  it.skip("should be able to receive fees", async function () {})
 
-  it("should be appointed as reward address", async function () {
-    let tx = await contract.setRewardAddress(contract.address);
-    await tx.wait()
-    let rewardAddress = await contract.currentRewardAddress();
-    expect(rewardAddress).to.be.equal(contract.address)
-  });
+  // I don't think it's necessary to test with an EOA since the logic is the same
+  it.skip("signer1 should be appointed as reward address", async function () {});
 
-  it("should be able to receive fees", async function () {
-    let previousBalance = await ethers.provider.getBalance(contract.address)
+  // I don't think it's necessary to test with an EOA since the logic is the same
+  it.skip("signer1 should be able to receive fees", async function () {})
 
-    // Send a transaction to mine a new block
-    const tx = await owner.sendTransaction({
-      to: signer1.address,
-      value: ethers.utils.parseEther("0.0001")
-    })
-    await tx.wait()
+  test("should return false for allowFeeRecipients check", "areFeeRecipientsAllowed")
 
-    let balance = await ethers.provider.getBalance(contract.address)
-    expect(balance.gt(previousBalance)).to.be.true
-  })
+  it.skip("should return false for allowFeeRecipients check", async function () {})
 
-  it("signer1 should be appointed as reward address", async function () {
-    let tx = await contract.setRewardAddress(signer1.address);
-    await tx.wait()
-    let rewardAddress = await contract.currentRewardAddress();
-    expect(rewardAddress).to.be.equal(signer1.address)
-  });
+  test("should enable allowFeeRecipients", "allowFeeRecipients")
 
-  it("signer1 should be able to receive fees", async function () {
-    let previousBalance = await ethers.provider.getBalance(signer1.address)
+  it.skip("should enable allowFeeRecipients", async function () {})
 
-    // Send a transaction to mine a new block
-    const tx = await owner.sendTransaction({
-      to: signer2.address,
-      value: ethers.utils.parseEther("0.0001")
-    })
-    await tx.wait()
+  test("should disable reward address", "disableRewardAddress")
 
-    let balance = await ethers.provider.getBalance(signer1.address)
-    expect(balance.gt(previousBalance)).to.be.true
-  })
-
-
-  it("should return false for allowFeeRecipients check", async function () {
-    let res = await contract.areFeeRecipientsAllowed();
-    expect(res).to.be.false
-  })
-
-  it("should enable allowFeeRecipients", async function () {
-    let tx = await contract.allowFeeRecipients();
-    await tx.wait()
-    let res = await contract.areFeeRecipientsAllowed();
-    expect(res).to.be.true
-  })
-
-  it("should disable reward address", async function () {
-    let tx = await contract.disableRewards();
-    await tx.wait()
-
-    let rewardAddress = await contract.currentRewardAddress();
-    expect(rewardAddress).to.be.equal(BLACKHOLE_ADDRESS)
-
-    let res = await contract.areFeeRecipientsAllowed();
-    expect(res).to.be.false
-  })
+  it.skip("should disable reward address", async function () {})
 });
