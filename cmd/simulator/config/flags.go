a79ae4da633a21e162c0404d9065afb93ba12358
Aaron Buchwald <aaron.buchwald56@gmail.com>
2023-04-21 11:10:23 -0400

    Remove . from simulator flags

--- cmd/simulator/config/flags.go
+++ cmd/simulator/config/flags.go
@@ -104,14 +104,14 @@ func BuildFlagSet() *pflag.FlagSet {
 }
 
 func addSimulatorFlags(fs *pflag.FlagSet) {
-	fs.Bool(VersionKey, false, "Print the version and exit.")
+	fs.Bool(VersionKey, false, "Print the version and exit")
 	fs.String(ConfigFilePathKey, "", "Specify the config path to use to load a YAML config for the simulator")
 	fs.StringSlice(EndpointsKey, []string{"ws://127.0.0.1:9650/ext/bc/C/ws"}, "Specify a comma separated list of RPC Websocket Endpoints (minimum of 1 endpoint)")
 	fs.Int64(MaxFeeCapKey, 50, "Specify the maximum fee cap to use for transactions denominated in GWei (must be > 0)")
 	fs.Int64(MaxTipCapKey, 1, "Specify the max tip cap for transactions denominated in GWei (must be >= 0)")
 	fs.Uint64(TxsPerWorkerKey, 100, "Specify the number of transactions to create per worker (must be > 0)")
-	fs.Int(WorkersKey, 1, "Specify the number of workers to create for the simulator (must be > 0).")
+	fs.Int(WorkersKey, 1, "Specify the number of workers to create for the simulator (must be > 0)")
 	fs.String(KeyDirKey, ".simulator/keys", "Specify the directory to save private keys in (INSECURE: only use for testing)")
-	fs.Duration(TimeoutKey, 5*time.Minute, "Specify the timeout for the simulator to complete (0 indicates no timeout).")
-	fs.String(LogLevelKey, "info", "Specify the log level to use in the simulator.")
+	fs.Duration(TimeoutKey, 5*time.Minute, "Specify the timeout for the simulator to complete (0 indicates no timeout)")
+	fs.String(LogLevelKey, "info", "Specify the log level to use in the simulator")
 }
