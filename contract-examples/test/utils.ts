459f820bf9a7782d0088d47dfc8fb0829b75ad4e
Richard Pringle <richard.pringle@avalabs.org>
2023-04-24 15:32:38 -0400

    WIP-10

--- contract-examples/test/utils.ts
+++ contract-examples/test/utils.ts
@@ -1,30 +1,31 @@
 const assert = require("assert")
 
-const testFn = (fnNameOrNames: string | string[], overrides = {}, debug = false) => async function () {
+const testFn = (fnNameOrNames: string | string[], overrides = {}, debug = false) => {
   const fnNames: string[] = Array.isArray(fnNameOrNames) ? fnNameOrNames : [fnNameOrNames]
-  assert(fnNames.every(name => name.startsWith('test_')))
+  assert(fnNames.every(name => name.startsWith('test_')), "Solidity test functions must be prefixed with 'test_'")
 
-  return fnNames.reduce((p: Promise<undefined>, fnName) => p.then(async () => {
-    const tx = await this.testContract[fnName](overrides)
-    const txReceipt = await tx.wait().catch(err => err.receipt)
+  return async function() {
+    return fnNames.reduce((p: Promise<undefined>, fnName) => p.then(async () => {
+      const tx = await this.testContract[fnName](overrides)
+      const txReceipt = await tx.wait().catch(err => err.receipt)
 
-    const failed = txReceipt.status !== 0 ? await this.testContract.callStatic.failed() : true
-    
-    if (debug || failed) {
-      console.log('')
+      const failed = txReceipt.status !== 0 ? await this.testContract.callStatic.failed() : true 
+      if (debug || failed) {
+        console.log('')
 
-      if (!txReceipt.events) console.warn('WARNING: No parseable events found in tx-receipt\n')
+        if (!txReceipt.events) console.warn('WARNING: No parseable events found in tx-receipt\n')
 
-      txReceipt
-        .events
-        ?.filter(event => debug || event.event?.startsWith('log'))
-        .map(event => event.args?.forEach(arg => console.log(arg)))
+        txReceipt
+          .events
+          ?.filter(event => debug || event.event?.startsWith('log'))
+          .map(event => event.args?.forEach(arg => console.log(arg)))
 
-      console.log('')
-    }
+        console.log('')
+      }
 
-    assert(!failed, `${fnName} failed`)
-  }), Promise.resolve());
+      assert(!failed, `${fnName} failed`, )
+    }), Promise.resolve())
+  }
 }
 
 export const test = (name, fnName, overrides = {}) => it(name, testFn(fnName, overrides));
