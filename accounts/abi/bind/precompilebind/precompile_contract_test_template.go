b81fdfb823b9c1a14266f8c09d575a410db2785c
Ceyhun Onur <ceyhun.onur@avalabs.org>
2023-04-28 20:11:03 +0300

    Update accounts/abi/bind/precompilebind/precompile_contract_test_template.go
    Co-authored-by: Darioush Jalali <darioush.jalali@avalabs.org>

--- accounts/abi/bind/precompilebind/precompile_contract_test_template.go
+++ accounts/abi/bind/precompilebind/precompile_contract_test_template.go
@@ -27,7 +27,7 @@ import (
 // These tests are run against the precompile contract directly with
 // the given input and expected output. They're just a guide to
 // help you write your own tests. These tests are for general cases like
-// allowlist, readOnly behaviour and gas cost. You should write your own
+// allowlist, readOnly behaviour, and gas cost. You should write your own
 // tests for specific cases.
 func TestRun(t *testing.T) {
 	tests := map[string]testutils.PrecompileTest{
