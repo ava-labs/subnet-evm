1e1044e2b82838c249a9954934330dc30be71a57
Richard Pringle <richard.pringle@avalabs.org>
2023-05-16 11:50:29 -0400

    Move ExampleRewardManagerTest

--- contract-examples/contracts/ExampleRewardManager.sol
+++ contract-examples/contracts/ExampleRewardManager.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.0;
 
 import "./IRewardManager.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "./test/AllowList.sol";
 
 address constant REWARD_MANAGER_ADDRESS = 0x0200000000000000000000000000000000000004;
 address constant BLACKHOLE_ADDRESS = 0x0100000000000000000000000000000000000000;
@@ -32,109 +31,5 @@ contract ExampleRewardManager is Ownable {
 
   function areFeeRecipientsAllowed() public view returns (bool) {
     return rewardManager.areFeeRecipientsAllowed();
-  }
-}
-
-contract ExampleRewardManagerTest is AllowListTest {
-  uint blackholeBalance;
-  ExampleRewardManager exampleReceiveFees;
-  uint exampleBalance;
-
-  function setUp() public {
-    blackholeBalance = BLACKHOLE_ADDRESS.balance;
-  }
-
-  function step_captureBlackholeBalance() public {
-    blackholeBalance = BLACKHOLE_ADDRESS.balance;
-  }
-
-  function step_checkSendFeesToBlackhole() public {
-    assertGt(BLACKHOLE_ADDRESS.balance, blackholeBalance);
-  }
-
-  function step_doesNotSetRewardAddressBeforeEnabled() public {
-    ExampleRewardManager example = new ExampleRewardManager();
-    address exampleAddress = address(example);
-    IRewardManager rewardManager = IRewardManager(REWARD_MANAGER_ADDRESS);
-
-    assertRole(rewardManager.readAllowList(exampleAddress), AllowList.Role.None);
-
-    try example.setRewardAddress(exampleAddress) {
-      assertTrue(false, "setRewardAddress should fail");
-    } catch {} // TODO should match on an error to make sure that this is failing in the way that's expected
-  }
-
-  function step_setEnabled() public {
-    ExampleRewardManager example = new ExampleRewardManager();
-    address exampleAddress = address(example);
-    IRewardManager rewardManager = IRewardManager(REWARD_MANAGER_ADDRESS);
-
-    assertRole(rewardManager.readAllowList(exampleAddress), AllowList.Role.None);
-    rewardManager.setEnabled(exampleAddress);
-    assertRole(rewardManager.readAllowList(exampleAddress), AllowList.Role.Enabled);
-  }
-
-  function step_setRewardAddress() public {
-    ExampleRewardManager example = new ExampleRewardManager();
-    address exampleAddress = address(example);
-    IRewardManager rewardManager = IRewardManager(REWARD_MANAGER_ADDRESS);
-
-    rewardManager.setEnabled(exampleAddress);
-    example.setRewardAddress(exampleAddress);
-
-    assertEq(example.currentRewardAddress(), exampleAddress);
-  }
-
-  function step_setupReceiveFees() public {
-    ExampleRewardManager example = new ExampleRewardManager();
-    address exampleAddress = address(example);
-
-    IRewardManager rewardManager = IRewardManager(REWARD_MANAGER_ADDRESS);
-
-    rewardManager.setEnabled(exampleAddress);
-    example.setRewardAddress(exampleAddress);
-
-    exampleReceiveFees = example;
-    exampleBalance = exampleAddress.balance;
-  }
-
-  function step_receiveFees() public {
-    // used as a noop to test if the correct address receives fees
-  }
-
-  function step_checkReceiveFees() public {
-    assertGt(address(exampleReceiveFees).balance, exampleBalance);
-  }
-
-  function step_areFeeRecipientsAllowed() public {
-    ExampleRewardManager example = new ExampleRewardManager();
-    assertTrue(!example.areFeeRecipientsAllowed());
-  }
-
-  function step_allowFeeRecipients() public {
-    ExampleRewardManager example = new ExampleRewardManager();
-    address exampleAddress = address(example);
-
-    IRewardManager rewardManager = IRewardManager(REWARD_MANAGER_ADDRESS);
-    rewardManager.setEnabled(exampleAddress);
-
-    example.allowFeeRecipients();
-    assertTrue(example.areFeeRecipientsAllowed());
-  }
-
-  function step_disableRewardAddress() public {
-    ExampleRewardManager example = new ExampleRewardManager();
-    address exampleAddress = address(example);
-
-    IRewardManager rewardManager = IRewardManager(REWARD_MANAGER_ADDRESS);
-    rewardManager.setEnabled(exampleAddress);
-
-    example.setRewardAddress(exampleAddress);
-
-    assertEq(example.currentRewardAddress(), exampleAddress);
-
-    example.disableRewards();
-
-    assertEq(example.currentRewardAddress(), BLACKHOLE_ADDRESS);
   }
 }
---
+++ contract-examples/contracts/test/ExampleRewardManager.sol
@@ -0,0 +1,110 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "../ExampleRewardManager.sol";
+import "../IRewardManager.sol";
+import "./AllowList.sol";
+
+contract ExampleRewardManagerTest is AllowListTest {
+  uint blackholeBalance;
+  ExampleRewardManager exampleReceiveFees;
+  uint exampleBalance;
+
+  function setUp() public {
+    blackholeBalance = BLACKHOLE_ADDRESS.balance;
+  }
+
+  function step_captureBlackholeBalance() public {
+    blackholeBalance = BLACKHOLE_ADDRESS.balance;
+  }
+
+  function step_checkSendFeesToBlackhole() public {
+    assertGt(BLACKHOLE_ADDRESS.balance, blackholeBalance);
+  }
+
+  function step_doesNotSetRewardAddressBeforeEnabled() public {
+    ExampleRewardManager example = new ExampleRewardManager();
+    address exampleAddress = address(example);
+    IRewardManager rewardManager = IRewardManager(REWARD_MANAGER_ADDRESS);
+
+    assertRole(rewardManager.readAllowList(exampleAddress), AllowList.Role.None);
+
+    try example.setRewardAddress(exampleAddress) {
+      assertTrue(false, "setRewardAddress should fail");
+    } catch {} // TODO should match on an error to make sure that this is failing in the way that's expected
+  }
+
+  function step_setEnabled() public {
+    ExampleRewardManager example = new ExampleRewardManager();
+    address exampleAddress = address(example);
+    IRewardManager rewardManager = IRewardManager(REWARD_MANAGER_ADDRESS);
+
+    assertRole(rewardManager.readAllowList(exampleAddress), AllowList.Role.None);
+    rewardManager.setEnabled(exampleAddress);
+    assertRole(rewardManager.readAllowList(exampleAddress), AllowList.Role.Enabled);
+  }
+
+  function step_setRewardAddress() public {
+    ExampleRewardManager example = new ExampleRewardManager();
+    address exampleAddress = address(example);
+    IRewardManager rewardManager = IRewardManager(REWARD_MANAGER_ADDRESS);
+
+    rewardManager.setEnabled(exampleAddress);
+    example.setRewardAddress(exampleAddress);
+
+    assertEq(example.currentRewardAddress(), exampleAddress);
+  }
+
+  function step_setupReceiveFees() public {
+    ExampleRewardManager example = new ExampleRewardManager();
+    address exampleAddress = address(example);
+
+    IRewardManager rewardManager = IRewardManager(REWARD_MANAGER_ADDRESS);
+
+    rewardManager.setEnabled(exampleAddress);
+    example.setRewardAddress(exampleAddress);
+
+    exampleReceiveFees = example;
+    exampleBalance = exampleAddress.balance;
+  }
+
+  function step_receiveFees() public {
+    // used as a noop to test if the correct address receives fees
+  }
+
+  function step_checkReceiveFees() public {
+    assertGt(address(exampleReceiveFees).balance, exampleBalance);
+  }
+
+  function step_areFeeRecipientsAllowed() public {
+    ExampleRewardManager example = new ExampleRewardManager();
+    assertTrue(!example.areFeeRecipientsAllowed());
+  }
+
+  function step_allowFeeRecipients() public {
+    ExampleRewardManager example = new ExampleRewardManager();
+    address exampleAddress = address(example);
+
+    IRewardManager rewardManager = IRewardManager(REWARD_MANAGER_ADDRESS);
+    rewardManager.setEnabled(exampleAddress);
+
+    example.allowFeeRecipients();
+    assertTrue(example.areFeeRecipientsAllowed());
+  }
+
+  function step_disableRewardAddress() public {
+    ExampleRewardManager example = new ExampleRewardManager();
+    address exampleAddress = address(example);
+
+    IRewardManager rewardManager = IRewardManager(REWARD_MANAGER_ADDRESS);
+    rewardManager.setEnabled(exampleAddress);
+
+    example.setRewardAddress(exampleAddress);
+
+    assertEq(example.currentRewardAddress(), exampleAddress);
+
+    example.disableRewards();
+
+    assertEq(example.currentRewardAddress(), BLACKHOLE_ADDRESS);
+  }
+}
