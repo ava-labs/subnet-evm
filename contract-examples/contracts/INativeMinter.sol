baed12834b8998ec376f5ec2b0e1e285b604d7dc
Ceyhun Onur <ceyhun.onur@avalabs.org>
2023-06-14 20:49:36 +0300

    deleting contract examples folder

--- contract-examples/contracts/INativeMinter.sol
+++
@@ -1,8 +0,0 @@
-//SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-import "./IAllowList.sol";
-
-interface INativeMinter is IAllowList {
-  // Mint [amount] number of native coins and send to [addr]
-  function mintNativeCoin(address addr, uint256 amount) external;
-}
