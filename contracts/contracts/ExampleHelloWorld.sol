a6e843376e28c24d1eac1c20940306d301d8f97a
Ceyhun Onur <ceyhun.onur@avalabs.org>
2023-06-16 22:21:02 +0300

    move constant

--- contracts/contracts/ExampleHelloWorld.sol
+++ contracts/contracts/ExampleHelloWorld.sol
@@ -3,9 +3,10 @@ pragma solidity ^0.8.0;
 
 import "./interfaces/IHelloWorld.sol";
 
+address constant HELLO_WORLD_ADDRESS = 0x0300000000000000000000000000000000000000;
+
 // ExampleHelloWorld shows how the HelloWorld precompile can be used in a smart contract.
 contract ExampleHelloWorld {
-  address constant HELLO_WORLD_ADDRESS = 0x0300000000000000000000000000000000000000;
   IHelloWorld helloWorld = IHelloWorld(HELLO_WORLD_ADDRESS);
 
   function sayHello() public view returns (string memory) {
