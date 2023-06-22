c6460df136f1fcd6b881a5879e3aa4266999869b
Aaron Buchwald <aaron.buchwald56@gmail.com>
2023-06-22 11:53:16 -0400

    Add new docker image to containerize subnet-evm

--- scripts/build_image.sh
+++ scripts/build_image.sh
@@ -13,12 +13,7 @@ source "$SUBNET_EVM_PATH"/scripts/versions.sh
 # Load the constants
 source "$SUBNET_EVM_PATH"/scripts/constants.sh
 
-echo "Building Docker Image: $DOCKERHUB_REPO:$BUILD_IMAGE_ID based of $AVALANCHEGO_VERSION"
-docker build -t "$DOCKERHUB_REPO:$BUILD_IMAGE_ID" "$SUBNET_EVM_PATH" -f "$SUBNET_EVM_PATH/Dockerfile" \
-  --build-arg AVALANCHE_VERSION="$AVALANCHEGO_VERSION" \
+echo "Building Docker Image: $DOCKERHUB_REPO:$BUILD_IMAGE_ID"
+docker build -t "$DOCKERHUB_REPO:$BUILD_IMAGE_ID" "$SUBNET_EVM_PATH" -f "$SUBNET_EVM_PATH/Dockerfile.container" \
   --build-arg SUBNET_EVM_COMMIT="$SUBNET_EVM_COMMIT" \
   --build-arg CURRENT_BRANCH="$CURRENT_BRANCH"
-
-if [[ ${PUSH_DOCKER_IMAGE:-""} == "true" ]]; then
-  docker push $DOCKERHUB_REPO:$BUILD_IMAGE_ID
-fi
