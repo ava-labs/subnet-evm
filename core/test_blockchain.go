136d4b513777d521fdfb4dfedf3f121c79f06d5f
najeal <haim.nathan@icloud.com>
2023-07-02 15:57:04 +0200

    core/test_blockchain.go: revert / fix comment

--- core/test_blockchain.go
+++ core/test_blockchain.go
@@ -1003,20 +1003,19 @@ func TestReorgReInsert(t *testing.T, create func(db ethdb.Database, chainConfig 
 	checkBlockChainState(t, blockchain, gspec, chainDB, create, checkState)
 }
 
+//nolint:goimports
 // Insert two different chains that result in the identical state root.
 // Once we accept one of the chains, we insert and accept A3 on top of the shared
 // state root
 //
-//	 G   (genesis)
-//	/ \
+//   G   (genesis)
+//  / \
 //
 // A1  B1
 // |   |
 // A2  B2 (A2 and B2 represent two different paths to the identical state trie)
 // |
 // A3
-//
-//nolint:goimports
 func TestAcceptBlockIdenticalStateRoot(t *testing.T, create func(db ethdb.Database, chainConfig *params.ChainConfig, lastAcceptedHash common.Hash) (*BlockChain, error)) {
 	var (
 		key1, _ = crypto.HexToECDSA("b71c71a67e1177ad4e901695e1b4b9ee17ae16c6668d313eac2f96dbcda3f291")
@@ -1153,21 +1152,20 @@ func TestAcceptBlockIdenticalStateRoot(t *testing.T, create func(db ethdb.Databa
 	checkBlockChainState(t, blockchain, gspec, chainDB, create, checkState)
 }
 
+//nolint:goimports
 // Insert two different chains that result in the identical state root.
 // Once we insert both of the chains, we restart, insert both the chains again,
 // and then we accept one of the chains and accept A3 on top of the shared state
 // root
 //
-//	 G   (genesis)
-//	/ \
+//   G   (genesis)
+//  / \
 //
 // A1  B1
 // |   |
 // A2  B2 (A2 and B2 represent two different paths to the identical state trie)
 // |
 // A3
-//
-//nolint:goimports
 func TestReprocessAcceptBlockIdenticalStateRoot(t *testing.T, create func(db ethdb.Database, chainConfig *params.ChainConfig, lastAcceptedHash common.Hash) (*BlockChain, error)) {
 	var (
 		key1, _ = crypto.HexToECDSA("b71c71a67e1177ad4e901695e1b4b9ee17ae16c6668d313eac2f96dbcda3f291")
