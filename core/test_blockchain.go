1e3f403a6c5d0972f8245971c48035a54db93ba9
Ceyhun Onur <ceyhun.onur@avalabs.org>
2023-07-10 19:20:46 +0300

    uncomment tests

--- core/test_blockchain.go
+++ core/test_blockchain.go
@@ -35,54 +35,54 @@ type ChainTest struct {
 }
 
 var tests = []ChainTest{
-	// {
-	// 	"InsertChainAcceptSingleBlock",
-	// 	TestInsertChainAcceptSingleBlock,
-	// },
-	// {
-	// 	"InsertForkedChain",
-	// 	TestInsertLongForkedChain,
-	// },
-	// {
-	// 	"AcceptNonCanonicalBlock",
-	// 	TestAcceptNonCanonicalBlock,
-	// },
-	// {
-	// 	"SetPreferenceRewind",
-	// 	TestSetPreferenceRewind,
-	// },
-	// {
-	// 	"BuildOnVariousStages",
-	// 	TestBuildOnVariousStages,
-	// },
+	{
+		"InsertChainAcceptSingleBlock",
+		TestInsertChainAcceptSingleBlock,
+	},
+	{
+		"InsertForkedChain",
+		TestInsertLongForkedChain,
+	},
+	{
+		"AcceptNonCanonicalBlock",
+		TestAcceptNonCanonicalBlock,
+	},
+	{
+		"SetPreferenceRewind",
+		TestSetPreferenceRewind,
+	},
+	{
+		"BuildOnVariousStages",
+		TestBuildOnVariousStages,
+	},
 	{
 		"EmptyBlocks",
 		TestEmptyBlocks,
 	},
-	// {
-	// 	"AcceptBlockIdenticalStateRoot",
-	// 	TestAcceptBlockIdenticalStateRoot,
-	// },
-	// {
-	// 	"ReprocessAcceptBlockIdenticalStateRoot",
-	// 	TestReprocessAcceptBlockIdenticalStateRoot,
-	// },
-	// {
-	// 	"GenerateChainInvalidBlockFee",
-	// 	TestGenerateChainInvalidBlockFee,
-	// },
-	// {
-	// 	"InsertChainInvalidBlockFee",
-	// 	TestInsertChainInvalidBlockFee,
-	// },
-	// {
-	// 	"InsertChainValidBlockFee",
-	// 	TestInsertChainValidBlockFee,
-	// },
-	// {
-	// 	"TestStatefulPrecompiles",
-	// 	TestStatefulPrecompiles,
-	// },
+	{
+		"AcceptBlockIdenticalStateRoot",
+		TestAcceptBlockIdenticalStateRoot,
+	},
+	{
+		"ReprocessAcceptBlockIdenticalStateRoot",
+		TestReprocessAcceptBlockIdenticalStateRoot,
+	},
+	{
+		"GenerateChainInvalidBlockFee",
+		TestGenerateChainInvalidBlockFee,
+	},
+	{
+		"InsertChainInvalidBlockFee",
+		TestInsertChainInvalidBlockFee,
+	},
+	{
+		"InsertChainValidBlockFee",
+		TestInsertChainValidBlockFee,
+	},
+	{
+		"TestStatefulPrecompiles",
+		TestStatefulPrecompiles,
+	},
 }
 
 func copyMemDB(db ethdb.Database) (ethdb.Database, error) {
