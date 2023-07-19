14e1d55ec443f6287cefc965beb4b2ff71e0eaba
Anusha <anushachillara@gmail.com>
2023-07-19 12:34:33 -0700

    ws

--- tests/load/load_test.go
+++ tests/load/load_test.go
@@ -33,7 +33,6 @@ var _ = ginkgo.Describe("[Load Simulator]", ginkgo.Ordered, func() {
 		blockchainID := subnetDetails.BlockchainID
 
 		nodeURIs := subnetDetails.ValidatorURIs
-
 		rpcEndpoints := make([]string, 0, len(nodeURIs))
 		for _, uri := range nodeURIs {
 			rpcEndpoints = append(rpcEndpoints, fmt.Sprintf("%s/ext/bc/%s/rpc", uri, blockchainID))
