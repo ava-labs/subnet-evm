19567d8d7ce8f48eedeef3b1682250e8b7281446
Aaron Buchwald <aaron.buchwald56@gmail.com>
2023-07-27 10:09:50 -0400

    Move IWarpMessenger.sol to interfaces/

--- contracts/contracts/ExampleWarp.sol
+++ contracts/contracts/ExampleWarp.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.0;
 pragma experimental ABIEncoderV2;
 
-import "./IWarpMessenger.sol";
+import "./interfaces/IWarpMessenger.sol";
 
 contract ExampleWarp {
     address constant WARP_ADDRESS = 0x0200000000000000000000000000000000000005;
@@ -11,7 +11,7 @@ contract ExampleWarp {
     // sendWarpMessage sends a warp message to the specified destination chain and address pair containing the payload
     function sendWarpMessage(
         bytes32 destinationChainID,
-        bytes32 destinationAddress,
+        address destinationAddress,
         bytes calldata payload
     ) external {
         warp.sendWarpMessage(destinationChainID, destinationAddress, payload);
@@ -21,9 +21,9 @@ contract ExampleWarp {
     // validateWarpMessage retrieves the warp message attached to the transaction and verifies all of its attributes.
     function validateWarpMessage(
         bytes32 originChainID,
-        bytes32 originSenderAddress,
+        address originSenderAddress,
         bytes32 destinationChainID,
-        bytes32 destinationAddress,
+        address destinationAddress,
         bytes calldata payload
     ) external view {
         (WarpMessage memory message, bool exists) = warp.getVerifiedWarpMessage();
