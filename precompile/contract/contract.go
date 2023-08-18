048c41c7cfed38090b47b4c2bba7698b900103c6
Aaron Buchwald <aaron.buchwald56@gmail.com>
2023-08-18 19:18:26 -0400

    inline formatted error for non-existent function selector

--- precompile/contract/contract.go
+++ precompile/contract/contract.go
@@ -98,12 +98,12 @@ func (s *statefulPrecompileWithFunctionSelectors) Run(accessibleState Accessible
 	functionInput := input[SelectorLen:]
 	function, ok := s.functions[string(selector)]
 	if !ok {
-		return nil, suppliedGas, InvalidFunctionErr(selector)
+		return nil, suppliedGas, fmt.Errorf("invalid function selector %#x", selector)
 	}
 
 	// Check if the function is activated
 	if !function.IsActivated(accessibleState) {
-		return nil, suppliedGas, InvalidFunctionErr(selector)
+		return nil, suppliedGas, fmt.Errorf("invalid non-activated function selector %#x", selector)
 	}
 
 	return function.execute(accessibleState, caller, addr, functionInput, suppliedGas, readOnly)
