21a16e5457f99c62d33b6e96ff2f25c6446f62d1
Matthew Lam <matthew.lam@avalabs.org>
2023-09-26 23:53:26 +0000

    more specified comment for slice of byte slices

--- core/state/statedb.go
+++ core/state/statedb.go
@@ -1197,8 +1197,9 @@ func (s *StateDB) Prepare(rules params.Rules, sender, coinbase common.Address, d
 }
 
 // preparePredicateStorageSlots populates the predicateStorageSlots field from the transaction's access list
-// Note: if an address is specified multiple times in the access list, the storage slots for that address are
-// appended to a list of predicate bytes, and every predicate in the list goes through verification.
+// Note: if an address is specified multiple times in the access list, each storage slot for that address is
+// appended to a slice of byte slices. Each byte slice represents a predicate, making it a slice of predicates
+// for each access list address, and every predicate in the slice goes through verification.
 func (s *StateDB) preparePredicateStorageSlots(rules params.Rules, list types.AccessList) {
 	s.predicateStorageSlots = make(map[common.Address][][]byte)
 	for _, el := range list {
