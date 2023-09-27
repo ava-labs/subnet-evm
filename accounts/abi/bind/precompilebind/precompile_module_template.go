f86c1567c04237392062cfedf1fe2bff52a8560c
Alberto Benegiamo <alberto.benegiamo@gmail.com>
2023-09-27 14:24:01 +0200

    some more fixes

--- accounts/abi/bind/precompilebind/precompile_module_template.go
+++ accounts/abi/bind/precompilebind/precompile_module_template.go
@@ -62,7 +62,7 @@ func (*configurator) MakeConfig() precompileconfig.Config {
 func (*configurator) Configure(chainConfig precompileconfig.ChainConfig, cfg precompileconfig.Config, state contract.StateDB, blockContext contract.ConfigurationBlockContext) error {
 	config, ok := cfg.(*Config)
 	if !ok {
-		return fmt.Errorf("incorrect config %T: %v", config, config)
+		return fmt.Errorf("expected config type %T, got %T: %v", &Config{}, cfg, cfg)
 	}
 	// CUSTOM CODE STARTS HERE
 	{{- if .Contract.AllowList}}
