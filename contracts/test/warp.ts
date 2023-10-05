ad0dfc7860d6cfe5f2d1b6d29e41a41b7fa40c03
Ceyhun Onur <ceyhun.onur@avalabs.org>
2023-10-05 20:38:11 +0300

    cleanups

--- contracts/test/warp.ts
+++ contracts/test/warp.ts
@@ -10,7 +10,10 @@ import { ethers } from "hardhat"
 
 const WARP_ADDRESS = "0x0200000000000000000000000000000000000005";
 var senderAddress: string
-var destAddress: string
+// Expected to be a hex string
+var payload: string
+var expectedUnsignedMessage: string
+var sourceID: string
 
 describe("IWarpMessenger", function () {
   this.timeout("30s")
@@ -21,26 +24,26 @@ describe("IWarpMessenger", function () {
     senderAddress = process.env["SENDER_ADDRESS"];
     owner = await ethers.getSigner(senderAddress);
     contract = await ethers.getContractAt("IWarpMessenger", WARP_ADDRESS, owner)
+
+    payload = process.env["PAYLOAD"];
+    expectedUnsignedMessage = process.env["EXPECTED_UNSIGNED_MESSAGE"];
+    sourceID = process.env["SOURCE_CHAIN_ID"];
   });
 
   it("contract should be to send warp message", async function () {
-    let payload = process.env["PAYLOAD"];
-    let expectedUnsignedMessage = process.env["EXPECTED_UNSIGNED_MESSAGE"];
-    let payloadHex = "0x" + payload.toString()
-    expect(ethers.utils.isHexString(payloadHex)).to.be.true;
-    let unsignedMessageHex = "0x" + expectedUnsignedMessage.toString()
-    expect(ethers.utils.isHexString(unsignedMessageHex)).to.be.true;
+    expect(ethers.utils.isHexString(payload)).to.be.true;
+    expect(ethers.utils.isHexString(expectedUnsignedMessage)).to.be.true;
 
-    console.log(`Sending warp message with payload ${payloadHex}, unsigned message ${unsignedMessageHex}`);
+    console.log(`Sending warp message with payload ${payload}, expected unsigned message ${expectedUnsignedMessage}`);
 
-    await expect(contract.sendWarpMessage(payloadHex))
+    await expect(contract.sendWarpMessage(payload))
       .to.emit(contract, 'SendWarpMessage')
-      .withArgs(senderAddress, unsignedMessageHex);
+      .withArgs(senderAddress, expectedUnsignedMessage);
   })
 
   it("should be able to fetch correct blockchain ID", async function () {
-    let sourceID = process.env["SOURCE_CHAIN_ID"];
-    let sourceIDHex = "0x" + sourceID.toString().padStart(32, "0");
-    expect(ethers.utils.isHexString(sourceIDHex)).to.be.true;
+    expect(ethers.utils.isHexString(sourceID)).to.be.true;
+    let blockchainID = await contract.getBlockchainID();
+    expect(blockchainID).to.be.equal(sourceID);
   })
 })
