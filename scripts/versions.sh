5a6b54a5765c56360a5d33c429f0d5a635faa7c0
Aaron Buchwald <aaron.buchwald56@gmail.com>
2023-11-28 13:52:27 -0500

    update avago dep

--- scripts/versions.sh
+++ scripts/versions.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Don't export them as they're used in the context of other calls
-AVALANCHE_VERSION=${AVALANCHE_VERSION:-'dc2373ff'} # C-Chain Warp Support
+AVALANCHE_VERSION=${AVALANCHE_VERSION:-'ea0aa322'} # C-Chain Warp Support
 GINKGO_VERSION=${GINKGO_VERSION:-'v2.2.0'}
 
 # This won't be used, but it's here to make code syncs easier
