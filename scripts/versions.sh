35c375b78017184909ebbf403e9ca03cbbc35215
Darioush Jalali <darioush.jalali@avalabs.org>
2023-12-13 20:15:07 -0800

    fix sh syntax

--- scripts/versions.sh
+++ scripts/versions.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Don't export them as they're used in the context of other calls
-AVALANCHE_VERSION=${AVALANCHE_VERSION:-'e5a0d878b12dfafa5f26779ca01a53ccc9692a25'
+AVALANCHE_VERSION=${AVALANCHE_VERSION:-'e5a0d878b12dfafa5f26779ca01a53ccc9692a25'}
 GINKGO_VERSION=${GINKGO_VERSION:-'v2.2.0'}
 
 # This won't be used, but it's here to make code syncs easier
