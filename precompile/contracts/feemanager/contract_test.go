f65fd757d1b3e496354630126764a5f70846c999
Ceyhun Onur <ceyhun.onur@avalabs.org>
2023-12-15 23:12:13 +0300

    fix tests

--- precompile/contracts/feemanager/contract_test.go
+++ precompile/contracts/feemanager/contract_test.go
@@ -281,8 +281,8 @@ var (
 				input = append(input, make([]byte, 32)...)
 				return input
 			},
-			ChainConfigFn: func(t testing.TB) precompileconfig.ChainConfig {
-				config := precompileconfig.NewMockChainConfig(gomock.NewController(t))
+			ChainConfigFn: func(ctrl *gomock.Controller) precompileconfig.ChainConfig {
+				config := precompileconfig.NewMockChainConfig(ctrl)
 				config.EXPECT().IsDUpgrade(gomock.Any()).Return(false).AnyTimes()
 				return config
 			},
@@ -304,8 +304,8 @@ var (
 				input = append(input, make([]byte, 32)...)
 				return input
 			},
-			ChainConfigFn: func(t testing.TB) precompileconfig.ChainConfig {
-				config := precompileconfig.NewMockChainConfig(gomock.NewController(t))
+			ChainConfigFn: func(ctrl *gomock.Controller) precompileconfig.ChainConfig {
+				config := precompileconfig.NewMockChainConfig(ctrl)
 				config.EXPECT().IsDUpgrade(gomock.Any()).Return(true).AnyTimes()
 				return config
 			},
