75f3b35709ae3a3331de1af7c0d43014d858ae22
Maru Newby <maru.newby@avalabs.org>
2023-12-22 16:18:00 -0800

    e2e: Switch load test to use the tmpnet fixture

--- scripts/run_ginkgo_load.sh
+++ scripts/run_ginkgo_load.sh
@@ -21,8 +21,11 @@ go install -v github.com/onsi/ginkgo/v2/ginkgo@${GINKGO_VERSION}
 
 TEST_SOURCE_ROOT=$(pwd)
 
-ACK_GINKGO_RC=true ginkgo build ./tests/load
+EXTRA_ARGS=
+AVALANCHEGO_BUILD_PATH="${AVALANCHEGO_BUILD_PATH:-}"
+if [[ -n "${AVALANCHEGO_BUILD_PATH}" ]]; then
+  EXTRA_ARGS="-- --avalanchego-path=${AVALANCHEGO_BUILD_PATH}/avalanchego --plugin-dir=${AVALANCHEGO_BUILD_PATH}/plugins"
+  echo "Running with extra args: ${EXTRA_ARGS}"
+fi
 
-./tests/load/load.test \
-  --ginkgo.vv \
-  --ginkgo.label-filter=${GINKGO_LABEL_FILTER:-""}
+ginkgo -vv --label-filter=${GINKGO_LABEL_FILTER:-""} ./tests/load ${EXTRA_ARGS}
