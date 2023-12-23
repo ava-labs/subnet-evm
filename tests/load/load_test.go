b68949694cfdb386bddf1b3f920acb9b58e5e64b
Maru Newby <maru.newby@avalabs.org>
2023-12-22 16:18:00 -0800

    e2e: Switch load test to use the tmpnet fixture

--- tests/load/load_test.go
+++ tests/load/load_test.go
@@ -7,19 +7,35 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"strings"
 	"testing"
 
-	"github.com/ava-labs/subnet-evm/tests/utils/runner"
-	"github.com/ethereum/go-ethereum/log"
 	ginkgo "github.com/onsi/ginkgo/v2"
+
 	"github.com/onsi/gomega"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/ethereum/go-ethereum/log"
+
+	"github.com/ava-labs/avalanchego/tests/fixture/e2e"
+	"github.com/ava-labs/avalanchego/tests/fixture/tmpnet"
+
+	"github.com/ava-labs/subnet-evm/tests"
+	"github.com/ava-labs/subnet-evm/tests/utils"
 )
 
-var getSubnet func() *runner.Subnet
+const subnetAName = "load-subnet-a"
+
+var (
+	flagVars     *e2e.FlagVars
+	repoRootPath = tests.GetRepoRootPath("tests/load")
+)
 
 func init() {
-	getSubnet = runner.RegisterFiveNodeSubnetRun()
+	// Configures flags used to configure tmpnet
+	flagVars = e2e.RegisterFlags()
 }
 
 func TestE2E(t *testing.T) {
@@ -28,25 +44,43 @@ func TestE2E(t *testing.T) {
 }
 
 var _ = ginkgo.Describe("[Load Simulator]", ginkgo.Ordered, func() {
-	ginkgo.It("basic subnet load test", ginkgo.Label("load"), func() {
-		subnetDetails := getSubnet()
-		blockchainID := subnetDetails.BlockchainID
+	require := require.New(ginkgo.GinkgoT())
 
-		nodeURIs := subnetDetails.ValidatorURIs
+	var env *e2e.TestEnvironment
+
+	ginkgo.BeforeAll(func() {
+		genesisPath := filepath.Join(repoRootPath, "tests/load/genesis/genesis.json")
+		env = e2e.NewTestEnvironment(
+			flagVars,
+			utils.NewTmpnetNetwork(
+				utils.NewTmpnetSubnet(subnetAName, genesisPath),
+			),
+		)
+	})
+
+	ginkgo.It("basic subnet load test", ginkgo.Label("load"), func() {
+		network := env.GetNetwork()
+
+		subnet := network.GetSubnet(subnetAName)
+		require.NotNil(subnet)
+		blockchainID := subnet.Chains[0].ChainID
+
+		nodeURIs := tmpnet.GetNodeURIs(network.Nodes)
 		rpcEndpoints := make([]string, 0, len(nodeURIs))
 		for _, uri := range nodeURIs {
-			rpcEndpoints = append(rpcEndpoints, fmt.Sprintf("%s/ext/bc/%s/rpc", uri, blockchainID))
+			rpcEndpoints = append(rpcEndpoints, fmt.Sprintf("%s/ext/bc/%s/rpc", uri.URI, blockchainID))
 		}
 		commaSeparatedRPCEndpoints := strings.Join(rpcEndpoints, ",")
 		err := os.Setenv("RPC_ENDPOINTS", commaSeparatedRPCEndpoints)
-		gomega.Expect(err).Should(gomega.BeNil())
+		require.NoError(err)
 
 		log.Info("Running load simulator...", "rpcEndpoints", commaSeparatedRPCEndpoints)
 		cmd := exec.Command("./scripts/run_simulator.sh")
+		cmd.Dir = repoRootPath
 		log.Info("Running load simulator script", "cmd", cmd.String())
 
 		out, err := cmd.CombinedOutput()
 		fmt.Printf("\nCombined output:\n\n%s\n", string(out))
-		gomega.Expect(err).Should(gomega.BeNil())
+		require.NoError(err)
 	})
 })
