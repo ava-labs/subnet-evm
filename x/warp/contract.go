6f403f0f665372fe7ec2e675a8cc234b51d27a68
Darioush Jalali <darioush.jalali@avalabs.org>
2024-01-02 07:32:55 -0800

    Update x/warp/contract.go
    Signed-off-by: Darioush Jalali <darioush.jalali@avalabs.org>

--- x/warp/contract.go
+++ x/warp/contract.go
@@ -109,8 +109,8 @@ func getBlockchainID(accessibleState contract.AccessibleState, caller common.Add
 // UnpackGetVerifiedWarpBlockHashInput attempts to unpack [input] into the uint32 type argument
 // assumes that [input] does not include selector (omits first 4 func signature bytes)
 func UnpackGetVerifiedWarpBlockHashInput(input []byte) (uint32, error) {
-	// We don't use strict mode here because it was disabled with the Durango.
-	// Since Warp will be deployed after the Durango, we don't need to use strict mode
+	// We don't use strict mode here because it was disabled with Durango.
+	// Since Warp will be deployed after Durango, we don't need to use strict mode
 	res, err := WarpABI.UnpackInput("getVerifiedWarpBlockHash", input, false)
 	if err != nil {
 		return 0, err
