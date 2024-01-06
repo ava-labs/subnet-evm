16afebf9eb597d44f4e56ada657694adf3e744b5
Patrick O'Grady <prohb125@gmail.com>
2024-01-05 16:30:31 -0800

    update avalanchego version

--- scripts/versions.sh
+++ scripts/versions.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Don't export them as they're used in the context of other calls
-AVALANCHE_VERSION=${AVALANCHE_VERSION:-'v1.10.18-rc.16'}
+AVALANCHE_VERSION=${AVALANCHE_VERSION:-'790b8a522a5c9068631272ef2df0e3a6e383db6d'}
 GINKGO_VERSION=${GINKGO_VERSION:-'v2.2.0'}
 
 # This won't be used, but it's here to make code syncs easier
