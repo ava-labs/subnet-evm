a8d3c820a4ddaea477bc50798f26a53643717ca9
Darioush Jalali <darioush.jalali@avalabs.org>
2024-01-26 16:13:38 -0800

    try again

--- scripts/versions.sh
+++ scripts/versions.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Don't export them as they're used in the context of other calls
-AVALANCHE_VERSION=${AVALANCHE_VERSION:-'tmpnet-restart-network-chainconfig'}
+AVALANCHE_VERSION=${AVALANCHE_VERSION:-'bddced5e24b210278d042d6321d6f4b7386713f1'}
 GINKGO_VERSION=${GINKGO_VERSION:-'v2.2.0'}
 
 # This won't be used, but it's here to make code syncs easier
