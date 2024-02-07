2c7178ac59466d989698822aad054fa9d5bfc7bc
Darioush Jalali <darioush.jalali@avalabs.org>
2024-02-06 17:28:01 -0800

    cherry-pick script

--- scripts/git_cherry_pick.sh
+++ scripts/git_cherry_pick.sh
@@ -5,7 +5,8 @@ set -o nounset
 set -o pipefail
 
 while true; do
-    git cherry-pick "$@" && exit 0 || echo "cherry-pick has conflicts, attempting to resolve..."
-    exit 1
+    git cherry-pick "$@" && exit 0 || echo "attempting to resolve delete/update conflicts..."
+    delete_update_conflicts=$(git status --porcelain | grep ^DU | cut -d' ' -f2 | xargs)
+    git rm ${delete_update_conflicts}
+    git cherry-pick --continue || exit 1
 done
-
