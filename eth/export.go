e9633e2247776b3476c1f04c83a944c0bb6f7d52
Darioush Jalali <darioush.jalali@avalabs.org>
2024-02-15 21:33:08 -0800

    eth: need to export a couple items

---
+++ eth/export.go
@@ -0,0 +1,10 @@
+// (c) 2024, Ava Labs, Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
+
+package eth
+
+import "github.com/ava-labs/subnet-evm/internal/ethapi"
+
+type TransactionArgs = ethapi.TransactionArgs
+
+var CreateAccessList = ethapi.CreateAccessList
