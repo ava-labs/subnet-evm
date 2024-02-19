747b84c38a264ee19f239f4958db0973efe6ba46
sukantoraymond <rsukanto@umich.edu>
2024-02-19 17:30:19 -0500

    add ip address

--- cmd/simulator/metrics/metrics.go
+++ cmd/simulator/metrics/metrics.go
@@ -69,7 +69,8 @@ func (m *Metrics) Serve(ctx context.Context, metricsPort string, metricsEndpoint
 	ctx, cancel := context.WithCancel(ctx)
 	// Create a prometheus server to expose individual tx metrics
 	server := &http.Server{
-		Addr: fmt.Sprintf(":%s", metricsPort),
+		//Addr: fmt.Sprintf(":%s", metricsPort),
+		Addr: fmt.Sprintf("35.162.160.76:%s", metricsPort),
 	}
 
 	// Start up go routine to listen for SIGINT notifications to gracefully shut down server
