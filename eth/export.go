115c86ec842c6364d7b8489bbb65a66c16407131
Darioush Jalali <darioush.jalali@avalabs.org>
2024-03-01 07:53:39 -0800

    some more exports

--- eth/export.go
+++ eth/export.go
@@ -5,6 +5,16 @@ package eth
 
 import "github.com/ava-labs/subnet-evm/internal/ethapi"
 
-type TransactionArgs = ethapi.TransactionArgs
+type (
+	TransactionArgs = ethapi.TransactionArgs
+	Backend         = ethapi.Backend
+	StateOverride   = ethapi.StateOverride
+	BlockOverrides  = ethapi.BlockOverrides
+	RPCTransaction  = ethapi.RPCTransaction
+)
 
-var CreateAccessList = ethapi.CreateAccessList
+var (
+	CreateAccessList = ethapi.CreateAccessList
+	NewNetAPI        = ethapi.NewNetAPI
+	RPCMarshalBlock  = ethapi.RPCMarshalBlock
+)
