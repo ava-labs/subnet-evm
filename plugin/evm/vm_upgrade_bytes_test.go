aecfb1cf3053fbb449f5dbe4396f2bfc4c2ffc62
Ceyhun Onur <ceyhun.onur@avalabs.org>
2024-04-27 14:06:34 +0300

    revert test changes

--- plugin/evm/vm_upgrade_bytes_test.go
+++ plugin/evm/vm_upgrade_bytes_test.go
@@ -171,7 +171,7 @@ func TestNetworkUpgradesOverriden(t *testing.T) {
 	upgradeBytesJSON := `{
 			"networkUpgradeOverrides": {
 				"subnetEVMTimestamp": 2,
-				"durangoTimestamp": 1607144401
+				"durangoTimestamp": 5
 			}
 		}`
 
@@ -206,7 +206,7 @@ func TestNetworkUpgradesOverriden(t *testing.T) {
 	require.False(t, vm.chainConfig.IsSubnetEVM(0))
 	require.True(t, vm.chainConfig.IsSubnetEVM(2))
 	require.False(t, vm.chainConfig.IsDurango(0))
-	require.True(t, vm.chainConfig.IsDurango(1607144401))
+	require.True(t, vm.chainConfig.IsDurango(5))
 }
 
 func mustMarshal(t *testing.T, v interface{}) string {
