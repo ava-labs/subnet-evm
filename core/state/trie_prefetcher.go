51f748329a3a67a2d138ae4b476da65ff9cd2283
Darioush Jalali <darioush.jalali@avalabs.org>
2024-06-13 18:10:54 -0700

    properly quiet the linter

--- core/state/trie_prefetcher.go
+++ core/state/trie_prefetcher.go
@@ -24,6 +24,7 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
 
+//nolint:unused
 package state
 
 import (
@@ -35,9 +36,6 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/log"
 )
-
-// XXX: Keep linter quiet
-var NewTriePrefetcher = newTriePrefetcher
 
 // triePrefetchMetricsPrefix is the prefix under which to publish the metrics.
 const triePrefetchMetricsPrefix = "trie/prefetch/"
