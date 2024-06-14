5ff6a94a837ce524445c7c16418c44fb63be2754
Darioush Jalali <darioush.jalali@avalabs.org>
2024-06-13 17:55:15 -0700

    fix lint

--- core/state/statedb.go
+++ core/state/statedb.go
@@ -38,12 +38,12 @@ import (
 	"github.com/ava-labs/coreth/core/types"
 	"github.com/ava-labs/coreth/metrics"
 	"github.com/ava-labs/coreth/params"
-	"github.com/ava-labs/coreth/trie"
 	"github.com/ava-labs/coreth/trie/trienode"
 	"github.com/ava-labs/coreth/trie/triestate"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/log"
+	"github.com/ethereum/go-ethereum/trie"
 )
 
 const (
