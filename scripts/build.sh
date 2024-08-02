8712437fb39187e1278455af8a64b347a9445de6
Darioush Jalali <darioush.jalali@avalabs.org>
2024-08-01 19:05:14 -0700

    fix build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -28,8 +28,3 @@ fi
 # Build Subnet EVM, which is run as a subprocess
 echo "Building Subnet EVM @ GitCommit: $SUBNET_EVM_COMMIT at $BINARY_PATH"
 go build -ldflags "-X github.com/ava-labs/subnet-evm/plugin/evm.GitCommit=$SUBNET_EVM_COMMIT $STATIC_LD_FLAGS" -o "$BINARY_PATH" "plugin/"*.go
-
-PLUGIN_PATH="${DEFAULT_PLUGIN_DIR}/srEXiWaHuhNyGwPUi444Tu47ZEDwxTWrbQiuD7FmgSAQ6X7Dy"
-echo "Symlinking ${BINARY_PATH} to ${PLUGIN_PATH}"
-mkdir -p "${DEFAULT_PLUGIN_DIR}"
-ln -sf "${BINARY_PATH}" "${PLUGIN_PATH}"
