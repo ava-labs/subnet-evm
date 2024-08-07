b15507b49923243f22c69222233c2e587f289a0d
Arran Schlosberg <me@arranschlosberg.com>
2024-08-07 13:19:52 +0100

    chore: update copyright years to keep linter happy

--- precompile/contracts/deployerallowlist/config.go
+++ precompile/contracts/deployerallowlist/config.go
@@ -1,4 +1,4 @@
-// (c) 2019-2020, Ava Labs, Inc. All rights reserved.
+// (c) 2019-2024, Ava Labs, Inc. All rights reserved.
 // See the file LICENSE for licensing terms.
 
 package deployerallowlist
