4dcb6298dd7a9fe6106a22523997c6d07920a79a
Darioush Jalali <darioush.jalali@avalabs.org>
2024-08-21 08:14:25 -0700

    add NO_RACE so uts can run faster locally

--- scripts/build_test.sh
+++ scripts/build_test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -o errexit
+set -o erexit
 set -o nounset
 set -o pipefail
 # TODO(marun) Ensure the working directory is the repository root or a non-canonical set of tests may be executed
@@ -20,5 +20,9 @@ source "$SUBNET_EVM_PATH"/scripts/constants.sh
 # We pass in the arguments to this script directly to enable easily passing parameters such as enabling race detection,
 # parallelism, and test coverage.
 # DO NOT RUN tests from the top level "tests" directory since they are run by ginkgo
+race="-race"
+if [[ -n "${NO_RACE:-}" ]]; then
+    race=""
+fi
 # shellcheck disable=SC2046
-go test -shuffle=on -race -timeout="${TIMEOUT:-600s}" -coverprofile=coverage.out -covermode=atomic "$@" $(go list ./... | grep -v github.com/ava-labs/subnet-evm/tests)
+go test -shuffle=on "${race}" -timeout="${TIMEOUT:-600s}" -coverprofile=coverage.out -covermode=atomic "$@" $(go list ./... | grep -v github.com/ava-labs/subnet-evm/tests)
