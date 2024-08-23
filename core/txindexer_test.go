ec1fcec9e11bbadf7dface78e20edaa44ffbf166
Arran Schlosberg <me@arranschlosberg.com>
2024-08-23 12:33:13 +0100

    fix: `ethereum/go-ethereum` instead of `ava-labs/subnet-evm` before formatting as local

--- core/txindexer_test.go
+++ core/txindexer_test.go
@@ -23,13 +23,13 @@ import (
 	"math/big"
 	"testing"
 
-	"github.com/ava-labs/subnet-evm/consensus/dummy"
-	"github.com/ava-labs/subnet-evm/core/rawdb"
-	"github.com/ava-labs/subnet-evm/core/types"
-	"github.com/ava-labs/subnet-evm/params"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/consensus/dummy"
+	"github.com/ethereum/go-ethereum/core/rawdb"
+	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethdb"
+	"github.com/ethereum/go-ethereum/params"
 	"github.com/stretchr/testify/require"
 )
 
