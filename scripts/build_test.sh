c5f6258c98071ff1e8e9b03da9b136bdd2838ede
Darioush Jalali <darioush.jalali@avalabs.org>
2024-09-10 09:17:53 -0700

    try

--- scripts/build_test.sh
+++ scripts/build_test.sh
@@ -46,6 +46,7 @@ do
         # First grep pattern corresponds to test failures, second pattern corresponds to test panics due to timeouts
         (grep "^--- FAIL" test.out | awk '{print $3}' || grep -E '^\s+Test.+ \(' test.out | awk '{print $1}') |
         sort -u | comm -23 -  <(dos2unix < ./scripts/known_flakes.txt)
+    )
     if [ -n "${unexpected_failures}" ]; then
         echo "Unexpected test failures: ${unexpected_failures}"
         exit 1
