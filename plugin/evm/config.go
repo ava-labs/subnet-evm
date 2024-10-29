2a9da82e5df765ae932fd3239472d502f2ea4bfd
Ceyhun Onur <ceyhun.onur@avalabs.org>
2024-10-29 17:51:53 +0300

    Revert "remove api changes"
    This reverts commit 8ef763fca65a7144ac35b69889873728d3485558.

--- plugin/evm/config.go
+++ plugin/evm/config.go
@@ -61,7 +61,10 @@ const (
 	// - state sync time: ~6 hrs.
 	defaultStateSyncMinBlocks   = 300_000
 	defaultStateSyncRequestSize = 1024 // the number of key/values to ask peers for per request
-	defaultDBType               = pebbledb.Name
+
+	defaultValidatorsAPIEnabled = true
+
+	defaultDBType = pebbledb.Name
 )
 
 type PBool bool
@@ -91,10 +94,11 @@ type Config struct {
 	AirdropFile string `json:"airdrop"`
 
 	// Subnet EVM APIs
-	SnowmanAPIEnabled bool   `json:"snowman-api-enabled"`
-	AdminAPIEnabled   bool   `json:"admin-api-enabled"`
-	AdminAPIDir       string `json:"admin-api-dir"`
-	WarpAPIEnabled    bool   `json:"warp-api-enabled"`
+	SnowmanAPIEnabled    bool   `json:"snowman-api-enabled"`
+	ValidatorsAPIEnabled bool   `json:"validators-api-enabled"`
+	AdminAPIEnabled      bool   `json:"admin-api-enabled"`
+	AdminAPIDir          string `json:"admin-api-dir"`
+	WarpAPIEnabled       bool   `json:"warp-api-enabled"`
 
 	// EnabledEthAPIs is a list of Ethereum services that should be enabled
 	// If none is specified, then we use the default list [defaultEnabledAPIs]
@@ -296,6 +300,7 @@ func (c *Config) SetDefaults() {
 	c.StateSyncRequestSize = defaultStateSyncRequestSize
 	c.AllowUnprotectedTxHashes = defaultAllowUnprotectedTxHashes
 	c.AcceptedCacheSize = defaultAcceptedCacheSize
+	c.ValidatorsAPIEnabled = defaultValidatorsAPIEnabled
 	c.DatabaseType = defaultDBType
 }
 
