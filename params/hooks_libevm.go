bbd95fae5281ea164f7b5982a1849c956a22b54b
Darioush Jalali <darioush.jalali@avalabs.org>
2024-11-07 09:09:05 -0800

    Remove IsProhibited (#682)

--- params/hooks_libevm.go
+++ params/hooks_libevm.go
@@ -8,13 +8,11 @@ import (
 	"math/big"
 
 	"github.com/ava-labs/avalanchego/snow"
-	"github.com/ava-labs/coreth/constants"
 	"github.com/ava-labs/coreth/precompile/contract"
 	"github.com/ava-labs/coreth/precompile/contracts/deployerallowlist"
 	"github.com/ava-labs/coreth/precompile/modules"
 	"github.com/ava-labs/coreth/precompile/precompileconfig"
 	"github.com/ava-labs/coreth/predicate"
-	"github.com/ava-labs/coreth/vmerrs"
 	"github.com/ava-labs/libevm/common"
 	"github.com/ava-labs/libevm/core/vm"
 	"github.com/ava-labs/libevm/libevm"
@@ -22,11 +20,6 @@ import (
 )
 
 func (r RulesExtra) CanCreateContract(ac *libevm.AddressContext, gas uint64, state libevm.StateReader) (uint64, error) {
-	// IsProhibited
-	if ac.Self == constants.BlackholeAddr || modules.ReservedAddress(ac.Self) {
-		return gas, vmerrs.ErrAddrProhibited
-	}
-
 	// If the allow list is enabled, check that [ac.Origin] has permission to deploy a contract.
 	if r.IsPrecompileEnabled(deployerallowlist.ContractAddress) {
 		allowListRole := deployerallowlist.GetContractDeployerAllowListStatus(state, ac.Origin)
