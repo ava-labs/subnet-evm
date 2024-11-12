5027dfbfd02a47176ab95b3720795ec20aa6df34
Darioush Jalali <darioush.jalali@avalabs.org>
2024-11-12 10:06:17 -0800

    chore(scripts): update scripts/lint_allowed_eth_imports.sh
    See https://github.com/ava-labs/coreth/pull/683

--- scripts/lint_allowed_eth_imports.sh
+++ scripts/lint_allowed_eth_imports.sh
@@ -4,14 +4,14 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-# Ensure that there are no geth imports that are not marked as explicitly allowed via ./scripts/geth-allowed-packages.txt
-# 1. Recursively search through all go files for any lines that include a direct import from go-ethereum
-# 2. Ignore lines that import go-ethereum with a named import starting with "geth"
+# Ensure that there are no eth imports that are not marked as explicitly allowed via ./scripts/eth-allowed-packages.txt
+# 1. Recursively search through all go files for any lines that include a direct import from libevm
+# 2. Ignore lines that import libevm with a named import starting with "eth"
 # 3. Sort the unique results
-# 4. Print out the difference between the search results and the list of specified allowed package imports from geth.
-geth_regexp='"github.com/ava-labs/libevm/.*"'
+# 4. Print out the difference between the search results and the list of specified allowed package imports from libevm.
+libevm_regexp='"github.com/ava-labs/libevm/.*"'
 allow_named_imports='eth\w\+ "'
-extra_imports=$(grep -r --include='*.go' --exclude=mocks.go --exclude-dir='simulator' "${geth_regexp}" -h | grep -v "${allow_named_imports}" | grep -o "${geth_regexp}" | sort -u | comm -23 - ./scripts/eth-allowed-packages.txt)
+extra_imports=$(grep -r --include='*.go' --exclude=mocks.go --exclude-dir='simulator' "${libevm_regexp}" -h | grep -v "${allow_named_imports}" | grep -o "${libevm_regexp}" | sort -u | comm -23 - ./scripts/eth-allowed-packages.txt)
 if [ -n "${extra_imports}" ]; then
     echo "new ethereum imports should be added to ./scripts/eth-allowed-packages.txt to prevent accidental imports:"
     echo "${extra_imports}"
