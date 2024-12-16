ae0462b99d308745c92d149a295c2e21be394380
Darioush Jalali <darioush.jalali@avalabs.org>
2024-12-16 13:18:54 -0800

    try

--- scripts/versions.sh
+++ scripts/versions.sh
@@ -4,7 +4,7 @@
 # shellcheck disable=SC2034
 
 # Don't export them as they're used in the context of other calls
-AVALANCHE_VERSION=${AVALANCHE_VERSION:-'f9b7a438'}
+AVALANCHE_VERSION=${AVALANCHE_VERSION:-'8c94e212'}
 GINKGO_VERSION=${GINKGO_VERSION:-'v2.2.0'}
 
 # This won't be used, but it's here to make code syncs easier
