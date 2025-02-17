29e39f0614e5ebc717ba8ff2fa7c5bc2957c9076
Quentin Mc Gaw <quentin.mcgaw@avalabs.org>
2025-02-17 15:40:24 +0100

    Rework `TestMustNotImport` to check for imports in non test code

--- plugin/evm/testutils/testutils.go
+++ plugin/evm/testutils/testutils.go
@@ -3,25 +3,3 @@
 // This package MUST NOT be imported by non-test packages.
 
 package testutils
-
-import (
-	"runtime"
-	"strings"
-)
-
-// panicIfCallsFromNonTest should be added at the top of every function defined in this package
-// to enforce this package to be used only by tests.
-func panicIfCallsFromNonTest() {
-	pc := make([]uintptr, 64)
-	runtime.Callers(0, pc)
-	frames := runtime.CallersFrames(pc)
-	for {
-		f, more := frames.Next()
-		if strings.HasPrefix(f.File, "/testing/") || strings.HasSuffix(f.File, "_test.go") {
-			return
-		}
-		if !more {
-			panic("no test file in call stack")
-		}
-	}
-}
