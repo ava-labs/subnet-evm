2a5bd114572603f7db02986cc717ac944977a583
Quentin McGaw <quentin.mcgaw@avalabs.org>
2025-02-17 15:47:08 +0100

    Clarify testutils package comment
    Signed-off-by: Quentin McGaw <quentin.mcgaw@gmail.com>

--- plugin/evm/testutils/testutils.go
+++ plugin/evm/testutils/testutils.go
@@ -1,5 +1,5 @@
 // Package testutils contains test utilities ONLY to be used outside plugin/evm.
 // The aim is to reduce changes in geth tests by using the utilities defined here.
-// This package MUST NOT be imported by non-test packages.
+// This package MUST NOT be imported by non-test files.
 
 package testutils
