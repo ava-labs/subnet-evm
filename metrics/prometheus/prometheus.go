8db925d4ba5e13838f6c01f939ede82d69c2ac19
Quentin Mc Gaw <quentin.mcgaw@avalabs.org>
2025-02-24 16:38:53 +0100

    Skip nil metric types

--- metrics/prometheus/prometheus.go
+++ metrics/prometheus/prometheus.go
@@ -67,6 +67,11 @@ func metricFamily(registry Registry, name string) (mf *dto.MetricFamily, err err
 	name = strings.ReplaceAll(name, "/", "_")
 
 	switch m := metric.(type) {
+	case metrics.NilCounter, metrics.NilCounterFloat64, metrics.NilEWMA,
+		metrics.NilGauge, metrics.NilGaugeFloat64, metrics.NilGaugeInfo,
+		metrics.NilHealthcheck, metrics.NilHistogram, metrics.NilMeter,
+		metrics.NilResettingTimer, metrics.NilSample, metrics.NilTimer:
+		return nil, fmt.Errorf("%w: %q metric is nil", errMetricSkip, name)
 	case metrics.Counter:
 		return &dto.MetricFamily{
 			Name: &name,
