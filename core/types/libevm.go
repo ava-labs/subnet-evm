76ba2a2b1eac5ac00966b0f2938680661a1968d6
Darioush Jalali <darioush.jalali@avalabs.org>
2025-02-25 08:19:41 -0800

    chore(core/types): header JSON and RLP serialization hooks
    - Define `HeaderExtra` with `RLP` and `JSON` serialization methods
    - remove `BlockNonce`
    - remove `EncodeNonce`
    - new functions `GetHeaderExtra` and `SetHeaderExtra`
    - Migrate existing custom `Header` to `HeaderSerializable` in block_ext.go with only `Hash` method for RLP code generation
    - Rename files gen_header_json.go to gen_header_serializable_json.go
    - Rename files gen_header_rlp.go to gen_header_serializable_rlp.go
    
    See original PR https://github.com/ava-labs/coreth/pull/746
    
    Signed-off-by: Quentin McGaw <quentin.mcgaw@gmail.com>
    Co-authored-by: Quentin Mc Gaw <quentin.mcgaw@avalabs.org>
    Co-authored-by: Arran Schlosberg <519948+ARR4N@users.noreply.github.com>
    Co-authored-by: Ceyhun Onur <ceyhun.onur@avalabs.org>

---
+++ core/types/libevm.go
@@ -0,0 +1,16 @@
+// (c) 2025, Ava Labs, Inc. All rights reserved.
+// See the file LICENSE for licensing terms.
+
+package types
+
+import (
+	ethtypes "github.com/ava-labs/libevm/core/types"
+)
+
+var extras = ethtypes.RegisterExtras[
+	HeaderExtra, *HeaderExtra,
+	ethtypes.NOOPBlockBodyHooks, *ethtypes.NOOPBlockBodyHooks,
+	isMultiCoin,
+]()
+
+type isMultiCoin bool
