5ea56ba3b376f7de3a6dc6abcae10ad3de95c0af
Darioush Jalali <darioush.jalali@avalabs.org>
2025-03-05 18:47:16 -0800

    try a minimal setup

--- scripts/run_ginkgo_load.sh
+++ scripts/run_ginkgo_load.sh
@@ -22,8 +22,9 @@ go install -v github.com/onsi/ginkgo/v2/ginkgo@"${GINKGO_VERSION}"
 EXTRA_ARGS=()
 AVALANCHEGO_BUILD_PATH="${AVALANCHEGO_BUILD_PATH:-}"
 if [[ -n "${AVALANCHEGO_BUILD_PATH}" ]]; then
-  EXTRA_ARGS=("--avalanchego-path=${AVALANCHEGO_BUILD_PATH}/avalanchego")
-  echo "Running with extra args:" "${EXTRA_ARGS[@]}"
+  EXTRA_ARGS+=("--avalanchego-path=${AVALANCHEGO_BUILD_PATH}/avalanchego")
 fi
+EXTRA_ARGS+=("$@")
 
-ginkgo -vv --label-filter="${GINKGO_LABEL_FILTER:-}" ./tests/load -- "${EXTRA_ARGS[@]}"
+echo "Running with extra args:" "${EXTRA_ARGS[@]:-}"
+ginkgo -vv --label-filter="${GINKGO_LABEL_FILTER:-}" ./tests/load -- "${EXTRA_ARGS[@]:-}"
