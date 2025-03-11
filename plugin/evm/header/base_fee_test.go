ava-labs/avalanchego

Implement header.ExtraPrefix serialization in `upgrade/` packages (#846)

graft/coreth/plugin/evm/header/base_fee_test.go
@@ -58,7 +58,7 @@ func TestBaseFee(t *testing.T) {
 			parent: &types.Header{
 				Number: big.NewInt(1),
 			},
-			wantErr: errDynamicFeeWindowInsufficientLength,
+			wantErr: ap3.ErrWindowInsufficientLength,
 		},
 		{
 			name:     "ap3_invalid_timestamp",
@@ -66,7 +66,7 @@ func TestBaseFee(t *testing.T) {
 			parent: &types.Header{
 				Number: big.NewInt(1),
 				Time:   1,
-				Extra:  feeWindowBytes(ap3.Window{}),
+				Extra:  (&ap3.Window{}).Bytes(),
 			},
 			timestamp: 0,
 			wantErr:   errInvalidTimestamp,
@@ -78,7 +78,7 @@ func TestBaseFee(t *testing.T) {
 				Number:  big.NewInt(1),
 				GasUsed: ap3.TargetGas - ap3.IntrinsicBlockGas,
 				Time:    1,
-				Extra:   feeWindowBytes(ap3.Window{}),
+				Extra:   (&ap3.Window{}).Bytes(),
 				BaseFee: big.NewInt(ap3.MinBaseFee + 1),
 			},
 			timestamp: 1,
@@ -89,7 +89,7 @@ func TestBaseFee(t *testing.T) {
 			upgrades: params.TestApricotPhase3Config.NetworkUpgrades,
 			parent: &types.Header{
 				Number:  big.NewInt(1),
-				Extra:   feeWindowBytes(ap3.Window{}),
+				Extra:   (&ap3.Window{}).Bytes(),
 				BaseFee: big.NewInt(ap3.MaxBaseFee),
 			},
 			timestamp: 1,
@@ -112,7 +112,7 @@ func TestBaseFee(t *testing.T) {
 			upgrades: params.TestApricotPhase3Config.NetworkUpgrades,
 			parent: &types.Header{
 				Number:  big.NewInt(1),
-				Extra:   feeWindowBytes(ap3.Window{}),
+				Extra:   (&ap3.Window{}).Bytes(),
 				BaseFee: big.NewInt(ap3.MaxBaseFee),
 			},
 			timestamp: 2 * ap3.WindowLen,
@@ -137,7 +137,7 @@ func TestBaseFee(t *testing.T) {
 			parent: &types.Header{
 				Number:  big.NewInt(1),
 				GasUsed: 2 * ap3.TargetGas,
-				Extra:   feeWindowBytes(ap3.Window{}),
+				Extra:   (&ap3.Window{}).Bytes(),
 				BaseFee: big.NewInt(ap3.MinBaseFee),
 			},
 			timestamp: 1,
@@ -161,7 +161,7 @@ func TestBaseFee(t *testing.T) {
 			parent: &types.Header{
 				Number:  big.NewInt(1),
 				GasUsed: 1,
-				Extra:   feeWindowBytes(ap3.Window{}),
+				Extra:   (&ap3.Window{}).Bytes(),
 				BaseFee: big.NewInt(1),
 			},
 			timestamp: 2 * ap3.WindowLen,
@@ -180,7 +180,7 @@ func TestBaseFee(t *testing.T) {
 			upgrades: params.TestApricotPhase4Config.NetworkUpgrades,
 			parent: &types.Header{
 				Number:       big.NewInt(1),
-				Extra:        feeWindowBytes(ap3.Window{}),
+				Extra:        (&ap3.Window{}).Bytes(),
 				BaseFee:      big.NewInt(ap4.MaxBaseFee),
 				BlockGasCost: big.NewInt(ap4.MinBlockGasCost),
 			},
@@ -205,7 +205,7 @@ func TestBaseFee(t *testing.T) {
 			parent: &types.Header{
 				Number:         big.NewInt(1),
 				GasUsed:        ap3.TargetGas,
-				Extra:          feeWindowBytes(ap3.Window{}),
+				Extra:          (&ap3.Window{}).Bytes(),
 				BaseFee:        big.NewInt(ap4.MinBaseFee),
 				ExtDataGasUsed: big.NewInt(ap3.TargetGas),
 				BlockGasCost:   big.NewInt(ap4.MinBlockGasCost),
@@ -238,7 +238,7 @@ func TestBaseFee(t *testing.T) {
 			upgrades: params.TestApricotPhase5Config.NetworkUpgrades,
 			parent: &types.Header{
 				Number:  big.NewInt(1),
-				Extra:   feeWindowBytes(ap3.Window{}),
+				Extra:   (&ap3.Window{}).Bytes(),
 				BaseFee: big.NewInt(ap4.MaxBaseFee),
 			},
 			timestamp: 1,
@@ -262,7 +262,7 @@ func TestBaseFee(t *testing.T) {
 			parent: &types.Header{
 				Number:         big.NewInt(1),
 				GasUsed:        ap5.TargetGas,
-				Extra:          feeWindowBytes(ap3.Window{}),
+				Extra:          (&ap3.Window{}).Bytes(),
 				BaseFee:        big.NewInt(ap4.MinBaseFee),
 				ExtDataGasUsed: big.NewInt(ap5.TargetGas),
 			},
@@ -295,7 +295,7 @@ func TestBaseFee(t *testing.T) {
 			parent: &types.Header{
 				Number:         big.NewInt(1),
 				GasUsed:        ap5.TargetGas,
-				Extra:          feeWindowBytes(ap3.Window{}),
+				Extra:          (&ap3.Window{}).Bytes(),
 				BaseFee:        big.NewInt(etna.MinBaseFee),
 				ExtDataGasUsed: big.NewInt(ap5.TargetGas),
 			},
@@ -346,7 +346,7 @@ func TestEstimateNextBaseFee(t *testing.T) {
 			upgrades: params.TestApricotPhase3Config.NetworkUpgrades,
 			parent: &types.Header{
 				Number:  big.NewInt(1),
-				Extra:   feeWindowBytes(ap3.Window{}),
+				Extra:   (&ap3.Window{}).Bytes(),
 				BaseFee: big.NewInt(ap3.MaxBaseFee),
 			},
 			timestamp: 1,

graft/coreth/plugin/evm/header/dynamic_fee_windower.go
@@ -4,12 +4,10 @@
 package header
 
 import (
-	"encoding/binary"
 	"errors"
 	"fmt"
 	"math/big"
 
-	"github.com/ava-labs/avalanchego/utils/wrappers"
 	"github.com/ava-labs/coreth/core/types"
 	"github.com/ava-labs/coreth/params"
 	"github.com/ava-labs/coreth/plugin/evm/upgrade/ap3"
@@ -19,10 +17,6 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/math"
 )
-
-// FeeWindowSize is the number of bytes that are used to encode the dynamic fee
-// window in the header's Extra field after the Apricot Phase 3 upgrade.
-const FeeWindowSize = wrappers.LongLen * ap3.WindowLen
 
 var (
 	maxUint256Plus1 = new(big.Int).Lsh(common.Big1, 256)
@@ -41,8 +35,7 @@ var (
 	ap3BaseFeeChangeDenominator = big.NewInt(ap3.BaseFeeChangeDenominator)
 	ap5BaseFeeChangeDenominator = big.NewInt(ap5.BaseFeeChangeDenominator)
 
-	errInvalidTimestamp                   = errors.New("invalid timestamp")
-	errDynamicFeeWindowInsufficientLength = errors.New("insufficient length for dynamic fee window")
+	errInvalidTimestamp = errors.New("invalid timestamp")
 )
 
 // baseFeeFromWindow should only be called if `timestamp` >= `config.ApricotPhase3Timestamp`
@@ -165,7 +158,7 @@ func feeWindow(
 		return ap3.Window{}, nil
 	}
 
-	dynamicFeeWindow, err := parseFeeWindow(parent.Extra)
+	dynamicFeeWindow, err := ap3.ParseWindow(parent.Extra)
 	if err != nil {
 		return ap3.Window{}, err
 	}
@@ -235,30 +228,4 @@ func selectBigWithinBounds(lowerBound, value, upperBound *big.Int) *big.Int {
 	default:
 		return value
 	}
-}
-
-func parseFeeWindow(bytes []byte) (ap3.Window, error) {
-	if len(bytes) < FeeWindowSize {
-		return ap3.Window{}, fmt.Errorf("%w: expected at least %d bytes but got %d bytes",
-			errDynamicFeeWindowInsufficientLength,
-			FeeWindowSize,
-			len(bytes),
-		)
-	}
-
-	var window ap3.Window
-	for i := range window {
-		offset := i * wrappers.LongLen
-		window[i] = binary.BigEndian.Uint64(bytes[offset:])
-	}
-	return window, nil
-}
-
-func feeWindowBytes(w ap3.Window) []byte {
-	bytes := make([]byte, FeeWindowSize)
-	for i, v := range w {
-		offset := i * wrappers.LongLen
-		binary.BigEndian.PutUint64(bytes[offset:], v)
-	}
-	return bytes
 }

graft/coreth/plugin/evm/header/extra_test.go
@@ -40,7 +40,7 @@ func TestExtraPrefix(t *testing.T) {
 				Number: big.NewInt(1),
 			},
 			timestamp: 1,
-			want:      feeWindowBytes(ap3.Window{}),
+			want:      (&ap3.Window{}).Bytes(),
 		},
 		{
 			name:     "ap3_genesis_block",
@@ -48,7 +48,7 @@ func TestExtraPrefix(t *testing.T) {
 			parent: &types.Header{
 				Number: big.NewInt(0),
 			},
-			want: feeWindowBytes(ap3.Window{}),
+			want: (&ap3.Window{}).Bytes(),
 		},
 		{
 			name:     "ap3_invalid_fee_window",
@@ -56,7 +56,7 @@ func TestExtraPrefix(t *testing.T) {
 			parent: &types.Header{
 				Number: big.NewInt(1),
 			},
-			wantErr: errDynamicFeeWindowInsufficientLength,
+			wantErr: ap3.ErrWindowInsufficientLength,
 		},
 		{
 			name:     "ap3_invalid_timestamp",
@@ -64,7 +64,7 @@ func TestExtraPrefix(t *testing.T) {
 			parent: &types.Header{
 				Number: big.NewInt(1),
 				Time:   1,
-				Extra:  feeWindowBytes(ap3.Window{}),
+				Extra:  (&ap3.Window{}).Bytes(),
 			},
 			timestamp: 0,
 			wantErr:   errInvalidTimestamp,
@@ -75,9 +75,9 @@ func TestExtraPrefix(t *testing.T) {
 			parent: &types.Header{
 				Number:  big.NewInt(1),
 				GasUsed: ap3.TargetGas,
-				Extra: feeWindowBytes(ap3.Window{
+				Extra: (&ap3.Window{
 					1, 2, 3, 4,
-				}),
+				}).Bytes(),
 			},
 			timestamp: 1,
 			want: func() []byte {
@@ -86,7 +86,7 @@ func TestExtraPrefix(t *testing.T) {
 				}
 				window.Add(ap3.TargetGas, ap3.IntrinsicBlockGas)
 				window.Shift(1)
-				return feeWindowBytes(window)
+				return window.Bytes()
 			}(),
 		},
 		{
@@ -95,7 +95,7 @@ func TestExtraPrefix(t *testing.T) {
 			parent: &types.Header{
 				Number: big.NewInt(0),
 			},
-			want: feeWindowBytes(ap3.Window{}),
+			want: (&ap3.Window{}).Bytes(),
 		},
 		{
 			name:     "ap4_no_block_gas_cost",
@@ -103,14 +103,14 @@ func TestExtraPrefix(t *testing.T) {
 			parent: &types.Header{
 				Number:  big.NewInt(1),
 				GasUsed: ap3.TargetGas,
-				Extra:   feeWindowBytes(ap3.Window{}),
+				Extra:   (&ap3.Window{}).Bytes(),
 			},
 			timestamp: 2,
 			want: func() []byte {
 				var window ap3.Window
 				window.Add(ap3.TargetGas)
 				window.Shift(2)
-				return feeWindowBytes(window)
+				return window.Bytes()
 			}(),
 		},
 		{
@@ -119,7 +119,7 @@ func TestExtraPrefix(t *testing.T) {
 			parent: &types.Header{
 				Number:       big.NewInt(1),
 				GasUsed:      ap3.TargetGas,
-				Extra:        feeWindowBytes(ap3.Window{}),
+				Extra:        (&ap3.Window{}).Bytes(),
 				BlockGasCost: big.NewInt(ap4.MinBlockGasCost),
 			},
 			timestamp: 1,
@@ -130,7 +130,7 @@ func TestExtraPrefix(t *testing.T) {
 					(ap4.TargetBlockRate-1)*ap4.BlockGasCostStep,
 				)
 				window.Shift(1)
-				return feeWindowBytes(window)
+				return window.Bytes()
 			}(),
 		},
 		{
@@ -139,7 +139,7 @@ func TestExtraPrefix(t *testing.T) {
 			parent: &types.Header{
 				Number:         big.NewInt(1),
 				GasUsed:        ap3.TargetGas,
-				Extra:          feeWindowBytes(ap3.Window{}),
+				Extra:          (&ap3.Window{}).Bytes(),
 				ExtDataGasUsed: big.NewInt(5),
 			},
 			timestamp: 1,
@@ -150,7 +150,7 @@ func TestExtraPrefix(t *testing.T) {
 					5,
 				)
 				window.Shift(1)
-				return feeWindowBytes(window)
+				return window.Bytes()
 			}(),
 		},
 		{
@@ -159,9 +159,9 @@ func TestExtraPrefix(t *testing.T) {
 			parent: &types.Header{
 				Number:  big.NewInt(1),
 				GasUsed: ap3.TargetGas,
-				Extra: feeWindowBytes(ap3.Window{
+				Extra: (&ap3.Window{
 					1, 2, 3, 4,
-				}),
+				}).Bytes(),
 				ExtDataGasUsed: big.NewInt(5),
 				BlockGasCost:   big.NewInt(ap4.MinBlockGasCost),
 			},
@@ -176,7 +176,7 @@ func TestExtraPrefix(t *testing.T) {
 					(ap4.TargetBlockRate-1)*ap4.BlockGasCostStep,
 				)
 				window.Shift(1)
-				return feeWindowBytes(window)
+				return window.Bytes()
 			}(),
 		},
 		{
@@ -185,7 +185,7 @@ func TestExtraPrefix(t *testing.T) {
 			parent: &types.Header{
 				Number:       big.NewInt(1),
 				GasUsed:      ap5.TargetGas,
-				Extra:        feeWindowBytes(ap3.Window{}),
+				Extra:        (&ap3.Window{}).Bytes(),
 				BlockGasCost: big.NewInt(ap4.MinBlockGasCost),
 			},
 			timestamp: 1,
@@ -193,7 +193,7 @@ func TestExtraPrefix(t *testing.T) {
 				var window ap3.Window
 				window.Add(ap5.TargetGas)
 				window.Shift(1)
-				return feeWindowBytes(window)
+				return window.Bytes()
 			}(),
 		},
 		{
@@ -202,9 +202,9 @@ func TestExtraPrefix(t *testing.T) {
 			parent: &types.Header{
 				Number:  big.NewInt(1),
 				GasUsed: ap5.TargetGas,
-				Extra: feeWindowBytes(ap3.Window{
+				Extra: (&ap3.Window{
 					1, 2, 3, 4,
-				}),
+				}).Bytes(),
 				ExtDataGasUsed: big.NewInt(5),
 				BlockGasCost:   big.NewInt(ap4.MinBlockGasCost),
 			},
@@ -218,7 +218,7 @@ func TestExtraPrefix(t *testing.T) {
 					5,
 				)
 				window.Shift(1)
-				return feeWindowBytes(window)
+				return window.Bytes()
 			}(),
 		},
 	}
@@ -276,7 +276,7 @@ func TestVerifyExtra(t *testing.T) {
 			rules: params.AvalancheRules{
 				IsApricotPhase3: true,
 			},
-			extra:    make([]byte, FeeWindowSize),
+			extra:    make([]byte, ap3.WindowSize),
 			expected: nil,
 		},
 		{
@@ -284,7 +284,7 @@ func TestVerifyExtra(t *testing.T) {
 			rules: params.AvalancheRules{
 				IsApricotPhase3: true,
 			},
-			extra:    make([]byte, FeeWindowSize-1),
+			extra:    make([]byte, ap3.WindowSize-1),
 			expected: errInvalidExtraLength,
 		},
 		{
@@ -292,7 +292,7 @@ func TestVerifyExtra(t *testing.T) {
 			rules: params.AvalancheRules{
 				IsApricotPhase3: true,
 			},
-			extra:    make([]byte, FeeWindowSize+1),
+			extra:    make([]byte, ap3.WindowSize+1),
 			expected: errInvalidExtraLength,
 		},
 		{
@@ -300,7 +300,7 @@ func TestVerifyExtra(t *testing.T) {
 			rules: params.AvalancheRules{
 				IsDurango: true,
 			},
-			extra:    make([]byte, FeeWindowSize),
+			extra:    make([]byte, ap3.WindowSize),
 			expected: nil,
 		},
 		{
@@ -308,7 +308,7 @@ func TestVerifyExtra(t *testing.T) {
 			rules: params.AvalancheRules{
 				IsDurango: true,
 			},
-			extra:    make([]byte, FeeWindowSize+1),
+			extra:    make([]byte, ap3.WindowSize+1),
 			expected: nil,
 		},
 		{
@@ -316,7 +316,7 @@ func TestVerifyExtra(t *testing.T) {
 			rules: params.AvalancheRules{
 				IsDurango: true,
 			},
-			extra:    make([]byte, FeeWindowSize-1),
+			extra:    make([]byte, ap3.WindowSize-1),
 			expected: errInvalidExtraLength,
 		},
 	}
@@ -342,18 +342,18 @@ func TestPredicateBytesFromExtra(t *testing.T) {
 		},
 		{
 			name:     "too_short",
-			extra:    make([]byte, FeeWindowSize-1),
+			extra:    make([]byte, ap3.WindowSize-1),
 			expected: nil,
 		},
 		{
 			name:     "empty_predicate",
-			extra:    make([]byte, FeeWindowSize),
+			extra:    make([]byte, ap3.WindowSize),
 			expected: nil,
 		},
 		{
 			name: "non_empty_predicate",
 			extra: []byte{
-				FeeWindowSize: 5,
+				ap3.WindowSize: 5,
 			},
 			expected: []byte{5},
 		},

ava-labs/subnet-evm

plugin/evm/header/base_fee_test.go
@@ -69,7 +69,7 @@ func BaseFeeTest(t *testing.T, feeConfig commontype.FeeConfig) {
 			parent: &types.Header{
 				Number: big.NewInt(1),
 			},
-			wantErr: errDynamicFeeWindowInsufficientLength,
+			wantErr: subnetevm.ErrWindowInsufficientLength,
 		},
 		{
 			name:     "subnet_evm_invalid_timestamp",
@@ -77,7 +77,7 @@ func BaseFeeTest(t *testing.T, feeConfig commontype.FeeConfig) {
 			parent: &types.Header{
 				Number: big.NewInt(1),
 				Time:   1,
-				Extra:  feeWindowBytes(subnetevm.Window{}),
+				Extra:  (&subnetevm.Window{}).Bytes(),
 			},
 			timestamp: 0,
 			wantErr:   errInvalidTimestamp,
@@ -89,7 +89,7 @@ func BaseFeeTest(t *testing.T, feeConfig commontype.FeeConfig) {
 				Number:  big.NewInt(1),
 				GasUsed: feeConfig.TargetGas.Uint64(),
 				Time:    1,
-				Extra:   feeWindowBytes(subnetevm.Window{}),
+				Extra:   (&subnetevm.Window{}).Bytes(),
 				BaseFee: big.NewInt(feeConfig.MinBaseFee.Int64() + 1),
 			},
 			timestamp: 1,
@@ -100,7 +100,7 @@ func BaseFeeTest(t *testing.T, feeConfig commontype.FeeConfig) {
 			upgrades: params.TestSubnetEVMChainConfig.NetworkUpgrades,
 			parent: &types.Header{
 				Number:  big.NewInt(1),
-				Extra:   feeWindowBytes(subnetevm.Window{}),
+				Extra:   (&subnetevm.Window{}).Bytes(),
 				BaseFee: big.NewInt(maxBaseFee),
 			},
 			timestamp: 1,
@@ -123,7 +123,7 @@ func BaseFeeTest(t *testing.T, feeConfig commontype.FeeConfig) {
 			upgrades: params.TestSubnetEVMChainConfig.NetworkUpgrades,
 			parent: &types.Header{
 				Number:  big.NewInt(1),
-				Extra:   feeWindowBytes(subnetevm.Window{}),
+				Extra:   (&subnetevm.Window{}).Bytes(),
 				BaseFee: big.NewInt(maxBaseFee),
 			},
 			timestamp: 2 * subnetevm.WindowLen,
@@ -148,7 +148,7 @@ func BaseFeeTest(t *testing.T, feeConfig commontype.FeeConfig) {
 			parent: &types.Header{
 				Number:  big.NewInt(1),
 				GasUsed: 2 * feeConfig.TargetGas.Uint64(),
-				Extra:   feeWindowBytes(subnetevm.Window{}),
+				Extra:   (&subnetevm.Window{}).Bytes(),
 				BaseFee: big.NewInt(feeConfig.MinBaseFee.Int64()),
 			},
 			timestamp: 1,
@@ -172,7 +172,7 @@ func BaseFeeTest(t *testing.T, feeConfig commontype.FeeConfig) {
 			parent: &types.Header{
 				Number:  big.NewInt(1),
 				GasUsed: 1,
-				Extra:   feeWindowBytes(subnetevm.Window{}),
+				Extra:   (&subnetevm.Window{}).Bytes(),
 				BaseFee: big.NewInt(1),
 			},
 			timestamp: 2 * subnetevm.WindowLen,
@@ -221,7 +221,7 @@ func EstimateNextBaseFeeTest(t *testing.T, feeConfig commontype.FeeConfig) {
 			upgrades: params.TestSubnetEVMChainConfig.NetworkUpgrades,
 			parent: &types.Header{
 				Number:  big.NewInt(1),
-				Extra:   feeWindowBytes(subnetevm.Window{}),
+				Extra:   (&subnetevm.Window{}).Bytes(),
 				BaseFee: new(big.Int).SetUint64(testBaseFee),
 			},
 			timestamp: 1,

plugin/evm/header/dynamic_fee_windower.go
@@ -4,12 +4,10 @@
 package header
 
 import (
-	"encoding/binary"
 	"errors"
 	"fmt"
 	"math/big"
 
-	"github.com/ava-labs/avalanchego/utils/wrappers"
 	"github.com/ava-labs/subnet-evm/commontype"
 	"github.com/ava-labs/subnet-evm/core/types"
 	"github.com/ava-labs/subnet-evm/params"
@@ -18,16 +16,11 @@ import (
 	"github.com/ethereum/go-ethereum/common/math"
 )
 
-// FeeWindowSize is the number of bytes that are used to encode the dynamic fee
-// window in the header's Extra field after the SubnetEVM activation.
-const FeeWindowSize = wrappers.LongLen * subnetevm.WindowLen
-
 var (
 	maxUint256Plus1 = new(big.Int).Lsh(common.Big1, 256)
 	maxUint256      = new(big.Int).Sub(maxUint256Plus1, common.Big1)
 
-	errInvalidTimestamp                   = errors.New("invalid timestamp")
-	errDynamicFeeWindowInsufficientLength = errors.New("insufficient length for dynamic fee window")
+	errInvalidTimestamp = errors.New("invalid timestamp")
 )
 
 // baseFeeFromWindow should only be called if `timestamp` >= `config.SubnetEVMTimestamp`
@@ -132,7 +125,7 @@ func feeWindow(
 		return subnetevm.Window{}, nil
 	}
 
-	dynamicFeeWindow, err := parseFeeWindow(parent.Extra)
+	dynamicFeeWindow, err := subnetevm.ParseWindow(parent.Extra)
 	if err != nil {
 		return subnetevm.Window{}, err
 	}
@@ -167,30 +160,4 @@ func selectBigWithinBounds(lowerBound, value, upperBound *big.Int) *big.Int {
 	default:
 		return value
 	}
-}
-
-func parseFeeWindow(bytes []byte) (subnetevm.Window, error) {
-	if len(bytes) < FeeWindowSize {
-		return subnetevm.Window{}, fmt.Errorf("%w: expected at least %d bytes but got %d bytes",
-			errDynamicFeeWindowInsufficientLength,
-			FeeWindowSize,
-			len(bytes),
-		)
-	}
-
-	var window subnetevm.Window
-	for i := range window {
-		offset := i * wrappers.LongLen
-		window[i] = binary.BigEndian.Uint64(bytes[offset:])
-	}
-	return window, nil
-}
-
-func feeWindowBytes(w subnetevm.Window) []byte {
-	bytes := make([]byte, FeeWindowSize)
-	for i, v := range w {
-		offset := i * wrappers.LongLen
-		binary.BigEndian.PutUint64(bytes[offset:], v)
-	}
-	return bytes
 }

plugin/evm/header/extra_test.go
@@ -43,7 +43,7 @@ func TestExtraPrefix(t *testing.T) {
 				Number: big.NewInt(1),
 			},
 			timestamp: 1,
-			want:      feeWindowBytes(subnetevm.Window{}),
+			want:      (&subnetevm.Window{}).Bytes(),
 		},
 		{
 			name:     "subnet_evm_genesis_block",
@@ -51,7 +51,7 @@ func TestExtraPrefix(t *testing.T) {
 			parent: &types.Header{
 				Number: big.NewInt(0),
 			},
-			want: feeWindowBytes(subnetevm.Window{}),
+			want: (&subnetevm.Window{}).Bytes(),
 		},
 		{
 			name:     "subnet_evm_invalid_fee_window",
@@ -59,7 +59,7 @@ func TestExtraPrefix(t *testing.T) {
 			parent: &types.Header{
 				Number: big.NewInt(1),
 			},
-			wantErr: errDynamicFeeWindowInsufficientLength,
+			wantErr: subnetevm.ErrWindowInsufficientLength,
 		},
 		{
 			name:     "subnet_evm_invalid_timestamp",
@@ -67,7 +67,7 @@ func TestExtraPrefix(t *testing.T) {
 			parent: &types.Header{
 				Number: big.NewInt(1),
 				Time:   1,
-				Extra:  feeWindowBytes(subnetevm.Window{}),
+				Extra:  (&subnetevm.Window{}).Bytes(),
 			},
 			timestamp: 0,
 			wantErr:   errInvalidTimestamp,
@@ -78,9 +78,9 @@ func TestExtraPrefix(t *testing.T) {
 			parent: &types.Header{
 				Number:  big.NewInt(1),
 				GasUsed: targetGas,
-				Extra: feeWindowBytes(subnetevm.Window{
+				Extra: (&subnetevm.Window{
 					1, 2, 3, 4,
-				}),
+				}).Bytes(),
 				BlockGasCost: big.NewInt(blockGas),
 			},
 			timestamp: 1,
@@ -90,7 +90,7 @@ func TestExtraPrefix(t *testing.T) {
 				}
 				window.Add(targetGas)
 				window.Shift(1)
-				return feeWindowBytes(window)
+				return window.Bytes()
 			}(),
 		},
 	}
@@ -132,7 +132,7 @@ func TestVerifyExtra(t *testing.T) {
 			rules: params.AvalancheRules{
 				IsSubnetEVM: true,
 			},
-			extra:    make([]byte, FeeWindowSize),
+			extra:    make([]byte, subnetevm.WindowSize),
 			expected: nil,
 		},
 		{
@@ -140,7 +140,7 @@ func TestVerifyExtra(t *testing.T) {
 			rules: params.AvalancheRules{
 				IsSubnetEVM: true,
 			},
-			extra:    make([]byte, FeeWindowSize-1),
+			extra:    make([]byte, subnetevm.WindowSize-1),
 			expected: errInvalidExtraLength,
 		},
 		{
@@ -148,7 +148,7 @@ func TestVerifyExtra(t *testing.T) {
 			rules: params.AvalancheRules{
 				IsSubnetEVM: true,
 			},
-			extra:    make([]byte, FeeWindowSize+1),
+			extra:    make([]byte, subnetevm.WindowSize+1),
 			expected: errInvalidExtraLength,
 		},
 		{
@@ -156,7 +156,7 @@ func TestVerifyExtra(t *testing.T) {
 			rules: params.AvalancheRules{
 				IsDurango: true,
 			},
-			extra:    make([]byte, FeeWindowSize),
+			extra:    make([]byte, subnetevm.WindowSize),
 			expected: nil,
 		},
 		{
@@ -164,7 +164,7 @@ func TestVerifyExtra(t *testing.T) {
 			rules: params.AvalancheRules{
 				IsDurango: true,
 			},
-			extra:    make([]byte, FeeWindowSize+1),
+			extra:    make([]byte, subnetevm.WindowSize+1),
 			expected: nil,
 		},
 		{
@@ -172,7 +172,7 @@ func TestVerifyExtra(t *testing.T) {
 			rules: params.AvalancheRules{
 				IsDurango: true,
 			},
-			extra:    make([]byte, FeeWindowSize-1),
+			extra:    make([]byte, subnetevm.WindowSize-1),
 			expected: errInvalidExtraLength,
 		},
 	}
@@ -198,18 +198,18 @@ func TestPredicateBytesFromExtra(t *testing.T) {
 		},
 		{
 			name:     "too_short",
-			extra:    make([]byte, FeeWindowSize-1),
+			extra:    make([]byte, subnetevm.WindowSize-1),
 			expected: nil,
 		},
 		{
 			name:     "empty_predicate",
-			extra:    make([]byte, FeeWindowSize),
+			extra:    make([]byte, subnetevm.WindowSize),
 			expected: nil,
 		},
 		{
 			name: "non_empty_predicate",
 			extra: []byte{
-				FeeWindowSize: 5,
+				subnetevm.WindowSize: 5,
 			},
 			expected: []byte{5},
 		},