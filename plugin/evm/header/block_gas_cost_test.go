ava-labs/avalanchego

Round up in `header.EstimateRequiredTip` (#840)

plugin/evm/header/block_gas_cost_test.go
@@ -203,6 +203,17 @@ func TestEstimateRequiredTip(t *testing.T) {
 			wantErr: errExtDataGasUsedNil,
 		},
 		{
+			name:         "no_gas_used",
+			ap4Timestamp: utils.NewUint64(0),
+			header: &types.Header{
+				GasUsed:        0,
+				ExtDataGasUsed: big.NewInt(0),
+				BaseFee:        big.NewInt(1),
+				BlockGasCost:   big.NewInt(1),
+			},
+			wantErr: errNoGasUsed,
+		},
+		{
 			name:         "success",
 			ap4Timestamp: utils.NewUint64(0),
 			header: &types.Header{
@@ -215,6 +226,20 @@ func TestEstimateRequiredTip(t *testing.T) {
 			// totalRequiredTips = BlockGasCost * BaseFee
 			// estimatedTip = totalRequiredTips / totalGasUsed
 			want: big.NewInt((101112 * 456) / (123 + 789)),
+		},
+		{
+			name:         "success_rounds_up",
+			ap4Timestamp: utils.NewUint64(0),
+			header: &types.Header{
+				GasUsed:        124,
+				ExtDataGasUsed: big.NewInt(789),
+				BaseFee:        big.NewInt(456),
+				BlockGasCost:   big.NewInt(101112),
+			},
+			// totalGasUsed = GasUsed + ExtDataGasUsed
+			// totalRequiredTips = BlockGasCost * BaseFee
+			// estimatedTip = totalRequiredTips / totalGasUsed
+			want: big.NewInt((101112*456)/(124+789) + 1), // +1 to round up
 		},
 	}
 	for _, test := range tests {

ava-labs/subnet-evm

plugin/evm/header/block_gas_cost_test.go
@@ -225,16 +225,39 @@ func TestEstimateRequiredTip(t *testing.T) {
 			wantErr: errBlockGasCostNil,
 		},
 		{
+			name:               "no_gas_used",
+			subnetEVMTimestamp: utils.NewUint64(0),
+			header: &types.Header{
+				GasUsed:      0,
+				BaseFee:      big.NewInt(1),
+				BlockGasCost: big.NewInt(1),
+			},
+			wantErr: errNoGasUsed,
+		},
+		{
 			name:               "success",
 			subnetEVMTimestamp: utils.NewUint64(0),
 			header: &types.Header{
-				GasUsed:      123,
+				GasUsed:      912,
 				BaseFee:      big.NewInt(456),
 				BlockGasCost: big.NewInt(101112),
 			},
 			// totalRequiredTips = BlockGasCost * BaseFee
 			// estimatedTip = totalRequiredTips / GasUsed
-			want: big.NewInt((101112 * 456) / (123)),
+			want: big.NewInt((101112 * 456) / (912)),
+		},
+		{
+			name:               "success_rounds_up",
+			subnetEVMTimestamp: utils.NewUint64(0),
+			header: &types.Header{
+				GasUsed:      124,
+				BaseFee:      big.NewInt(456),
+				BlockGasCost: big.NewInt(101112),
+			},
+			// totalGasUsed = GasUsed + ExtDataGasUsed
+			// totalRequiredTips = BlockGasCost * BaseFee
+			// estimatedTip = totalRequiredTips / totalGasUsed
+			want: big.NewInt((101112*456)/(124) + 1), // +1 to round up
 		},
 	}
 	for _, test := range tests {