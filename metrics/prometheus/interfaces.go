5cb90304c950077a4978e125aed425c7036ae93f
Quentin Mc Gaw <quentin.mcgaw@avalabs.org>
2025-03-17 19:35:44 +0100

    Improve Registry comment
    - remove non linkable prometheus.Registry

--- metrics/prometheus/interfaces.go
+++ metrics/prometheus/interfaces.go
@@ -2,8 +2,8 @@
 // See the file LICENSE for licensing terms.
 package prometheus
 
-// Registry is a narrower interface of [prometheus.Registry] containing
-// only the required functions for the [Gatherer].
+// Registry is a narrower interface of the official prometheus
+// Registry containing only the required functions for the [Gatherer].
 type Registry interface {
 	// Call the given function for each registered metric.
 	Each(func(string, any))
