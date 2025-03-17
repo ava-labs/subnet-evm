f7adf7f66aad63051599d708f4ef1de423208466
Quentin Mc Gaw <quentin.mcgaw@avalabs.org>
2025-03-17 19:23:51 +0100

    Add mutex for WithMetrics call

--- internal/testutils/metrics.go
+++ internal/testutils/metrics.go
@@ -1,15 +1,22 @@
 package testutils
 
 import (
+	"sync"
 	"testing"
 
 	"github.com/ava-labs/libevm/metrics"
 )
 
+var metricsLock sync.Mutex
+
 // WithMetrics enables go-ethereum metrics globally for the test.
 // If the [metrics.Enabled] is already true, nothing is done.
 // Otherwise, it is set to true and is reverted to false when the test finishes.
 func WithMetrics(t *testing.T) {
+	metricsLock.Lock()
+	t.Cleanup(func() {
+		metricsLock.Unlock()
+	})
 	if metrics.Enabled {
 		return
 	}
