05cbf43c40282f0501ce7dd5ecf57e12d9bccf2b
Ceyhun Onur <ceyhun.onur@avalabs.org>
2025-04-21 14:36:23 +0300

    clarify tag latest

--- scripts/build_docker_image.sh
+++ scripts/build_docker_image.sh
@@ -25,7 +25,10 @@ SUBNET_EVM_PATH=$(
 # Load the constants
 source "$SUBNET_EVM_PATH"/scripts/constants.sh
 
-TAG_LATEST="${TAG_LATEST:-}"
+# ALLOW_TAG_LATEST is used to tag the image as 'latest' if set to true.
+# It only works if the image is built from the master branch. This is to avoid
+# tagging images from a manual triggered build as 'latest' with older avalanchego versions.
+ALLOW_TAG_LATEST="${ALLOW_TAG_LATEST:-}"
 
 # buildx (BuildKit) improves the speed and UI of builds over the legacy builder and
 # simplifies creation of multi-arch images.
@@ -105,7 +108,7 @@ ${DOCKER_CMD} -t "$IMAGE_NAME:$BUILD_IMAGE_ID" -t "$IMAGE_NAME:${DOCKERHUB_TAG}"
   --build-arg CURRENT_BRANCH="$CURRENT_BRANCH" \
   --build-arg VM_ID="$VM_ID"
 
-if [[ -n "${PUBLISH}" && $CURRENT_BRANCH == "master" && $TAG_LATEST == true ]]; then
+if [[ -n "${PUBLISH}" && $CURRENT_BRANCH == "master" && $ALLOW_TAG_LATEST == true ]]; then
   echo "Tagging current image as $IMAGE_NAME:latest"
   docker buildx imagetools create -t "$IMAGE_NAME:latest" "$IMAGE_NAME:$BUILD_IMAGE_ID"
 fi
