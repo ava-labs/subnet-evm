f0354e4fc009294fea82cc8a72398ee554dfcfc9
Ceyhun Onur <ceyhun.onur@avalabs.org>
2025-04-24 14:27:53 +0300

    revert simulator

--- scripts/run_simulator.sh
+++ scripts/run_simulator.sh
@@ -19,23 +19,23 @@ SUBNET_EVM_PATH=$(
 source "$SUBNET_EVM_PATH"/scripts/constants.sh
 
 run_simulator() {
-  #################################
-  echo "building simulator"
-  pushd ./cmd/simulator
-  go build -o ./simulator main/*.go
-  echo
+    #################################
+    echo "building simulator"
+    pushd ./cmd/simulator
+    go build -o ./simulator main/*.go
+    echo
 
-  popd
-  echo "running simulator from $PWD"
-  ./cmd/simulator/simulator \
-    --endpoints="$RPC_ENDPOINTS" \
-    --key-dir=./cmd/simulator/.simulator/keys \
-    --timeout=300s \
-    --workers=1 \
-    --txs-per-worker=50000 \
-    --batch-size=50000 \
-    --max-fee-cap=1000000 \
-    --max-tip-cap=10000
+    popd
+    echo "running simulator from $PWD"
+    ./cmd/simulator/simulator \
+        --endpoints="$RPC_ENDPOINTS" \
+        --key-dir=./cmd/simulator/.simulator/keys \
+        --timeout=300s \
+        --workers=1 \
+        --txs-per-worker=50000 \
+        --batch-size=50000 \
+        --max-fee-cap=1000000 \
+        --max-tip-cap=10000
 }
 
 run_simulator
