73337e7208f33862caf71ece0801135e8b1590b7
Jonathan Oppenheimer <jonathan.oppenheimer@avalabs.org>
2025-07-14 14:20:47 -0400

    change back to latest

--- plugin/evm/vm_upgrade_bytes_test.go
+++ plugin/evm/vm_upgrade_bytes_test.go
@@ -188,7 +188,7 @@ func TestNetworkUpgradesOverriden(t *testing.T) {
 		}`
 
 	vm := &VM{}
-	ctx, dbManager, _, _ := setupGenesis(t, upgradetest.NoUpgrades)
+	ctx, dbManager, _, _ := setupGenesis(t, upgradetest.Latest)
 	appSender := &enginetest.Sender{T: t}
 	appSender.CantSendAppGossip = true
 	appSender.SendAppGossipF = func(context.Context, commonEng.SendConfig, []byte) error { return nil }
