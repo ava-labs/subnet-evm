5679418cb3b1f361ae12067fe181753065308aa9
Jonathan Oppenheimer <jonathan.oppenheimer@avalabs.org>
2025-07-17 12:35:39 -0400

    fix import

--- network/network_test.go
+++ network/network_test.go
@@ -22,7 +22,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
-	"github.com/ava-labs/subnet-evm/peer/peertest"
+	"github.com/ava-labs/subnet-evm/network/peertest"
 	"github.com/ava-labs/subnet-evm/plugin/evm/message"
 
 	"github.com/ava-labs/avalanchego/codec"
