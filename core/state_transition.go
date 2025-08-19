aae013c2770cfe1b07298eb998c76e5cce5b57c2
Jonathan Oppenheimer <jonathan.oppenheimer@avalabs.org>
2025-08-19 13:07:17 -0400

    reduce diff

--- core/state_transition.go
+++ core/state_transition.go
@@ -164,8 +164,7 @@ func accessListGas(rules params.Rules, accessList types.AccessList) (uint64, err
 			}
 			gas = totalGas
 		} else {
-			pred := predicate.Predicate(accessTuple.StorageKeys)
-			predicateGas, err := predicaterContract.PredicateGas(pred)
+			predicateGas, err := predicaterContract.PredicateGas(predicate.Predicate(accessTuple.StorageKeys))
 			if err != nil {
 				return 0, err
 			}
