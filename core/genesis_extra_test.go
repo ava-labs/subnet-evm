ava-labs/avalanchego

ACP-226: add min delay excess to block and verify (#1263)
Signed-off-by: Ceyhun Onur <ceyhunonur54@gmail.com>
Co-authored-by: Stephen Buttolph <stephen@avalabs.org>
Co-authored-by: Michael Kaplan <55204436+michaelkaplan13@users.noreply.github.com>

core/genesis_extra_test.go
@@ -118,7 +118,7 @@ func TestGenesisToBlockDecoding(t *testing.T) {
 		upgradetest.Durango:           common.HexToHash("0xab4ce08ac987c618e1d12642338da6b2308e7f3886fb6a671e9560212d508d2a"),
 		upgradetest.Etna:              common.HexToHash("0x1094f685d39b737cf599fd599744b9849923a11ea3314826f170b443a87cb0e0"),
 		upgradetest.Fortuna:           common.HexToHash("0x1094f685d39b737cf599fd599744b9849923a11ea3314826f170b443a87cb0e0"),
-		upgradetest.Granite:           common.HexToHash("0x479c4950fdd50b7228808188b790fecde7e8b29776daf24e612307b0fd280365"),
+		upgradetest.Granite:           common.HexToHash("0xe870dd86820001c7ca3f2fb9a2f31e1fcfdb17743874a061db8e75425ae7fd23"),
 	}
 	for fork, chainConfig := range paramstest.ForkToChainConfig {
 		t.Run(fork.String(), func(t *testing.T) {

core/genesis_test.go
@@ -62,7 +62,7 @@ func setupGenesisBlock(db ethdb.Database, triedb *triedb.Database, genesis *Gene
 }
 
 func TestGenesisBlockForTesting(t *testing.T) {
-	genesisBlockForTestingHash := common.HexToHash("0xab81ae467cf9b9d64b1f54e6ee341ded32432cd57a33d5a1909d7585f5e9db31")
+	genesisBlockForTestingHash := common.HexToHash("0x9cb63c1f765d359257f0a9f132c967d7d9325015be73789cade2db5f65e83ad6")
 	block := GenesisBlockForTesting(rawdb.NewMemoryDatabase(), common.Address{1}, big.NewInt(1))
 	if block.Hash() != genesisBlockForTestingHash {
 		t.Errorf("wrong testing genesis hash, got %v, want %v", block.Hash(), genesisBlockForTestingHash)

ava-labs/subnet-evm

core/genesis_extra_test.go
@@ -87,7 +87,7 @@ func TestGenesisToBlockDecoding(t *testing.T) {
 		upgradetest.Durango:       common.HexToHash("0x74dd5d404823f342fb3d372ea289565e5b1ff25d07e48a59db8130c5f61e941a"),
 		upgradetest.Etna:          common.HexToHash("0xa5de01cb7e5c6d721be62ab4b37878e863d65e0c1fe308e5df1f4c5b148650f9"),
 		upgradetest.Fortuna:       common.HexToHash("0xa5de01cb7e5c6d721be62ab4b37878e863d65e0c1fe308e5df1f4c5b148650f9"),
-		upgradetest.Granite:       common.HexToHash("0x9f3811050eedcfd289695cd16bbc3ccd9db4c3b32aec1125e96654df35285656"),
+		upgradetest.Granite:       common.HexToHash("0x4d5ff1b8509d30c6b7842b784a6cf8be78e34c78bc5de8559ea6f15ad18d6a10"),
 	}
 	for fork, chainConfig := range paramstest.ForkToChainConfig {
 		t.Run(fork.String(), func(t *testing.T) {

core/genesis_test.go
@@ -65,7 +65,7 @@ func setupGenesisBlock(db ethdb.Database, triedb *triedb.Database, genesis *Gene
 }
 
 func TestGenesisBlockForTesting(t *testing.T) {
-	genesisBlockForTestingHash := common.HexToHash("0x66d680f02c4a370700a8c0af564d147f65b797cc6752ee62675c6e6927dc45ae")
+	genesisBlockForTestingHash := common.HexToHash("0x7d576d17f8083b02cada42b908f66c0f45a9be57a06a4673b89e53b81053a787")
 	block := GenesisBlockForTesting(rawdb.NewMemoryDatabase(), common.Address{1}, big.NewInt(1))
 	if block.Hash() != genesisBlockForTestingHash {
 		t.Errorf("wrong testing genesis hash, got %v, want %v", block.Hash(), genesisBlockForTestingHash)