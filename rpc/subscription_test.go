2dbbf71e319fcbb5394cc6baa387f03c00f5b90d
Austin Larson <78000745+alarso16@users.noreply.github.com>
2025-10-28 09:25:28 -0400

    test: Update rpc tests to reduce flake likelihood (ava-labs/coreth#1363)

--- rpc/subscription_test.go
+++ rpc/subscription_test.go
@@ -35,21 +35,13 @@ import (
 	"io"
 	"math/big"
 	"net"
-	"os"
 	"strings"
 	"testing"
 	"time"
 
 	"github.com/ava-labs/libevm/common"
 	"github.com/ava-labs/libevm/core/types"
-
-	"github.com/ava-labs/subnet-evm/plugin/evm/customtypes"
 )
-
-func TestMain(m *testing.M) {
-	customtypes.Register()
-	os.Exit(m.Run())
-}
 
 func TestNewID(t *testing.T) {
 	hexchars := "0123456789ABCDEFabcdef"
