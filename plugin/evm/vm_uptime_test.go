54a4fb70f92c5d3e76550c79d2c15b3e1005a918
Jonathan Oppenheimer <jonathan.oppenheimer@avalabs.org>
2025-10-28 14:23:00 -0400

    improve test

--- plugin/evm/vm_uptime_test.go
+++ plugin/evm/vm_uptime_test.go
@@ -13,6 +13,8 @@ import (
 	"github.com/ava-labs/avalanchego/snow/engine/enginetest"
 	"github.com/ava-labs/avalanchego/snow/validators/validatorstest"
 	"github.com/ava-labs/avalanchego/upgrade/upgradetest"
+	"github.com/ava-labs/avalanchego/utils/timer/mockable"
+	"github.com/ava-labs/avalanchego/vms/evm/uptimetracker"
 	"github.com/stretchr/testify/require"
 
 	commonEng "github.com/ava-labs/avalanchego/snow/engine/common"
@@ -22,7 +24,8 @@ import (
 func TestUptimeTracker(t *testing.T) {
 	testNodeID := ids.GenerateTestNodeID()
 	testValidationID := ids.GenerateTestID()
-	startTime := uint64(time.Now().Unix())
+	baseTime := time.Unix(0, 0)
+	startTime := uint64(baseTime.Unix())
 
 	// TODO(JonathanOppenheimer): see func NewTestValidatorState() -- this should be examined
 	// when we address the issue of that function.
@@ -62,17 +65,61 @@ func TestUptimeTracker(t *testing.T) {
 	))
 	defer vm.Shutdown(context.Background())
 
+	// Replace the clock with a mockable one and recreate the uptime tracker;
+	// this allows us to control the time progression
+	var err error
+	clock := &mockable.Clock{}
+	clock.Set(baseTime)
+	vm.clock = clock
+	vm.uptimeTracker, err = uptimetracker.New(vm.ctx.ValidatorState, vm.ctx.SubnetID, vm.validatorsDB, clock)
+	require.NoError(err)
+
 	require.NoError(vm.SetState(context.Background(), snow.Bootstrapping))
 
 	// After bootstrapping but before NormalOp, uptimeTracker hasn't started syncing yet
-	// so GetUptime should return an error
-	_, _, err := vm.uptimeTracker.GetUptime(testValidationID)
-	require.Error(err)
+	// so GetUptime should not be able to find the validation ID
+	_, _, err = vm.uptimeTracker.GetUptime(testValidationID)
+	require.ErrorIs(err, uptimetracker.ErrValidationIDNotFound)
 
 	require.NoError(vm.SetState(context.Background(), snow.NormalOp))
 
 	// After transitioning to NormalOp, Sync() is called automatically to populate uptime
 	// from validator state, so GetUptime should work now
-	_, _, err = vm.uptimeTracker.GetUptime(testValidationID)
+	// The validator starts with 0 uptime since it has not connected yet
+	initialUptime, initialLastUpdated, err := vm.uptimeTracker.GetUptime(testValidationID)
 	require.NoError(err)
+	require.Equal(time.Duration(0), initialUptime, "Initial uptime should be 0")
+	require.Equal(baseTime, initialLastUpdated, "Initial lastUpdated should be baseTime")
+
+	// connect, time passes
+	require.NoError(vm.uptimeTracker.Connect(testNodeID))
+	clock.Set(baseTime.Add(1 * time.Hour))
+
+	// get uptime after 1 hour of being connected - uptime should have increased by 1 hour
+	upDuration, lastUpdated, err := vm.uptimeTracker.GetUptime(testValidationID)
+	require.NoError(err)
+	require.Equal(1*time.Hour, upDuration, "Uptime should be 1 hour")
+	require.Equal(baseTime.Add(1*time.Hour), lastUpdated, "lastUpdated should reflect new clock time")
+
+	// disconnect, time passes another 2 hours
+	require.NoError(vm.uptimeTracker.Disconnect(testNodeID))
+	clock.Set(baseTime.Add(2 * time.Hour))
+
+	// get uptime - should not have increased since we were disconnected
+	upDuration, lastUpdated, err = vm.uptimeTracker.GetUptime(testValidationID)
+	require.NoError(err)
+	require.Equal(1*time.Hour, upDuration, "Uptime should not increase while disconnected")
+	require.Equal(baseTime.Add(2*time.Hour), lastUpdated, "lastUpdated should still advance")
+
+	// reconnect, time passes another 30 minutes
+	require.NoError(vm.uptimeTracker.Connect(testNodeID))
+	clock.Set(baseTime.Add(2*time.Hour + 30*time.Minute))
+
+	// get uptime - total uptime should be 1h30m
+	upDuration, lastUpdated, err = vm.uptimeTracker.GetUptime(testValidationID)
+	require.NoError(err)
+	require.Equal(1*time.Hour+30*time.Minute, upDuration,
+		"Uptime should be 1h30m (1h from first connection + 30m from second)")
+	require.Equal(baseTime.Add(2*time.Hour+30*time.Minute), lastUpdated,
+		"lastUpdated should reflect final clock time")
 }
