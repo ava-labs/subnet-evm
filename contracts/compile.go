c546cdd3c2276d1b40c018a876535885942fa1d2
Jonathan Oppenheimer <jonathan.oppenheimer@avalabs.org>
2025-10-30 11:06:35 -0400

    use proper version of solc

--- contracts/compile.go
+++ contracts/compile.go
@@ -5,14 +5,14 @@
 // This file contains go:generate directives to compile Solidity contracts using solc.
 package contracts
 
-//go:generate solc -o ./artifacts --overwrite --abi --bin --base-path . @openzeppelin/contracts/=lib/openzeppelin-contracts/contracts/ contracts/AllowList.sol contracts/ERC20NativeMinter.sol contracts/ExampleDeployerList.sol contracts/ExampleFeeManager.sol contracts/ExampleRewardManager.sol contracts/ExampleTxAllowList.sol contracts/ExampleWarp.sol
+//go:generate solc-v0.8.30 -o ./artifacts --overwrite --abi --bin --base-path . @openzeppelin/contracts/=lib/openzeppelin-contracts/contracts/ contracts/AllowList.sol contracts/ERC20NativeMinter.sol contracts/ExampleDeployerList.sol contracts/ExampleFeeManager.sol contracts/ExampleRewardManager.sol contracts/ExampleTxAllowList.sol contracts/ExampleWarp.sol
 
 // Compile interface contracts
-//go:generate solc -o ./artifacts --overwrite --abi --bin --base-path . contracts/interfaces/IAllowList.sol @openzeppelin/contracts/=lib/openzeppelin-contracts/contracts/
-//go:generate solc -o ./artifacts --overwrite --abi --bin --base-path . contracts/interfaces/IFeeManager.sol @openzeppelin/contracts/=lib/openzeppelin-contracts/contracts/
-//go:generate solc -o ./artifacts --overwrite --abi --bin --base-path . contracts/interfaces/INativeMinter.sol @openzeppelin/contracts/=lib/openzeppelin-contracts/contracts/
-//go:generate solc -o ./artifacts --overwrite --abi --bin --base-path . contracts/interfaces/IRewardManager.sol @openzeppelin/contracts/=lib/openzeppelin-contracts/contracts/
-//go:generate solc -o ./artifacts --overwrite --abi --bin --base-path . contracts/interfaces/IWarpMessenger.sol @openzeppelin/contracts/=lib/openzeppelin-contracts/contracts/
+//go:generate solc-v0.8.30 -o ./artifacts --overwrite --abi --bin --base-path . contracts/interfaces/IAllowList.sol @openzeppelin/contracts/=lib/openzeppelin-contracts/contracts/
+//go:generate solc-v0.8.30 -o ./artifacts --overwrite --abi --bin --base-path . contracts/interfaces/IFeeManager.sol @openzeppelin/contracts/=lib/openzeppelin-contracts/contracts/
+//go:generate solc-v0.8.30 -o ./artifacts --overwrite --abi --bin --base-path . contracts/interfaces/INativeMinter.sol @openzeppelin/contracts/=lib/openzeppelin-contracts/contracts/
+//go:generate solc-v0.8.30 -o ./artifacts --overwrite --abi --bin --base-path . contracts/interfaces/IRewardManager.sol @openzeppelin/contracts/=lib/openzeppelin-contracts/contracts/
+//go:generate solc-v0.8.30 -o ./artifacts --overwrite --abi --bin --base-path . contracts/interfaces/IWarpMessenger.sol @openzeppelin/contracts/=lib/openzeppelin-contracts/contracts/
 
 // NOTE: Test contracts (contracts/test/*Test.sol) are NOT compiled here
 // They use DS-Test which we're deprecating. These will be replaced with Go tests in Phase 3.
