a4959f18c6fe03b09c08f7731a963e769b27a6a6
Ceyhun Onur <ceyhun.onur@avalabs.org>
2025-11-14 21:25:12 +0300

    replace deployer allowlist hardhat tests with simulated backend

---
+++ precompile/allowlist/allowlisttest/AllowListTest.sol
@@ -0,0 +1,23 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import "./AllowList.sol";
+import "../../precompiletest/test.sol";
+
+contract AllowListTestHelper is DSTest {
+    function assertRole(uint result, AllowList.Role role) internal {
+        assertEq(result, uint(role));
+    }
+}
+
+contract AllowListTest is AllowList {
+    // Precompiled Allow List Contract Address
+    constructor(address precompileAddr) AllowList(precompileAddr) {}
+
+    function deployContract() public {
+        new Example();
+    }
+}
+
+// This is an empty contract that can be used to test contract deployment
+contract Example {}
